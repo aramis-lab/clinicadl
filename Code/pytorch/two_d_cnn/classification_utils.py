5968d97acc7e43241c5adec9ead7cc84ff85f96b
junhao.wen <anbai106@hotmail.com>
2018-11-16 19:13:42 +0100

    dont use clinica dependancy

--- Code/pytorch/two_d_cnn/classification_utils.py
+++ Code/pytorch/two_d_cnn/classification_utils.py
@@ -5,7 +5,6 @@ import matplotlib.ticker as ticker
 import nibabel as nib
 from torch.utils.data import Dataset
 import random
-from clinica.pipelines.machine_learning.svm_utils import evaluate_prediction
 import os, shutil
 from skimage.transform import resize
 from os import path
@@ -482,3 +481,66 @@ def results_to_tsvs(output_dir, iteration, subject_list, y_truth, y_hat):
     pd.DataFrame(results, index=[0]).to_csv(os.path.join(iteration_dir, 'result.tsv'), index=False, sep='\t', encoding='utf-8')
 
     return iteration_subjects_df, pd.DataFrame(results, index=[0])
+
+def evaluate_prediction(y, y_hat):
+
+    true_positive = 0.0
+    true_negative = 0.0
+    false_positive = 0.0
+    false_negative = 0.0
+
+    tp = []
+    tn = []
+    fp = []
+    fn = []
+
+    for i in range(len(y)):
+        if y[i] == 1:
+            if y_hat[i] == 1:
+                true_positive += 1
+                tp.append(i)
+            else:
+                false_negative += 1
+                fn.append(i)
+        else:  # -1
+            if y_hat[i] == 0:
+                true_negative += 1
+                tn.append(i)
+            else:
+                false_positive += 1
+                fp.append(i)
+
+    accuracy = (true_positive + true_negative) / (true_positive + true_negative + false_positive + false_negative)
+
+    if (true_positive + false_negative) != 0:
+        sensitivity = true_positive / (true_positive + false_negative)
+    else:
+        sensitivity = 0.0
+
+    if (false_positive + true_negative) != 0:
+        specificity = true_negative / (false_positive + true_negative)
+    else:
+        specificity = 0.0
+
+    if (true_positive + false_positive) != 0:
+        ppv = true_positive / (true_positive + false_positive)
+    else:
+        ppv = 0.0
+
+    if (true_negative + false_negative) != 0:
+        npv = true_negative / (true_negative + false_negative)
+    else:
+        npv = 0.0
+
+    balanced_accuracy = (sensitivity + specificity) / 2
+
+    results = {'accuracy': accuracy,
+               'balanced_accuracy': balanced_accuracy,
+               'sensitivity': sensitivity,
+               'specificity': specificity,
+               'ppv': ppv,
+               'npv': npv,
+               'confusion_matrix': {'tp': len(tp), 'tn': len(tn), 'fp': len(fp), 'fn': len(fn)}
+               }
+
+    return results
