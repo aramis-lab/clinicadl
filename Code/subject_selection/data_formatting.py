5b6508f49e8c4f35f6d71fba3573ec246e95657a
elina.thibeausutre <elina.thibeausutre@icm-institute.org>
2018-12-19 10:54:00 +0100

    Add subject selection tool

---
+++ Code/subject_selection/data_formatting.py
@@ -0,0 +1,14 @@
+"""
+Source files can be obtained by running the following commands on a BIDS folder:
+ - clinica iotools merge-tsv
+ - clinica iotools missing-mods
+To download Clinica follow the instructions at http://www.clinica.run/doc/#installation
+"""
+
+from utils import *
+
+import os
+import pandas as pd
+import numpy as np
+from os import path
+
