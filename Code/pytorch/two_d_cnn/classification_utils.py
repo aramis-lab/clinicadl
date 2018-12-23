156a4bd31994044a5c855c7c9b117fb57b7cf049
junhao.wen <anbai106@hotmail.com>
2018-12-23 16:06:46 +0100

    Adding resnet transfer learning for 2D cnn
    Former-commit-id: 0af57b94856b98a0a9b289453e88119a216cf472

--- Code/pytorch/two_d_cnn/classification_utils.py
+++ Code/pytorch/two_d_cnn/classification_utils.py
@@ -78,6 +78,7 @@ def train(model, data_loader, use_cuda, loss_func, optimizer, writer, epoch_i, m
             if model_mode == "train" or model_mode == 'valid':
                 print("output.device: " + str(output.device))
                 print("ground_truth.device: " + str(ground_truth.device))
+                print("The predicted label is: " + str(output))
                 loss = loss_func(output, ground_truth)
                 loss_batch += loss.item()
             correct_this_batch = (predict.squeeze(1) == ground_truth).sum().float()
@@ -200,55 +201,6 @@ def multiple_time_points(df, subset_df):
     mtp_df.reset_index(inplace=True, drop=True)
     return mtp_df
 
-#
-# def split_subjects_to_tsv_old(diagnoses_tsv, n_splits=5, val_size=0.15):
-#     """
-#     Write the tsv files corresponding to the train/val/test splits of all folds
-#
-#     :param diagnoses_tsv: (str) path to the tsv file with diagnoses
-#     :param n_splits: (int) the number of splits wanted in the cross-validation
-#     :param val_size: (float) proportion of the train set being used for validation
-#     :return: None
-#     """
-#
-#     df = pd.io.parsers.read_csv(diagnoses_tsv, sep='\t')
-#     if 'diagnosis' not in list(df.columns.values):
-#         raise Exception('Diagnoses file is not in the correct format.')
-#     # check if we have duplicated row in the tsv, this will cause problem if yes
-#     if df.duplicated().unique().size > 1:
-#         raise ValueError("There are duplicated rows in the tsv files, please double check it!!!")
-#     # Here we reduce the DataFrame to have only one diagnosis per subject (multiple time points case)
-#     diagnosis_df = subject_diagnosis_df(df)
-#
-#     diagnoses_list = list(diagnosis_df.diagnosis)
-#     unique = list(set(diagnoses_list))
-#     y = np.array([unique.index(x) for x in diagnoses_list])  # There is one label per diagnosis depending on the order
-#
-#     splits = StratifiedKFold(n_splits=n_splits, shuffle=True)
-#
-#     n_iteration = 0
-#     for train_index, test_index in splits.split(np.zeros(len(y)), y):
-#
-#         y_train = y[train_index]
-#         diagnosis_df_train = diagnosis_df.loc[train_index]
-#
-#         # split the train data into training and validation set
-#         skf_2 = StratifiedShuffleSplit(n_splits=1, test_size=val_size)
-#         indices = next(skf_2.split(np.zeros(len(y_train)), y_train))
-#         train_ind, valid_ind = indices
-#
-#         df_test = diagnosis_df.iloc[test_index]
-#         df_sub_valid = diagnosis_df_train.iloc[valid_ind]
-#         df_sub_train = diagnosis_df_train.iloc[train_ind]
-#         df_valid = multiple_time_points(df, df_sub_valid)
-#         df_train = multiple_time_points(df, df_sub_train)
-#         df_test = multiple_time_points(df, df_test)
-#
-#         df_train.to_csv(path.join(path.dirname(diagnoses_tsv), path.basename(diagnoses_tsv).split('.')[0] + '_iteration-' + str(n_iteration) + '_splits-' + str(n_splits) + '_valid_size-' + str(val_size) + '_train.tsv'), sep='\t', index=False)
-#         df_test.to_csv(path.join(path.dirname(diagnoses_tsv), path.basename(diagnoses_tsv).split('.')[0] + '_iteration-' + str(n_iteration) + '_splits-' + str(n_splits) + '_valid_size-' + str(val_size) + '_test.tsv'), sep='\t', index=False)
-#         df_valid.to_csv(path.join(path.dirname(diagnoses_tsv), path.basename(diagnoses_tsv).split('.')[0] + '_iteration-' + str(n_iteration) + '_splits-' + str(n_splits) + '_valid_size-' + str(val_size) + '_valid.tsv'), sep='\t', index=False)
-#         n_iteration += 1
-
 def split_subjects_to_tsv(diagnoses_tsv, val_size=0.15):
     """
     Write the tsv files corresponding to the train/val/test splits of all folds
@@ -285,27 +237,6 @@ def split_subjects_to_tsv(diagnoses_tsv, val_size=0.15):
 
     df_valid.to_csv(path.join(sets_dir, 'valid.tsv'), sep='\t', index=False)
     df_train.to_csv(path.join(sets_dir, 'train.tsv'), sep='\t', index=False)
-
-
-
-# def load_split_old(diagnoses_tsv, fold, n_splits, val_size=0.15):
-#     """
-#     Loads the
-#
-#     :param diagnoses_tsv: (str) path to the tsv file with diagnoses
-#     :param fold: (int) the number of the current fold
-#     :return: 3 DataFrame
-#         training_tsv
-#         test_tsv
-#         valid_tsv
-#     """
-#     training_tsv = path.join(path.dirname(diagnoses_tsv),
-#                              path.basename(diagnoses_tsv).split('.')[0] + '_iteration-' + str(fold) + '_splits-' + str(n_splits) + '_valid_size-' + str(val_size) + '_train.tsv')
-#     test_tsv = path.join(path.dirname(diagnoses_tsv),
-#                          path.basename(diagnoses_tsv).split('.')[0] + '_iteration-' + str(fold) + '_splits-' + str(n_splits) + '_valid_size-' + str(val_size) + '_test.tsv')
-#     valid_tsv = path.join(path.dirname(diagnoses_tsv),
-#                           path.basename(diagnoses_tsv).split('.')[0] + '_iteration-' + str(fold) + '_splits-' + str(n_splits) + '_valid_size-' + str(val_size) + '_valid.tsv')
-#     return training_tsv, test_tsv, valid_tsv
 
 def load_split(diagnoses_tsv, val_size=0.15):
     """
@@ -346,6 +277,7 @@ class mri_to_slice_level(Dataset):
 
     Return: a Pytorch Dataset objective
     """
+    ## TODO: using pytorch tensor.unfold to extract the slices instead of numpy array.
 
     def __init__(self, caps_directory, tsv, transform=None, transfer_learning=True, mri_plane=0):
         """
