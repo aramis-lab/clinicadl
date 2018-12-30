33c6123ae3414017641673866293caa6c6188fb6
junhao.wen <anbai106@hotmail.com>
2018-12-30 18:04:47 +0100

    Implement SParseAutoEncoder for 3D CNN
    Former-commit-id: 2185cf1f688fac982b3a1b65ae8561832564fefa

--- Code/image_preprocessing/run_postprocessing.py
+++ Code/image_preprocessing/run_postprocessing.py
@@ -4,12 +4,12 @@ from T1_postprocessing import postprocessing_t1w
 
 ## run the pipeline
 #  for test
-caps_directory = '/teams/ARAMIS/PROJECTS/junhao.wen/PhD/ADNI_classification/gitlabs/AD-DL/Results/CAPS'
-tsv = '/teams/ARAMIS/PROJECTS/junhao.wen/PhD/ADNI_classification/gitlabs/AD-DL/tsv_files/image_preprocessing_test.tsv'
+caps_directory = '/localdrive10TB/scratch/junhao.wen/Frontiers_DL/CAPS_ADNI'
+tsv = '/teams/ARAMIS/PROJECTS/junhao.wen/PhD/ADNI_classification/gitlabs/AD-DL/tsv_files/tsv_after_qc/ADNI_after_qc.tsv'
 patch_size = 21
 stride_size = 21
-working_dir = '/teams/ARAMIS/PROJECTS/junhao.wen/PhD/ADNI_classification/gitlabs/AD-DL/Results/working_dir'
+working_dir = '/localdrive10TB/scratch/junhao.wen/Frontiers_DL/working_dir_ADNI_postprocessing'
 
 
 wf = postprocessing_t1w(caps_directory, tsv, patch_size, stride_size, working_directory=working_dir)
-wf.run(plugin='MultiProc', plugin_args={'n_procs': 8})
+wf.run(plugin='MultiProc', plugin_args={'n_procs': 72})
