02601c77c50322c97904de7c1ba3d03cc245805c
junhao.wen <anbai106@hotmail.com>
2019-01-01 15:31:56 +0100

    image postprocessing

--- Code/image_preprocessing/run_postprocessing.py
+++ Code/image_preprocessing/run_postprocessing.py
@@ -4,12 +4,12 @@ from T1_postprocessing import postprocessing_t1w
 
 ## run the pipeline
 #  for test
-caps_directory = '/localdrive10TB/scratch/junhao.wen/Frontiers_DL/CAPS_ADNI'
+caps_directory = '/network/lustre/dtlake01/aramis/projects/clinica/CLINICA_datasets/CAPS/Frontiers_DL/ADNI'
 tsv = '/teams/ARAMIS/PROJECTS/junhao.wen/PhD/ADNI_classification/gitlabs/AD-DL/tsv_files/tsv_after_qc/ADNI_after_qc.tsv'
 patch_size = 21
 stride_size = 21
-working_dir = '/localdrive10TB/scratch/junhao.wen/Frontiers_DL/working_dir_ADNI_postprocessing'
+working_dir = '/network/lustre/dtlake01/aramis/projects/clinica/CLINICA_datasets/run/junhao.wen'
 
 
-wf = postprocessing_t1w(caps_directory, tsv, patch_size, stride_size, working_directory=working_dir)
+wf = postprocessing_t1w(caps_directory, tsv, patch_size, stride_size, working_directory=working_dir, extract_method='slice')
 wf.run(plugin='MultiProc', plugin_args={'n_procs': 72})
