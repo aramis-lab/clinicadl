742a63fa5ef70e752f43559fbb2df6065219f261
elina.thibeausutre <elina.thibeausutre@icm-institute.org>
2019-01-14 10:53:12 +0100

    Different learning rates for convolutional and FC layers

--- Code/pytorch/three_d_cnn/main.py
+++ Code/pytorch/three_d_cnn/main.py
@@ -49,6 +49,9 @@ parser.add_argument("--transfer_learning_epochs", "-t_e", type=int, default=10,
                     help="Number of epochs for pretraining")
 parser.add_argument("--transfer_learning_rate", "-t_lr", type=float, default=1e-4,
                     help='The learning rate used for AE pretraining')
+parser.add_argument("--features_learning_rate", "f_lr", type=float, default=None,
+                    help="Learning rate applied to the convolutional layers."
+                         "If None all the layers have the same learning rate.")
 parser.add_argument("--visualization", action='store_true', default=False,
                     help='Chooses if visualization is done on AE pretraining')
 
@@ -167,8 +170,15 @@ def main(options):
 
         # Define criterion and optimizer
         criterion = torch.nn.CrossEntropyLoss()
-        optimizer = eval("torch.optim." + options.optimizer)(filter(lambda x: x.requires_grad, model.parameters()),
-                                                             options.learning_rate)
+        if options.features_learning_rate is None:
+            optimizer = eval("torch.optim." + options.optimizer)(filter(lambda x: x.requires_grad, model.parameters()),
+                                                                 options.learning_rate)
+        else:
+            optimizer = eval("torch.optim." + options.optimizer)([
+                {'params': filter(lambda x: x.requires_grad, model.features.parameters()),
+                 'lr': options.features_learning_rate},
+                {'params': filter(lambda x: x.requires_grad, model.classifier.parameters())}
+                ], lr=options.learning_rate)
 
         print('Beginning the training task')
         training_time = time()
