7c5e474968e482558f68541501404754bdbc1157
junhao.wen <anbai106@hotmail.com>
2019-02-08 15:52:08 +0100

    adding one FC layer, cuz its hard to convert with 4 conv plus 2 fc
    Former-commit-id: a0889ed95d35c4c000e2dcb2d4f53d244265988f

--- Code/pytorch/two_d_cnn/model.py
+++ Code/pytorch/two_d_cnn/model.py
@@ -161,6 +161,14 @@ def DenseNet161(transfer_learning=True, **kwargs):
 ### ResNets
 ############################################
 
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
 def ResNet(resnet_type='resnet18', transfer_learning=True, **kwargs):
     """
     Construct a RestNet model, the type of resnet models were list as variable: resnet_type.
--- Code/pytorch/two_d_cnn/models/resnet_tl.py
+++ Code/pytorch/two_d_cnn/models/resnet_tl.py
@@ -1,15 +1,6 @@
 import torch.nn as nn
 import math
 
-model_urls = {
-    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
-    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
-}
-
-
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
