6674204498d2212f8672a1dcdcd8ac540120fc05
junhao.wen <anbai106@hotmail.com>
2019-02-10 17:04:47 +0100

    excute hippo postprocessing
    Former-commit-id: 1647746c3d1bc1203c3a1fc478bf2207a6c844d4

--- Code/image_preprocessing/run_postprocessing_hippo.py
+++ Code/image_preprocessing/run_postprocessing_hippo.py
@@ -10,12 +10,13 @@ caps_directory= '/network/lustre/dtlake01/aramis/projects/clinica/CLINICA_datase
 tsv= '/network/lustre/iss01/home/junhao.wen/Project/AD-DL/tsv_files/tsv_after_qc/ADNI_after_qc.tsv'
 # tsv= '/teams/ARAMIS/PROJECTS/junhao.wen/PhD/ADNI_classification/gitlabs/AD-DL/tsv_files/test.tsv'
 working_dir = '/network/lustre/dtlake01/aramis/projects/clinica/CLINICA_datasets/CAPS/Frontiers_DL/working_dir'
+# working_dir = '/network/lustre/dtlake01/aramis/projects/clinica/CLINICA_datasets/CAPS/Frontiers_DL'
 
 
 ## for the left hippocampus
 wf = postprocessing_t1w_extract_hippo(caps_directory, tsv, working_directory=working_dir, hemi='left')
-wf.run(plugin='MultiProc', plugin_args={'n_procs': 8})
+wf.run(plugin='MultiProc', plugin_args={'n_procs': 72})
 
 ## for the right hippocampus
 wf = postprocessing_t1w_extract_hippo(caps_directory, tsv, working_directory=working_dir, hemi='right')
-wf.run(plugin='MultiProc', plugin_args={'n_procs': 8})
+wf.run(plugin='MultiProc', plugin_args={'n_procs': 72})
