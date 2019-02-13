ef6da987d97e152074d608a718a3566995b9b12b
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2019-02-13 11:27:40 +0100

    Add model for SPM data

--- Code/pytorch/three_d_cnn/model.py
+++ Code/pytorch/three_d_cnn/model.py
@@ -68,6 +68,66 @@ class Conv5_FC3(nn.Module):
         return x
 
 
+class Conv5_FC3_mni(nn.Module):
+    """
+    Classifier for a multi-class classification task
+
+    Initially named Initial_architecture
+    """
+    def __init__(self, dropout=0.5):
+        super(Conv5_FC3_mni, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv3d(1, 8, 3, padding=1),
+            nn.BatchNorm3d(8),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(8, 16, 3, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(16, 32, 3, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(32, 64, 3, padding=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+            nn.Conv3d(64, 128, 3, padding=1),
+            nn.BatchNorm3d(128),
+            nn.ReLU(),
+            PadMaxPool3d(2, 2),
+
+        )
+
+        self.classifier = nn.Sequential(
+            Flatten(),
+            nn.Dropout(p=dropout),
+
+            nn.Linear(128 * 4 * 5 * 4, 1300),
+            nn.ReLU(),
+
+            nn.Linear(1300, 50),
+            nn.ReLU(),
+
+            nn.Linear(50, 2)
+
+        )
+
+        self.flattened_shape = [-1, 128, 4, 5, 4]
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+
+        return x
+
+
 def create_model(options):
     from classification_utils import load_model
     from os import path
