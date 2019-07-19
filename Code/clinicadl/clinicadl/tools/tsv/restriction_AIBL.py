e4977a6a9f7b6fbb5ec55c4ed8bf16915e4244a8
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2019-07-19 14:47:36 +0200

    Add restriction files
    Former-commit-id: bbb248f02256bef999868ef097ceb8d7764246af

---
+++ Code/clinicadl/clinicadl/tools/tsv/restriction_AIBL.py
@@ -0,0 +1,26 @@
+import argparse
+import pandas as pd
+from copy import deepcopy
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Argparser for data formatting")
+
+    # Mandatory arguments
+    parser.add_argument("merged_tsv", type=str,
+                        help="Path to the file obtained by the command clinica iotools merge-tsv.")
+    parser.add_argument("results_path", type=str,
+                        help="Path to the resulting tsv file (filename included).")
+
+    args = parser.parse_args()
+
+    merged_df = pd.read_csv(args.merged_tsv, sep='\t')
+    merged_df.set_index(['participant_id', 'session_id'], inplace=True)
+    results_df = deepcopy(merged_df)
+    for subject, subject_df in merged_df.groupby(level=0):
+        examination_df = subject_df['examination_date']
+        dates_list = examination_df.values
+        if (dates_list == '-4').all():
+            results_df.drop(subject, inplace=True)
+
+    results_df.to_csv(args.results_path, sep='\t')
---
+++ Code/clinicadl/clinicadl/tools/tsv/restriction_OASIS.py
@@ -0,0 +1,22 @@
+import argparse
+import pandas as pd
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Argparser for data formatting")
+
+    # Mandatory arguments
+    parser.add_argument("merged_tsv", type=str,
+                        help="Path to the file obtained by the command clinica iotools merge-tsv.")
+    parser.add_argument("results_path", type=str,
+                        help="Path to the resulting tsv file (filename included).")
+
+    # Modality selection
+    parser.add_argument("--age_restriction", type=int, default=62,
+                        help="Includes all the subjects older than the limit age (limit age included).")
+
+    args = parser.parse_args()
+
+    merged_df = pd.read_csv(args.merged_tsv, sep='\t')
+    results_df = merged_df[merged_df.age_bl >= args.age_restriction]
+    results_df.to_csv(args.results_path, sep='\t', index=False)
