c96ff6d6680bf3b740bc2d70761a9478eabce85b
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2019-08-26 15:10:26 +0200

    Refactor Early stopping
    Former-commit-id: 3d504a70d276d068db9479d4d63d4b55af96cdea

--- Code/clinicadl/clinicadl/classifiers/two_d_cnn/main_training.py
+++ Code/clinicadl/clinicadl/classifiers/two_d_cnn/main_training.py
@@ -191,7 +191,7 @@ def main(options):
         y_hats_valid = []
 
         # initialize the early stopping instance
-        early_stopping = EarlyStopping('loss', min_delta=options.tolerance, patience=options.patience)
+        early_stopping = EarlyStopping('min', min_delta=options.tolerance, patience=options.patience)
 
         for epoch in range(options.epochs):
             print("At %s -th epoch." % str(epoch))
