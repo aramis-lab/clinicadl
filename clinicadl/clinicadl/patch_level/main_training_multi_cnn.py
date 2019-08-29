1f4c87a4e3f023e612c814713d100cff9d8cf66a
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2019-08-29 17:19:48 +0200

    Change transfer learning to avoid manual copy paste
    Former-commit-id: 0c6e770dad0cec0202b842e16345c27ec1cc68d1

--- clinicadl/clinicadl/patch_level/main_training_multi_cnn.py
+++ clinicadl/clinicadl/patch_level/main_training_multi_cnn.py
@@ -10,9 +10,8 @@ import torchvision.transforms as transforms
 from .utils import load_model_after_ae, load_model_after_cnn
 from .utils import MRIDataset_patch_by_index, train, hard_voting_to_tsvs
 
-from tools.deep_learning import EarlyStopping, save_checkpoint, commandline_to_json
+from tools.deep_learning import EarlyStopping, save_checkpoint, commandline_to_json, create_model
 from tools.deep_learning.data import MinMaxNormalization, load_data
-from tools.deep_learning.models import create_model
 
 __author__ = "Junhao Wen"
 __copyright__ = "Copyright 2018 The Aramis Lab Team"
@@ -35,6 +34,8 @@ parser.add_argument("output_dir", type=str,
                     help="Path to store the classification outputs and the tsv files containing the performances.")
 
 # Data management
+parser.add_argument("--diagnoses", default=["sMCI", "pMCI"], type=str,
+                    help="Labels based on binary classification")
 parser.add_argument("--patch_size", default=50, type=int,
                     help="The patch size extracted from the MRI")
 parser.add_argument("--patch_stride", default=50, type=int,
@@ -44,15 +45,16 @@ parser.add_argument("--baseline", default=False, action="store_true",
 
 # Transfer learning
 parser.add_argument("--network", default="Conv_4_FC_3",
-                    help="Autoencoder network type. (default=Conv_4_FC_3). Also, you can try training from scratch using VoxResNet and AllConvNet3D")
-parser.add_argument("--transfer_learning_autoencoder", default=True, type=bool,
-                    help="If do transfer learning using autoencoder, the learnt weights will be transferred. Should be exclusive with net_work")
-parser.add_argument("--diagnoses", default=["sMCI", "pMCI"], type=str,
-                    help="Labels based on binary classification")
+                    help="Architecture of the network.")
+parser.add_argument("--transfer_learning_autoencoder", default=False, action="store_true",
+                    help="If do transfer learning using autoencoder, the learnt weights will be transferred.")
+parser.add_argument("--transfer_learning_path", default=None,
+                    help='When a path to an experiment is given, will load the autoencoder / CNN weights.')
 
 # Training arguments
 parser.add_argument("--num_cnn", default=36, type=int,
-                    help="How many CNNs we want to train in a patch-wise way. By default, we train each patch from all subjects for one CNN")
+                    help="How many CNNs we want to train in a patch-wise way. "
+                         "By default, we train each patch from all subjects for one CNN")
 parser.add_argument("--epochs", default=1, type=int,
                     help="Epochs through the data. (default=20)")
 parser.add_argument("-lr", "--learning_rate", default=1e-3, type=float,
@@ -83,6 +85,8 @@ parser.add_argument('--gpu', default=False, action='store_true',
 def main(options):
 
     model = create_model(options.network, options.gpu)
+    init_state = copy.deepcopy(model.state_dict())
+    transformations = transforms.Compose([MinMaxNormalization()])
 
     if options.split is None:
         fold_iterator = range(options.n_splits)
@@ -100,33 +104,22 @@ def main(options):
 
             print("Running for the %d -th fold" % fi)
             print("Train the model from 0 epoch")
-
-            if options.transfer_learning_autoencoder:
-                print('Train the model with the weights from a pre-trained model by autoencoder!')
-                if sorted(options.diagnoses) == ['AD', 'CN']:
-                    model, saved_epoch = load_model_after_ae(model, os.path.join(options.output_dir, 'best_model_dir',
-                                                                                 "fold_" + str(fi),
-                                                                                 'ConvAutoencoder', 'fine_tune',
-                                                                                 'Encoder'),
-                                                             filename='model_best_encoder.pth.tar')
-                    print("The AE was saved at %s -th epoch" % str(saved_epoch))
+            if options.transfer_learning_path is not None:
+                if options.transfer_learning_autoencoder:
+                    print('Train the model with the weights from a pre-trained autoencoder.')
+                    model, _ = load_model_after_ae(model, os.path.join(options.transfer_learning_path, 'best_model_dir',
+                                                                       "fold_" + str(fi), 'ConvAutoencoder',
+                                                                       'fine_tune', 'Encoder'),
+                                                   filename='model_best_encoder.pth.tar')
                 else:
-                    if not os.path.exists(os.path.join(options.output_dir, 'best_model_dir', "fold_" + str(fi),
-                                                       'CNN_source_task', 'best_acc')):
-                        raise Exception(
-                            "To make sure that you have manually moved the output folder of the source task to CNN_source_task!")
-                    ## For other taskes, we can always do transfer learning from the task AD vs CN
-                    model, saved_epoch = load_model_after_cnn(model, os.path.join(options.output_dir, 'best_model_dir',
-                                                                                  "fold_" + str(fi),
-                                                                                  'CNN_source_task', 'best_acc'),
-                                                              filename='model_best.pth.tar')
-                    print("The CNN was saved at %s -th epoch" % str(saved_epoch))
-
-            # the inital model weight and bias
-            init_state = copy.deepcopy(model.state_dict())
-
-            # need to normalized the value to [0, 1]
-            transformations = transforms.Compose([MinMaxNormalization()])
+                    print('Train the model with the weights from a pre-trained CNN.')
+                    model, _ = load_model_after_cnn(model,
+                                                    os.path.join(options.transfer_learning_path, 'best_model_dir',
+                                                                 "fold_" + str(fi), 'CNN', 'best_acc'),
+                                                    filename='model_best.pth.tar')
+            else:
+                print('The model is trained from scratch.')
+                model.load_state_dict(init_state)
 
             data_train = MRIDataset_patch_by_index(options.caps_directory, training_tsv, options.patch_size,
                                                    options.patch_stride, i, transformations=transformations)
@@ -155,7 +148,6 @@ def main(options):
             scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.995)
 
             global_step = 0
-            model.load_state_dict(init_state)
 
             # Define loss and optimizer
             loss = torch.nn.CrossEntropyLoss()
@@ -191,13 +183,13 @@ def main(options):
 
                 # calculate the training accuracy based on all the training data
                 train_subject_all, y_ground_train_all, y_hat_train_all, train_proba_all, acc_mean_train_all, _, loss_batch_mean_train_all = train(model, train_loader, options.gpu, loss, optimizer, writer_train_all_data, epoch, fi, model_mode='valid', global_step=global_step)
-                print("For training, subject level balanced accuracy is %f at the end of epoch %d" % (
-                acc_mean_train_all, epoch))
+                print("For training, subject level balanced accuracy is %f at the end of epoch %d"
+                      % (acc_mean_train_all, epoch))
 
                 # at then end of each epoch, we validate one time for the model with the validation data
                 valid_subject, y_ground_valid, y_hat_valid, valide_proba, acc_mean_valid, global_step, loss_batch_mean_valid = train(model, valid_loader, options.gpu, loss, optimizer, writer_valid, epoch, fi, model_mode='valid', global_step=global_step)
-                print("For validation, subject level balanced accuracy is %f at the end of epoch %d" % (
-                acc_mean_valid, epoch))
+                print("For validation, subject level balanced accuracy is %f at the end of epoch %d"
+                      % (acc_mean_valid, epoch))
 
                 # update the learing rate
                 if epoch % 20 == 0 and epoch != 0:
