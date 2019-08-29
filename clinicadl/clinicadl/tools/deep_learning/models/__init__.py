a19904a2f4ac18d4353fc58a98ad34482b8cf0d7
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2019-08-29 10:19:38 +0200

    Rename model

--- clinicadl/clinicadl/tools/deep_learning/models/__init__.py
+++ clinicadl/clinicadl/tools/deep_learning/models/__init__.py
@@ -1,5 +1,5 @@
 from .subject_level import Conv5_FC3, Conv5_FC3_mni
-from .slice_level import Conv_4_FC_3
+from .patch_level import Conv_4_FC_3
 from .autoencoder import AutoEncoder, initialize_other_autoencoder, transfer_learning
 from .iotools import load_model, load_optimizer
 
