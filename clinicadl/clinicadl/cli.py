ebe7e6e0f09f356cfc481b2a0214eaf2733b3316
Mauricio DIAZ <mauricio.diaz@inria.fr>
2019-09-10 16:44:45 +0200

    Add subparsers for preprocessing and extract in cmd line.

--- clinicadl/clinicadl/cli.py
+++ clinicadl/clinicadl/cli.py
@@ -11,7 +11,7 @@ def preprocessing_t1w_func(args):
             args.working_directory)
     wf.run(plugin='MultiProc', plugin_args={'n_procs': 8})
 
-def postprocessing_t1w_func(args):
+def extract_data_func(args):
     wf = postprocessing_t1w(args.caps_directory, 
             args.tsv_file,
             args.patch.size,
@@ -23,52 +23,75 @@ def postprocessing_t1w_func(args):
     wf.run(plugin='MultiProc', plugin_args={'n_procs': 8})
 
 def parse_command_line():
-    parser = argparse.ArgumentParser(description='Clinica Deep Learning.')
+    parser = argparse.ArgumentParser(prog='clinicadl', 
+            description='Clinica Deep Learning.')
 
     subparsers = parser.add_subparsers(dest='cmd', help='subcommands')
 
     subparsers.required = True
 
     # Preprocessing 1
-    preprocessing1 = subparsers.add_parser('preprocessing',
+    # preprocessing_parser: get command line arguments and options for
+    # preprocessing
+
+    preprocessing_parser = subparsers.add_parser('preprocessing',
             help='Prepare data for training')
-    preprocessing1.add_argument('-bd', '--bids_directory',
-            help='Data using BIDS structure.')
-    preprocessing1.add_argument('-cd', '--caps_directory',
-            help='Data using CAPS structure.')
-    preprocessing1.add_argument('-tsv', '--tsv_file',
-            help='tsv file with sujets/sessions to process.')
-    preprocessing1.add_argument('-rt', '--ref_template',
-            help='Template reference.')
-    preprocessing1.add_argument('-wd', '--working_directory', default=None,
-            help='Working directory to save temporary file.')
+    preprocessing_parser.add_argument('bids_directory',
+            help='Data using BIDS structure.',
+            default=None)
+    preprocessing_parser.add_argument('caps_directory',
+            help='Data using CAPS structure.',
+            default=None)
+    preprocessing_parser.add_argument('tsv_file',
+            help='tsv file with sujets/sessions to process.',
+            default=None)
+    preprocessing_parser.add_argument('ref_template',
+            help='Template reference.',
+            default=None)
+    preprocessing_parser.add_argument('working_directory',
+            help='Working directory to save temporary file.',
+            default=None)
+    preprocessing_parser.add_argument('-np', '--nproc',
+            help='Number of cores used for processing'
+            type=int, default=2)
 
 
-    preprocessing1.set_defaults(func=preprocessing_t1w_func)
+    preprocessing_parser.set_defaults(func=preprocessing_t1w_func)
 
-    # Preprocessing 2 - Create slices and patches
-    
-    preprocessing2 = subparsers.add_parser('createdata',
+    # Preprocessing 2 - Extract data: slices or patches
+    # extract_parser: get command line argument and options
+
+    extract_parser = subparsers.add_parser('extract',
             help='Create data (slices or patches) for training')
-    preprocessing2.add_argument('-cd', '--caps_directory',
-            help='Data using CAPS structure.')
-    preprocessing2.add_argument('-tsv', '--tsv_file',
-            help='tsv file with sujets/sessions to process.')
-    preprocessing2.add_argument('-wd', '--working_directory', default=None,
-            help='Working directory to save temporary file.')
-    preprocessing2.add_argument('-psz', '--patch_size',
-            help='Patch size')
-    preprocessing2.add_argument('-ssz', '--stride_size',
-            help='Stride size')
-    preprocessing2.add_argument('-ex', '--extract_method',
-            help='Method used to extact features: slice or patch')
-    preprocessing2.add_argument('-sd', '--slice_direction',
-            help='Slice direction')
-    preprocessing2.add_argument('-sm', '--slice_mode',
-            help='Slice mode')
+    extract_parser.add_argument('caps_directory',
+            help='Data using CAPS structure.',
+            default=None)
+    extract_parser.add_argument('tsv_file',
+            help='tsv file with sujets/sessions to process.',
+            default=None)
+    extract_parser.add_argument('working_directory',
+            help='Working directory to save temporary file.',
+            default=None)
+    extract_parser.add_argument('extract_method',
+            help='Method used to extract features: slice or patch',
+            choices=['slice', 'patch'], default=None)
+    extract_parser.add_argument('-psz', '--patch_size',
+            help='Patch size e.g: --patch_size 50',
+            type=int, default=50)
+    extract_parser.add_argument('-ssz', '--stride_size',
+            help='Stride size  e.g.: --stride_size 50',
+            type=int, default=50)
+    extract_parser.add_argument('-sd', '--slice_direction',
+            help='Slice direction',
+            type=int, default=0)
+    extract_parser.add_argument('-sm', '--slice_mode',
+            help='Slice mode',
+            choices=['original', 'rgb'], default='rgb')
+    extract_parser.add_argument('-np', '--nproc',
+            help='Number of cores used for processing'
+            type=int, default=2)
     
-    
-    preprocessing2.set_defaults(func=postprocessing_t1w_func)
+    extract_parser.set_defaults(func=extract_data_func)
    
     
     args = parser.parse_args()
