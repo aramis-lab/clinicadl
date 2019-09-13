82db222c7bfa49e006f2f08fcd0ab192ebd11390
Mauricio DIAZ <mauricio.diaz@inria.fr>
2019-09-13 11:44:59 +0200

    Add experiment 2 script for train AE

---
+++ clinicadl/scripts/subject_level/autoencoder/experiment-2.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --time=2-00:00:00
+#SBATCH --mem=17G
+#SBATCH --cpus-per-task=6
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --chdir=.
+#SBATCH --output=outputs/pytorch_job_%j.out
+#SBATCH --error=outputs/pytorch_job_%j.err
+#SBATCH --job-name=3DAE
+#SBATCH --gres=gpu:1
+
+#export http_proxy=http://10.10.2.1:8123
+#export https_proxy=http://10.10.2.1:8123
+
+# Experiment training autoencoder
+module load clinica.all
+eval "$(conda shell.bash hook)"
+conda activate clinica_dl_pre_py36
+
+# Network structure
+NETWORK="Conv5_FC3"
+COHORT="ADNI"
+DATE="reproducibility_results"
+CAPS_EXT="_skull_stripping"
+
+# Input arguments to clinicadl
+CAPS_DIR="/network/lustre/dtlake01/aramis/users/clinica/CLINICA_datasets/CAPS/Frontiers_DL/ADNI_rerun$CAPS_EXT"
+TSV_PATH="/network/lustre/dtlake01/aramis/users/mauricio.diazmelo/code/AD-DL/data/$COHORT/lists_by_diagnosis/train"
+OUTPUT_DIR="/network/lustre/iss01/home/mauricio.diazmelo/ADNI_rerun/results/$DATE/"
+
+# Computation ressources
+NUM_PROCESSORS=8
+
+# Dataset Management
+PREPROCESSING='mnisjullstrip'
+DIAGNOSES="AD CN MCI"
+SPLITS=5
+SPLIT=$1
+
+# Training arguments
+EPOCHS=50
+RUNS=1
+BATCH=6
+ACCUMULATION=2
+EVALUATION=20
+LR=1e-4
+GREEDY_LEARNING=0
+SIGMOID=0
+NORMALIZATION=1
+PATIENCE=50
+
+
+
+NAME="model-${NETWORK}_preprocessing-${PREPROCESSING}_task-autoencoder_baseline-${BASELINE}_norm-${NORMALIZATION}"
+
+if [ $SPLITS > 0 ]; then
+echo "Use of $SPLITS-fold cross validation, split $SPLIT"
+NAME="${NAME}_splits-${SPLITS}"
+fi
+
+echo $NAME
+
+# Run clinicadl
+clinicadl train \
+  subject \
+  $S_DIR \
+  $TSV_PATH \
+  $OUTPUT_DIR$NAME \
+  $NETWORK \
+  --train_autoencoder \
+  --use_gpu \
+  --nproc $NUM_PROCESSORS \
+  --batch_size $BATCH \
+  --evaluation_steps $EVALUATION \
+  --preprocessing $PREPROCESSING \
+  --diagnoses $DIAGNOSES \
+  --baseline \
+  --minmaxnormalization \
+  --n_splits $SPLITS \
+  --split $SPLIT \
+  --accumulation_steps $ACCUMULATION \
+  --epochs $EPOCHS \
+  --learning_rate $LR \
+  --patience $PATIENCE
