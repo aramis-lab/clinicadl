4a970e4e10a44fecd8021dd10e954dce78ca2907
Mauricio DIAZ <mauricio.diaz@inria.fr>
2019-09-15 15:35:17 +0200

    Add example scripts to launch experiments in a SLURM-managed cluster

--- clinicadl/scripts/subject_level/CNN/experiment-2.sh
+++ clinicadl/scripts/subject_level/CNN/experiment-2.sh
@@ -3,8 +3,9 @@
 #SBATCH --time=20:00:00
 #SBATCH --mem=30G
 #SBATCH --cpus-per-task=10
-#SBATCH --threads-per-core=1        # on réserve des coeurs physiques et non logiques
+#SBATCH --threads-per-core=1
 #SBATCH --ntasks=1
+#SBATCH --nodes=1
 #SBATCH --chdir=.
 #SBATCH --output=pytorch_job_%j.out
 #SBATCH --error=pytorch_job_%j.err
@@ -14,7 +15,7 @@
 #export http_proxy=http://10.10.2.1:8123
 #export https_proxy=http://10.10.2.1:8123
 
-# Experiment training autoencoder
+# Experiment taining CNN
 eval "$(conda shell.bash hook)"
 conda activate clinicadl_env_py37
 
@@ -26,14 +27,15 @@ DATE="reproducibility_results"
 # Input arguments to clinicadl
 CAPS_DIR="$SCRATCH/../commun/datasets/ADNI_rerun"
 TSV_PATH="$HOME/code/AD-DL/data/$COHORT/lists_by_diagnosis/train"
-OUTPUT_DIR="$SCRATCH/results/$DATE/"
+OUTPUT_DIR="$SCRATCH/results_subject/$DATE"
 
 # Computation ressources
 NUM_PROCESSORS=8
 
 # Dataset Management
 PREPROCESSING='linear'
-DIAGNOSES="AD CN MCI"
+BASELINE=1
+TASK='AD CN'
 SPLITS=5
 SPLIT=$1
 
@@ -42,15 +44,25 @@ EPOCHS=50
 BATCH=6
 ACCUMULATION=2
 EVALUATION=20
+SAMPLER="random"
 LR=1e-4
-GREEDY_LEARNING=0
-SIGMOID=0
 NORMALIZATION=1
-PATIENCE=50
+PATIENCE=5
+TOLERANCE=0
 
+OPTIONS=""
 
+TASK_NAME="${TASK// /_}"
 
-NAME="model-${NETWORK}_preprocessing-${PREPROCESSING}_task-autoencoder_baseline-${BASELINE}_norm-${NORMALIZATION}"
+if [ $BASELINE = 1 ]; then
+  echo "using only baseline data"
+  TASK_NAME="${TASK_NAME}_baseline"
+  OPTIONS="$OPTIONS --baseline"
+  PATIENCE=10
+fi
+echo $TASK_NAME
+
+NAME="model-${NETWORK}_preprocessing-${PREPROCESSING}_task-${TASK_NAME}_norm-${NORMALIZATION}"
 
 if [ $SPLITS > 0 ]; then
 echo "Use of $SPLITS-fold cross validation, split $SPLIT"
@@ -66,18 +78,18 @@ clinicadl train \
   $TSV_PATH \
   $OUTPUT_DIR$NAME \
   $NETWORK \
-  --train_autoencoder \
+  --diagnoses $TASK \
   --use_gpu \
   --nproc $NUM_PROCESSORS \
   --batch_size $BATCH \
   --evaluation_steps $EVALUATION \
   --preprocessing $PREPROCESSING \
-  --diagnoses $DIAGNOSES \
   --baseline \
-  --minmaxnormalization \
   --n_splits $SPLITS \
+  --minmaxnormalization \
   --split $SPLIT \
   --accumulation_steps $ACCUMULATION \
   --epochs $EPOCHS \
+  --sampler $SAMPLER \
   --learning_rate $LR \
   --patience $PATIENCE
