a11d0f2e2545fe6431c23ae8415a2377dc9f5e4c
Mauricio DIAZ <mauricio.diaz@inria.fr>
2019-09-15 23:11:45 +0200

    Fix variable names in autoencoder for patch-level

--- clinicadl/clinicadl/patch_level/train_autoencoder.py
+++ clinicadl/clinicadl/patch_level/train_autoencoder.py
@@ -9,146 +9,112 @@ import torchvision.transforms as transforms
 from .utils import MRIDataset_patch_hippocampus, MRIDataset_patch
 from .utils import stacked_ae_learning, visualize_ae
 
-from tools.deep_learning import commandline_to_json, create_model
-from tools.deep_learning.data import load_data, MinMaxNormalization
+from ..tools.deep_learning.iotools import Parameters
+from ..itools.deep_learning import commandline_to_json, create_model
+from ..tools.deep_learning.data import load_data, MinMaxNormalization
 
-__author__ = "Junhao Wen"
+__author__ = "Junhao Wen, Elina Thibeau-Sutre, Mauricio Diaz"
 __copyright__ = "Copyright 2018 The Aramis Lab Team"
-__credits__ = ["Junhao Wen"]
+__credits__ = ["Junhao Wen", "Elina Thibeau-Sutre", "Mauricio Diaz"]
 __license__ = "See LICENSE.txt file"
 __version__ = "0.1.0"
-__maintainer__ = "Junhao Wen"
-__email__ = "junhao.wen89@gmail.com"
+__maintainer__ = "Junhao Wen, Elina Thibeau-Sutre, Mauricio Diaz"
+__email__ = "junhao.wen89@gmail.com, mauricio.diaz@inria.fr"
 __status__ = "Development"
 
 parser = argparse.ArgumentParser(description="Argparser for 3D convolutional autoencoder, the AE will be reconstructed based on the CNN that you choose")
 
 # Mandatory arguments
-parser.add_argument("caps_directory", type=str,
-                    help="Path to the caps of image processing pipeline of DL")
-parser.add_argument("diagnosis_tsv_path", type=str,
-                    help="Path to tsv file of the population based on the diagnosis tsv files."
-                         "To note, the column name should be participant_id, session_id and diagnosis.")
-parser.add_argument("output_dir", type=str,
-                    help="Path to store the classification outputs and the tsv files containing the performances.")
-
-# Data management
-parser.add_argument("--diagnoses", default=["AD", "CN", "MCI"], type=str, nargs="+",
-                    help="Take all the subjects possible for autoencoder training")
-parser.add_argument("--patch_size", default=50, type=int,
-                    help="The patch size extracted from the MRI")
-parser.add_argument("--patch_stride", default=50, type=int,
-                    help="The stride for the patch extract window from the MRI")
-parser.add_argument("--baseline", default=False, action="store_true",
-                    help="Use only baseline data instead of all scans available")
-parser.add_argument('--hippocampus_roi', default=False, action='store_true',
-                    help="If train the model using only hippocampus ROI")
-parser.add_argument('--prepare_dl', default=False, action="store_true",
-                    help="If True the outputs of preprocessing prepare_dl are used, else the whole MRI is loaded.")
-
-# Cross-validation
-parser.add_argument("--n_splits", default=5, type=int,
-                    help="Define the cross validation, by default, we use 5-fold.")
-parser.add_argument("--split", default=None, type=int,
-                    help="Default behaviour will run all splits, else only the splits specified will be run.")
 
 # Training arguments
-parser.add_argument("--network", default="Conv4_FC3",
-                    help="Architecture of the network on which the autoencoder is based.")
-parser.add_argument("--epochs", default=1, type=int,
-                    help="Epochs through the data.")
-parser.add_argument("--learning_rate", "-lr", default=1e-3, type=float,
-                    help="Learning rate of the optimization. (default=0.01)")
-parser.add_argument("--optimizer", default="Adam", choices=["SGD", "Adadelta", "Adam"],
-                    help="Optimizer of choice for training. (default=Adam)")
 
-# visualization
-parser.add_argument("--visualization", default=False, action='store_true',
-                    help='Chooses if visualization is done on AE pretraining')
+def train_autoencoder_patch(params):
 
-# Computational issues
-parser.add_argument("--num_workers", default=0, type=int,
-                    help='the number of batch being loaded in parallel')
-parser.add_argument("--batch_size", default=2, type=int,
-                    help="Batch size for training. (default=1)")
-parser.add_argument("--gpu", default=False, action='store_true',
-                    help='Uses gpu instead of cpu if cuda is available')
-
-
-def main(options):
-
-    model = create_model(options.network, options.gpu)
+    model = create_model(params.network, params.gpu)
     init_state = copy.deepcopy(model.state_dict())
     transformations = transforms.Compose([MinMaxNormalization()])
 
-    if options.split is None:
-        fold_iterator = range(options.n_splits)
+    if params.split is None:
+        fold_iterator = range(params.n_splits)
     else:
-        fold_iterator = [options.split]
+        fold_iterator = [params.split]
 
     for fi in fold_iterator:
 
-        training_tsv, valid_tsv = load_data(options.diagnosis_tsv_path, options.diagnoses, fi,
-                                            n_splits=options.n_splits, baseline=options.baseline)
+        training_tsv, valid_tsv = load_data(params.tsv_path,
+                params.diagnoses, 
+                fi,
+                n_splits=params.n_splits,
+                baseline=params.baseline)
 
         print("Running for the %d-th fold" % fi)
 
-        if options.hippocampus_roi:
+        if params.hippocampus_roi:
             print("Only using hippocampus ROI")
 
-            data_train = MRIDataset_patch_hippocampus(options.caps_directory, training_tsv,
-                                                      transformations=transformations)
-            data_valid = MRIDataset_patch_hippocampus(options.caps_directory, valid_tsv,
-                                                      transformations=transformations)
+            data_train = MRIDataset_patch_hippocampus(params.input_dir,
+                    training_tsv,
+                    transformations=transformations)
+            data_valid = MRIDataset_patch_hippocampus(params.input_dir,
+                    valid_tsv,
+                    transformations=transformations)
 
         else:
-            data_train = MRIDataset_patch(options.caps_directory, training_tsv, options.patch_size,
-                                          options.patch_stride, transformations=transformations,
-                                          prepare_dl=options.prepare_dl)
-            data_valid = MRIDataset_patch(options.caps_directory, valid_tsv, options.patch_size,
-                                          options.patch_stride, transformations=transformations,
-                                          prepare_dl=options.prepare_dl)
+            data_train = MRIDataset_patch(params.input_dir, 
+                    training_tsv, 
+                    params.patch_size,
+                    params.patch_stride,
+                    transformations=transformations,
+                    prepare_dl=params.prepare_dl)
+            data_valid = MRIDataset_patch(params.input_dir, 
+                    valid_tsv,
+                    params.patch_size,
+                    params.patch_stride,
+                    transformations=transformations,
+                    prepare_dl=params.prepare_dl)
 
         # Use argument load to distinguish training and testing
         train_loader = DataLoader(data_train,
-                                  batch_size=options.batch_size,
-                                  shuffle=True,
-                                  num_workers=options.num_workers,
-                                  pin_memory=True
-                                  )
+                batch_size=params.batch_size,
+                shuffle=True,
+                num_workers=params.num_workers,
+                pin_memory=True
+                )
 
         valid_loader = DataLoader(data_valid,
-                                  batch_size=options.batch_size,
-                                  shuffle=False,
-                                  num_workers=options.num_workers,
-                                  pin_memory=True
-                                  )
+                batch_size=params.batch_size,
+                shuffle=False,
+                num_workers=params.num_workers,
+                pin_memory=True
+                )
 
         model.load_state_dict(init_state)
 
         criterion = torch.nn.MSELoss()
-        writer_train = SummaryWriter(log_dir=(os.path.join(options.output_dir, "log_dir", "fold_" + str(fi),
-                                                           "ConvAutoencoder", "train")))
-        writer_valid = SummaryWriter(log_dir=(os.path.join(options.output_dir, "log_dir", "fold_" + str(fi),
-                                                           "ConvAutoencoder", "valid")))
+        writer_train = SummaryWriter(log_dir=(os.path.join(params.output_dir, 
+            "log_dir", 
+            "fold_" + str(fi),
+            "ConvAutoencoder", "train")))
+        writer_valid = SummaryWriter(log_dir=(os.path.join(params.output_dir, 
+            "log_dir", 
+            "fold_" + str(fi),
+            "ConvAutoencoder", "valid")))
 
-        model, best_autodecoder = stacked_ae_learning(model, train_loader, valid_loader, criterion, writer_train,
-                                                      writer_valid, options, fi)
+        model, best_autodecoder = stacked_ae_learning(model,
+                train_loader,
+                valid_loader,
+                criterion,
+                writer_train,
+                writer_valid, 
+                params, 
+                fi)
 
-        if options.visualization:
+        if params.visualization:
             example_batch = data_train[0]['image'].unsqueeze(0)
-            if options.gpu:
+            if params.gpu:
                 example_batch = example_batch.cuda()
-            visualize_ae(best_autodecoder, example_batch, os.path.join(options.output_dir, "visualize", "fold_%i" % fi))
+            visualize_ae(best_autodecoder, example_batch, 
+                    os.path.join(params.output_dir, "visualize", "fold_%i" % fi))
 
         del best_autodecoder, train_loader, valid_loader
         torch.cuda.empty_cache()
-
-
-if __name__ == "__main__":
-    commandline = parser.parse_known_args()
-    commandline_to_json(commandline, "ConvAutoencoder")
-    options = commandline[0]
-    if commandline[1]:
-        raise Exception("unknown arguments: %s" % parser.parse_known_args()[1])
-    main(options)
