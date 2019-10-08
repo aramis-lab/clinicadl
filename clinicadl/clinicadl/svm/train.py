5b876fccb9f2ced8f8302873974f0278caa63c3b
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2019-10-08 16:37:07 +0200

    Add svm
    Former-commit-id: 2dfc2c4dcf89a00bc70ea67d8d3a7c983b036734

--- clinicadl/clinicadl/svm/train.py
+++ clinicadl/clinicadl/svm/train.py
@@ -1,9 +1,7 @@
 import argparse
-from os import path
-import numpy as np
-import pickle
-from clinica.pipelines.machine_learning.input import CAPSVoxelBasedInput
-from clinica.pipelines.machine_learning.ml_workflows import VB_RepHoldOut_DualSVM
+from .classification_utils import extract_indices_from_5_fold
+from .model import VB_KFold_DualSVM
+from ..tools.deep_learning import commandline_to_json
 
 parser = argparse.ArgumentParser(description="Argparser for Pytorch 3D CNN")
 
@@ -23,77 +21,30 @@ parser.add_argument("--baseline", default=False, action="store_true",
                     help="Use only baseline data instead of all scans available")
 parser.add_argument("--n_splits", type=int, default=5,
                     help="Load the data of the corresponding k-fold CV")
-parser.add_argument("--split", type=int, default=None,
-                    help="Split to perform. Default behaviour will perform all splits.")
 parser.add_argument("--group_id", type=str, default='ADNIbl',
                     help="Select the group to load the correspondin data in CAPS.")
 
 # Computational issues
-parser.add_argument("--split_indices_path", type=str, default=None,
-                    help="If provided, will load fixed split indices for reproducibility issues.")
 parser.add_argument("--num_workers", '-w', default=1, type=int,
                     help='the number of batch being loaded in parallel')
 
 
-if __name__ == "__main__":
-    from .utils import SVMTester, save_data, save_additional_parameters
-    from tools.deep_learning.data import load_data
-    from tools.deep_learning import commandline_to_json
+def main(options):
 
+    splits_indices, diagnosis_tsv = extract_indices_from_5_fold(options.diagnosis_path, options.n_splits,
+                                                                options.output_dir, baseline=options.baseline,
+                                                                diagnoses_list=options.diagnoses)
+
+    wf = VB_KFold_DualSVM(options.input_dir, diagnosis_tsv, options.group_id, "T1", options.output_dir,
+                          n_threads=options.num_workers, n_folds=options.n_splits, splits_indices=splits_indices)
+
+    wf.run()
+
+
+if __name__ == "__main__":
     commandline = parser.parse_known_args()
     options = commandline[0]
+    commandline_to_json(options, 'SVM')
     if commandline[1]:
-        print("Unknown arguments: %s" % parser.parse_known_args()[1])
-    commandline_to_json(commandline, 'SVM')
-
-    if options.split is None:
-        fold_iterator = np.arange(options.n_splits)
-    else:
-        fold_iterator = [options.split]
-
-    for split in fold_iterator:
-        fold_dir = path.join(options.output_dir, 'fold_' + str(split))
-
-        # Load and generate tsv files
-        training_df, valid_df = load_data(options.diagnosis_path, options.diagnoses, split,
-                                          options.n_splits, options.baseline)
-
-        train_path = save_data(training_df, fold_dir, 'train')
-        valid_path = save_data(valid_df, fold_dir, 'validation' + '_' + '_'.join(options.diagnoses))
-
-        # Load split indices
-        print("Loading pre-generated indices")
-        if options.split_indices_path is not None:
-            with open(path.join(options.split_indices_path, 'fold_%i' % split, 'indices.pkl'), 'rb') as file:
-                split_indices = pickle.load(file)
-        else:
-            split_indices = None
-
-        # Run workflow of (Samper-Gonzalez et al., 2018)
-        wf = VB_RepHoldOut_DualSVM(options.input_dir,
-                                   path.join(train_path, 'sessions.tsv'),
-                                   path.join(train_path, 'diagnoses.tsv'),
-                                   options.group_id, "T1", fold_dir,
-                                   n_threads=options.num_workers, fwhm=8, mask_zeros=True, splits_indices=split_indices)
-        wf.run()
-        save_additional_parameters(wf, train_path)
-
-        # Test on training and validation sets
-        train_dataset = CAPSVoxelBasedInput(options.input_dir,
-                                            path.join(train_path, 'sessions.tsv'),
-                                            path.join(train_path, 'diagnoses.tsv'),
-                                            options.group_id, "T1",
-                                            fwhm=8, mask_zeros=False)
-        valid_dataset = CAPSVoxelBasedInput(options.input_dir,
-                                            path.join(valid_path, 'sessions.tsv'),
-                                            path.join(valid_path, 'diagnoses.tsv'),
-                                            options.group_id, "T1",
-                                            fwhm=8, mask_zeros=False)
-
-        svm_tester = SVMTester(fold_dir)
-
-        valid_path = path.join(fold_dir, 'evaluation', 'validation' + '_' + '_'.join(options.diagnoses))
-        svm_tester.test_and_save(valid_dataset, valid_path)
-
-        train_path = path.join(fold_dir, 'evaluation', 'train')
-        svm_tester.test_and_save(train_dataset, train_path)
+        raise Exception("unknown arguments: %s" % (parser.parse_known_args()[1]))
+    main(options)
