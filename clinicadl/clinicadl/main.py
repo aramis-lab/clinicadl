630ac3ac78db9830cf439269bb94b72b3524b6bd
Mauricio DIAZ <mauricio.diaz@inria.fr>
2019-10-16 22:45:32 +0200

    Add cli refactoring to run initial cli tests
    Former-commit-id: 26d6f62b593642db11cf0b4438920d71212e5419

--- clinicadl/clinicadl/main.py
+++ clinicadl/clinicadl/main.py
@@ -1,8 +1,22 @@
 from . import cli
+from clinicadl.tools.deep_learning import commandline_to_json
 
 def main():
-    args = cli.parse_command_line()
+
+    parser = cli.parse_command_line()
+    args = parser.parse_args()
+
+    commandline = parser.parse_known_args()
+
+    if args.train_autoencoder:
+      model_type = 'autoencoder'
+    else:
+      model_type = 'cnn'
+
+    commandline_to_json(commandline, model_type)
+
     args.func(args)
+
 
 if __name__ == '__main__':
     main()
