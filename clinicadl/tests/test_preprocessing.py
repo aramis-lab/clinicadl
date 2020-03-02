8643ec1d9aef4b112ac94f72652d2cbd7bb8aab9
Mauricio DIAZ <mauricio.diaz@inria.fr>
2020-03-02 21:40:41 +0100

    Add preprocessing T1_Linear tests
    Former-commit-id: c10fd382a08a6169d39520b488323a1451d1325e

---
+++ clinicadl/tests/test_preprocessing.py
@@ -0,0 +1,13 @@
+from  clinicadl.preprocessing.T1_lineal import preprocessing_t1w
+
+bids_dir = 
+caps_dir =
+tsv_file =
+working_dir = 
+nproc = 2
+
+wf = preprocessing_t1w(args.bids_directory,
+        args.caps_dir,
+        args.tsv_file,
+        args.working_directory)
+wf.run(plugin='MultiProc', plugin_args={'n_procs': nproc})
