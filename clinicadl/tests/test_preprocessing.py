3b020ee32f8400cc3d2557600de71bc0cbd9f8ba
Mauricio DIAZ <mauricio.diaz@inria.fr>
2020-05-15 16:50:07 +0200

    Add vague structure for preprocessing unit test
    Former-commit-id: 0c2d915b1e3f5dc81be474cf5e174b3b0126a021

--- clinicadl/tests/test_preprocessing.py
+++ clinicadl/tests/test_preprocessing.py
@@ -1,15 +1,18 @@
 # coding: utf8
 
-from clinicadl.preprocessing.T1_lineal import preprocessing_t1w
+def test_preprocessing_t1w():
+    from clinicadl.preprocessing.T1_lineal import preprocessing_t1w
 
-bids_dir =
-caps_dir =
-tsv_file =
-working_dir =
-nproc = 2
+    bids_dir = './test/bids/'
+    caps_dir = './test/caps/'
+    tsv_file = './test/subjects.tsv'
+    working_dir = './test/working_dir'
+    nproc = 2
 
-wf = preprocessing_t1w(args.bids_directory,
-        args.caps_dir,
-        args.tsv_file,
-        args.working_directory)
-wf.run(plugin='MultiProc', plugin_args={'n_procs': nproc})
+    wf = preprocessing_t1w(args.bids_directory,
+            args.caps_dir,
+            args.tsv_file,
+            args.working_directory)
+    wf.run(plugin='MultiProc', plugin_args={'n_procs': nproc})
+
+    pass
