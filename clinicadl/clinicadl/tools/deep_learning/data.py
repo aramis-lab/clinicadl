93bc93ea46d2a4e2f9ac57ac50472ff9c9a4b071
Mauricio DIAZ <mauricio.diaz@inria.fr>
2020-05-26 18:38:19 +0200

    Fix style issues

--- clinicadl/clinicadl/tools/deep_learning/data.py
+++ clinicadl/clinicadl/tools/deep_learning/data.py
@@ -12,9 +12,9 @@ from scipy.ndimage.filters import gaussian_filter
 # Datasets loaders
 #################################
 FILENAME_TYPE = {
-        'full': '_T1w_space-MNI152NLin2009cSym_res-1x1x1_T1w',
-        'cropped': '_T1w_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_T1w'
-        }
+                 'full': '_T1w_space-MNI152NLin2009cSym_res-1x1x1_T1w',
+                 'cropped': '_T1w_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_T1w'
+                }
 
 
 class MRIDataset(Dataset):
