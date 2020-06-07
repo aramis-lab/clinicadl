07e6af68f494a153e432731b549e43c6ae49c745
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2020-06-07 15:18:14 +0200

    Clean imports
    Former-commit-id: 710d64c28ef0f0cde5f88e8375e75e4f911f6c6b

--- clinicadl/clinicadl/patch_level/train_multiCNN.py
+++ clinicadl/clinicadl/patch_level/train_multiCNN.py
@@ -1,6 +1,5 @@
 # coding: utf8
 
-import argparse
 import copy
 import os
 import torch
@@ -12,10 +11,8 @@ import torchvision.transforms as transforms
 from .utils import load_model_after_ae, load_model_after_cnn
 from .utils import train, test, patch_level_to_tsvs, soft_voting_to_tsvs
 
-from ..tools.deep_learning.iotools import Parameters
 from ..tools.deep_learning import (EarlyStopping,
                                    save_checkpoint,
-                                   commandline_to_json,
                                    create_model,
                                    load_model)
 from ..tools.deep_learning.data import (MinMaxNormalization,
