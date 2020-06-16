c2d96265c72c6ea140d49364352c9c5249cdd55f
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2020-06-16 14:03:02 +0200

    Add discarded_slices argument
    Former-commit-id: 2c9c0dedfd9b0998df5e718ff09ee2baba9b7aff

--- clinicadl/clinicadl/tools/deep_learning/iotools.py
+++ clinicadl/clinicadl/tools/deep_learning/iotools.py
@@ -52,6 +52,7 @@ class Parameters:
             selection_threshold: float = 0.0,
             num_cnn: int = 36,
             mri_plane: int = 0,
+            discarded_slices: int = 20,
             prepare_dl: bool = False,
             visualization: bool = False):
         """
@@ -126,6 +127,7 @@ class Parameters:
         self.hippocampus_roi = hippocampus_roi
         self.num_cnn = num_cnn
         self.mri_plane = mri_plane
+        self.discarded_slices = discarded_slices
         self.prepare_dl = prepare_dl
         self.visualization = visualization
         self.selection_threshold = selection_threshold
