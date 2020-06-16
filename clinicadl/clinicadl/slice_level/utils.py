06a729ac2ee6e63bfc41947010ba46c1f492553b
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2020-06-16 12:51:31 +0200

    Fix type bug due to np array conversion
    Former-commit-id: c77769e3e7e47310981cdc81d39be53cb5d58ce7

--- clinicadl/clinicadl/slice_level/utils.py
+++ clinicadl/clinicadl/slice_level/utils.py
@@ -145,11 +145,11 @@ def test(model, data_loader, use_cuda, loss_func):
 
             # Generate detailed DataFrame
             for idx, sub in enumerate(data['participant_id']):
-                row = [sub, data['session_id'][idx], data['slice_id'][idx].item(),
+                row = [[sub, data['session_id'][idx], data['slice_id'][idx].item(),
                        labels[idx].item(), predicted[idx].item(),
-                       normalized_output[idx, 0].item(), normalized_output[idx, 1].item()]
+                       normalized_output[idx, 0].item(), normalized_output[idx, 1].item()]]
 
-                row_df = pd.DataFrame(np.array(row).reshape(1, -1), columns=columns)
+                row_df = pd.DataFrame(row, columns=columns)
                 results_df = pd.concat([results_df, row_df])
 
             del imgs, labels, output
