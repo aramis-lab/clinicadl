c460d5be7605f715796330f287e8d499c7f190a5
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2020-06-17 10:56:18 +0200

    Change subject --> image
    Former-commit-id: 86097a477ec022fe8847381112f35620379049ff

--- clinicadl/clinicadl/tools/deep_learning/models/__init__.py
+++ clinicadl/clinicadl/tools/deep_learning/models/__init__.py
@@ -1,6 +1,6 @@
 from .autoencoder import AutoEncoder, initialize_other_autoencoder, transfer_learning
 from .iotools import load_model, load_optimizer, save_checkpoint
-from .subject_level import Conv5_FC3, Conv5_FC3_mni
+from .image_level import Conv5_FC3, Conv5_FC3_mni
 from .patch_level import Conv4_FC3
 from .slice_level import resnet18
 
