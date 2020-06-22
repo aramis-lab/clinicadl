fd337c2d66d50a420750a54f958cc1380870f556
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2020-06-22 11:55:19 +0200

    Refactor QC

--- clinicadl/clinicadl/preprocessing/quality_check_dl.py
+++ clinicadl/clinicadl/preprocessing/quality_check_dl.py
@@ -3,14 +3,11 @@
 
 import argparse
 import sys
+from os import path
 
-from quality_check_dl_utils import load_nifti_images
-from model.util import *
-import numpy as np
+from .quality_check_dl_utils import load_nifti_images
 import torch
-import torch.nn as nn
 import pandas as pd
-from torch.autograd import Variable
 
 __author__ = "Junhao Wen"
 __copyright__ = "Copyright 2018 The Aramis Lab Team"
@@ -21,24 +18,17 @@ __maintainer__ = "Junhao Wen"
 __email__ = "junhao.wen89@gmail.com"
 __status__ = "Development"
 
-default_data_dir = os.path.dirname(sys.argv[0])
 
 
 def parse_options():
     parser = argparse.ArgumentParser(description='Apply automated QC',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("--ref", action="store_true", default=False,
-                        help="Use reference images")
-    parser.add_argument("--caps_dir", type=str,
+    parser.add_argument("caps_dir", type=str,
                         help="the caps_directory for the outputs files of preprocessing pipeline")
-    parser.add_argument("--tsv", type=str,
+    parser.add_argument("tsv", type=str,
                         help="the tsv file conatining participant_id and session_id")
-    parser.add_argument("--load", type=str, default=default_data_dir + os.sep + 'python/model_r18/best_tnr_cpu.pth',
-                        help="Load pretrained model (mondatory)")
-    parser.add_argument("--net", choices=['r18', 'r34', 'r50', 'r101', 'r152', 'sq101'],
-                        help="Network type", default='r18')
-    parser.add_argument('--soft_thres', type=float, default=0.975,
+    parser.add_argument('--threshold', type=float, default=0.5,
                         help='The threshold of softmax layer to decide the label')
 
     params = parser.parse_args()
@@ -48,56 +38,41 @@ def parse_options():
 
 if __name__ == '__main__':
     params = parse_options()
-    use_ref = False
 
-    if params.load is None:
-        print("need to provide pre-trained model!")
-        exit(1)
+    # Load QC model
+    script_dir = path.dirname(sys.argv[0])
+    model = torch.load(path.join(script_dir, "model", "resnet18.pth"))
+    model.eval()
 
+    # Load DataFrame
     df = pd.read_csv(params.tsv, sep='\t')
-    if ('diagnosis' != list(df.columns.values)[2]) and ('session_id' != list(df.columns.values)[1]) and (
-                'participant_id' != list(df.columns.values)[0]):
-        raise Exception('the data file is not in the correct format.')
-    img_list = list(df['participant_id'])
-    sess_list = list(df['session_id'])
-    label_list = list(df['diagnosis'])
+    if ('session_id' not in list(df.columns.values)) or ('participant_id' not in list(df.columns.values)):
+        raise Exception("the data file is not in the correct format."
+                        "Columns should include ['participant_id', 'session_id']")
 
-    colmn_name = list(df.columns)
-    colmn_name.extend(['pass_probability', 'qc'])
-    # creat a new df to store the QC results.
-    qc_df = pd.DataFrame(columns=colmn_name)
+    columns = ['participant_id', 'session_id', 'pass_probability', 'pass']
+    qc_df = pd.DataFrame(columns=columns)
+    softmax = torch.nn.Softmax(dim=1)
 
-    for i in range(len(img_list)):
-        img_path = os.path.join(params.caps_dir, 'subjects', img_list[i], sess_list[i], 't1', 'preprocessing_dl',
-                                img_list[i] + '_' + sess_list[i] + '_space-MNI_res-1x1x1_linear_registration.nii.gz')
+    for _, row in df.iterrows():
+        subject = row['participant_id']
+        session = row['session_id']
+        img_path = path.join(params.caps_dir, 'subjects', subject, session, 't1_linear',
+                             '%s_%s_T1w_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_T1w.nii.gz'
+                             % (subject, session))
 
-        print("Quality check for subject: %s_%s" % (img_list[i], sess_list[i]))
         inputs = load_nifti_images(img_path)
-
-        model = get_qc_model(params, use_ref=use_ref)
-        model.train(False)
-
-        # convert inputs into properly formated tensor
-        # with a single batch dimension
         inputs = torch.cat(inputs).unsqueeze_(0)
-        inputs = Variable(inputs)
 
-        softmax = nn.Softmax(dim=1)
         outputs = softmax.forward(model(inputs))
-        # _, preds = torch.max(outputs.data, 1)
-
         pass_prob = outputs.data[0, 1]
 
-        if pass_prob >= params.soft_thres:
-            print("Pass probability is %f !! QC Results for subject: %s_%s" % (pass_prob, img_list[i], sess_list[i]))
-            row = list([img_list[i], sess_list[i], label_list[i], pass_prob.item(), 'Pass'])
+        if pass_prob >= params.threshold:
+            row = [[subject, session, pass_prob.item(), True]]
         else:
-            print("Fail probability is %f !! QC Results for subject: %s_%s" % (pass_prob, img_list[i], sess_list[i]))
-            row = list([img_list[i], sess_list[i], label_list[i], pass_prob.item(), 'Fail'])
+            row = [[subject, session, pass_prob.item(), False]]
 
-        row = np.array(row).reshape(1, len(row))
-        row_df = pd.DataFrame(row, columns=colmn_name)
+        row_df = pd.DataFrame(row, columns=columns)
         qc_df = qc_df.append(row_df)
 
-    qc_df.reset_index(inplace=True, drop=True)
-    qc_df.to_csv(os.path.join(params.caps_dir, 'qc_dl.tsv'), sep='\t', index=False, encoding='utf-8')
+    qc_df.to_csv(params.output_path, sep='\t', index=False)
