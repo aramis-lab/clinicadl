f3b4d51a6593e2508981614b3eb273f37d7b3db3
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2020-07-02 15:01:35 +0200

    Fix style issues

--- clinicadl/clinicadl/train/train_singleCNN_bad_data_split.py
+++ clinicadl/clinicadl/train/train_singleCNN_bad_data_split.py
@@ -30,150 +30,163 @@ def test_cnn(data_loader, subset_name, split, criterion, options):
 
     for selection in ["best_acc", "best_loss"]:
         # load the best trained model during the training
-        model = init_model(options.model, gpu=options.gpu, dropout=options.dropout)
+        model = init_model(
+            options.model,
+            gpu=options.gpu,
+            dropout=options.dropout)
         model, best_epoch = load_model(model, os.path.join(options.output_dir, 'best_model_dir', "fold_%i" % split,
                                                            'CNN', selection),
                                        gpu=options.gpu, filename='model_best.pth.tar')
 
-        results_df, metrics = test(model, data_loader, options.gpu, criterion, options.mode)
-        print("Slice level balanced accuracy is %f" % metrics['balanced_accuracy'])
+        results_df, metrics = test(
+            model, data_loader, options.gpu, criterion, options.mode)
+        print(
+            "Slice level balanced accuracy is %f" %
+            metrics['balanced_accuracy'])
 
-        mode_level_to_tsvs(options.output_dir, results_df, metrics, split, selection, options.mode, dataset=subset_name)
+        mode_level_to_tsvs(
+            options.output_dir,
+            results_df,
+            metrics,
+            split,
+            selection,
+            options.mode,
+            dataset=subset_name)
 
 
 # Train 2D CNN - Slice level network
 # The input MRI's dimension is 169*208*179 after cropping"
 parser = argparse.ArgumentParser(
-        description="Argparser for Pytorch 2D CNN, The input MRI's dimension is 169*208*179 after cropping")
+    description="Argparser for Pytorch 2D CNN, The input MRI's dimension is 169*208*179 after cropping")
 
 # Mandatory argument
 parser.add_argument(
-        "caps_directory",
-        type=str,
-        help="Path to the caps of image processing pipeline of DL")
+    "caps_directory",
+    type=str,
+    help="Path to the caps of image processing pipeline of DL")
 
 parser.add_argument(
-        "tsv_path",
-        type=str,
-        help="Path to tsv file of the population based on the diagnosis tsv files. "
-             "To note, the column name should be participant_id, session_id and diagnosis.")
+    "tsv_path",
+    type=str,
+    help="Path to tsv file of the population based on the diagnosis tsv files. "
+    "To note, the column name should be participant_id, session_id and diagnosis.")
 parser.add_argument(
-        "output_dir",
-        type=str,
-        help="Path to store the classification outputs, and the tsv files containing the performances.")
+    "output_dir",
+    type=str,
+    help="Path to store the classification outputs, and the tsv files containing the performances.")
 
 # Data argument
 parser.add_argument(
-        "--mri_plane",
-        default=0,
-        type=int,
-        help='Which coordinate axis to take for slicing the MRI. 0 is for saggital, 1 is for coronal and 2 is for axial direction, respectively ')
+    "--mri_plane",
+    default=0,
+    type=int,
+    help='Which coordinate axis to take for slicing the MRI. 0 is for saggital, 1 is for coronal and 2 is for axial direction, respectively ')
 
 parser.add_argument(
-        '--baseline',
-        default=False,
-        action="store_true",
-        help="Using baseline scans or all available longitudinal scans for training")
+    '--baseline',
+    default=False,
+    action="store_true",
+    help="Using baseline scans or all available longitudinal scans for training")
 
 # train argument
 parser.add_argument(
-        "--model",
-        default="resnet18",
-        help="Deep network type. Only ResNet was designed for training from scratch.")
+    "--model",
+    default="resnet18",
+    help="Deep network type. Only ResNet was designed for training from scratch.")
 
 parser.add_argument(
-        "--diagnoses",
-        default=["AD", "CN"],
-        type=str,
-        nargs="+",
-        help="Labels for any binary task")
+    "--diagnoses",
+    default=["AD", "CN"],
+    type=str,
+    nargs="+",
+    help="Labels for any binary task")
 
 parser.add_argument(
-        "--learning_rate", "-lr",
-        default=1e-3,
-        type=float,
-        help="Learning rate of the optimization. (default=0.01)")
+    "--learning_rate", "-lr",
+    default=1e-3,
+    type=float,
+    help="Learning rate of the optimization. (default=0.01)")
 
 parser.add_argument(
-        "--n_splits",
-        default=5,
-        type=int,
-        help="Define the cross validation, by default, we use 5-fold.")
+    "--n_splits",
+    default=5,
+    type=int,
+    help="Define the cross validation, by default, we use 5-fold.")
 
 parser.add_argument(
-        "--split",
-        default=None,
-        type=int,
-        help="Define a specific fold in the k-fold, this is very useful to find the optimal model, where you do not want to run your k-fold validation")
+    "--split",
+    default=None,
+    type=int,
+    help="Define a specific fold in the k-fold, this is very useful to find the optimal model, where you do not want to run your k-fold validation")
 
 parser.add_argument(
-        "--epochs",
-        default=1,
-        type=int,
-        help="Epochs through the data. (default=20)")
+    "--epochs",
+    default=1,
+    type=int,
+    help="Epochs through the data. (default=20)")
 
 parser.add_argument(
-        "--batch_size",
-        default=32,
-        type=int,
-        help="Batch size for training. (default=1)")
+    "--batch_size",
+    default=32,
+    type=int,
+    help="Batch size for training. (default=1)")
 
 parser.add_argument(
-        "--optimizer",
-        default="Adam",
-        choices=["SGD", "Adadelta", "Adam"],
-        help="Optimizer of choice for training. (default=Adam)")
+    "--optimizer",
+    default="Adam",
+    choices=["SGD", "Adadelta", "Adam"],
+    help="Optimizer of choice for training. (default=Adam)")
 
 parser.add_argument(
-        "--gpu",
-        default=False,
-        action="store_true",
-        help="If use gpu or cpu. Empty implies cpu usage.")
+    "--gpu",
+    default=False,
+    action="store_true",
+    help="If use gpu or cpu. Empty implies cpu usage.")
 
 parser.add_argument(
-        "--num_workers",
-        default=0,
-        type=int,
-        help='the number of batch being loaded in parallel')
+    "--num_workers",
+    default=0,
+    type=int,
+    help='the number of batch being loaded in parallel')
 
 parser.add_argument(
-        "--weight_decay",
-        default=1e-2,
-        type=float,
-        help='weight decay (default: 1e-4)')
+    "--weight_decay",
+    default=1e-2,
+    type=float,
+    help='weight decay (default: 1e-4)')
 
 parser.add_argument(
-        "--dropout",
-        default=0,
-        type=float,
-        help="Rate of dropout applied to dropout layers."
+    "--dropout",
+    default=0,
+    type=float,
+    help="Rate of dropout applied to dropout layers."
 )
 
 parser.add_argument(
-        '--selection_threshold',
-        default=None,
-        type=float,
-        help='Threshold on the balanced accuracies to compute the subject-level '
-             'performance only based on patches with balanced accuracy > threshold.')
+    '--selection_threshold',
+    default=None,
+    type=float,
+    help='Threshold on the balanced accuracies to compute the subject-level '
+    'performance only based on patches with balanced accuracy > threshold.')
 
 parser.add_argument(
-        '--prepare_dl',
-        default=False,
-        action="store_true",
-        help="If True the outputs of preprocessing prepare_dl are used, else the whole MRI is loaded.")
+    '--prepare_dl',
+    default=False,
+    action="store_true",
+    help="If True the outputs of preprocessing prepare_dl are used, else the whole MRI is loaded.")
 
 # early stopping arguments
 parser.add_argument(
-        "--patience",
-        type=int,
-        default=10,
-        help="tolerated epochs without improving for early stopping.")
+    "--patience",
+    type=int,
+    default=10,
+    help="tolerated epochs without improving for early stopping.")
 
 parser.add_argument(
-        "--tolerance",
-        type=float,
-        default=0,
-        help="Tolerance of magnitude of performance after each epoch.")
+    "--tolerance",
+    type=float,
+    default=0,
+    help="Tolerance of magnitude of performance after each epoch.")
 
 
 def train_CNN_bad_data_split(params):
@@ -182,7 +195,8 @@ def train_CNN_bad_data_split(params):
     print('Do transfer learning with existed model trained on ImageNet!\n')
     print('The chosen network is %s !' % params.model)
 
-    trg_size = (224, 224)  # most of the imagenet pretrained model has this input size
+    # most of the imagenet pretrained model has this input size
+    trg_size = (224, 224)
 
     # All pre-trained models expect input images normalized in the same way,
     # i.e. mini-batches of 3-channel RGB images of shape (3 x H x W), where H
@@ -206,56 +220,59 @@ def train_CNN_bad_data_split(params):
         print("Running for the %d-th fold" % fi)
 
         training_sub_df, valid_sub_df = load_data(
-                params.tsv_path,
-                params.diagnoses,
-                fi,
-                n_splits=params.n_splits,
-                baseline=params.baseline
-                )
+            params.tsv_path,
+            params.diagnoses,
+            fi,
+            n_splits=params.n_splits,
+            baseline=params.baseline
+        )
 
         # split the training + validation by slice
         training_df, valid_df = mix_slices(
-                training_sub_df,
-                valid_sub_df,
-                mri_plane=params.mri_plane
-                )
+            training_sub_df,
+            valid_sub_df,
+            mri_plane=params.mri_plane
+        )
 
         data_train = MRIDataset_slice_mixed(
-                params.caps_directory,
-                training_df,
-                transformations=transformations,
-                mri_plane=params.mri_plane,
-                prepare_dl=params.prepare_dl
-                )
+            params.caps_directory,
+            training_df,
+            transformations=transformations,
+            mri_plane=params.mri_plane,
+            prepare_dl=params.prepare_dl
+        )
 
         data_valid = MRIDataset_slice_mixed(
-                params.caps_directory,
-                valid_df,
-                transformations=transformations,
-                mri_plane=params.mri_plane,
-                prepare_dl=params.prepare_dl
-                )
+            params.caps_directory,
+            valid_df,
+            transformations=transformations,
+            mri_plane=params.mri_plane,
+            prepare_dl=params.prepare_dl
+        )
 
         # Use argument load to distinguish training and testing
         train_loader = DataLoader(
-                data_train,
-                batch_size=params.batch_size,
-                shuffle=True,
-                num_workers=params.num_workers,
-                pin_memory=True
-                )
+            data_train,
+            batch_size=params.batch_size,
+            shuffle=True,
+            num_workers=params.num_workers,
+            pin_memory=True
+        )
 
         valid_loader = DataLoader(
-                data_valid,
-                batch_size=params.batch_size,
-                shuffle=False,
-                num_workers=params.num_workers,
-                pin_memory=True
-                )
+            data_valid,
+            batch_size=params.batch_size,
+            shuffle=False,
+            num_workers=params.num_workers,
+            pin_memory=True
+        )
 
         # Initialize the model
         print('Initialization of the model')
-        model = init_model(params.model, gpu=params.gpu, dropout=params.dropout)
+        model = init_model(
+            params.model,
+            gpu=params.gpu,
+            dropout=params.dropout)
 
         # Define criterion and optimizer
         criterion = torch.nn.CrossEntropyLoss()
@@ -265,11 +282,22 @@ def train_CNN_bad_data_split(params):
         setattr(params, 'beginning_epoch', 0)
 
         # Define output directories
-        log_dir = os.path.join(params.output_dir, 'fold-%i' % fi, 'tensorboard_logs')
+        log_dir = os.path.join(
+            params.output_dir, 'fold-%i' %
+            fi, 'tensorboard_logs')
         model_dir = os.path.join(params.output_dir, 'fold-%i' % fi, 'models')
 
         print('Beginning the training task')
-        train(model, train_loader, valid_loader, criterion, optimizer, False, log_dir, model_dir, params)
+        train(
+            model,
+            train_loader,
+            valid_loader,
+            criterion,
+            optimizer,
+            False,
+            log_dir,
+            model_dir,
+            params)
 
         test_cnn(train_loader, "train", fi, criterion, options)
         test_cnn(valid_loader, "validation", fi, criterion, options)
@@ -283,5 +311,7 @@ if __name__ == "__main__":
     commandline_to_json(commandline, "CNN")
     options = commandline[0]
     if commandline[1]:
-        raise Exception("unknown arguments: %s" % (parser.parse_known_args()[1]))
+        raise Exception(
+            "unknown arguments: %s" %
+            (parser.parse_known_args()[1]))
     train_CNN_bad_data_split(options)
