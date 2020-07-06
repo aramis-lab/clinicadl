04ade9173ad37771954d771a9b3088825e837b0a
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2020-07-06 09:10:04 +0200

    Fix style issues
    Former-commit-id: 4ccc7f92630df671f08ba7f3deb2b7350b08f23c

--- clinicadl/clinicadl/tools/data/utils.py
+++ clinicadl/clinicadl/tools/data/utils.py
@@ -17,7 +17,9 @@ def find_image_path(caps_dir, participant_id, session_id, preprocessing):
                                participant_id + '_' + session_id +
                                FILENAME_TYPE['skull_stripped'] + '.nii.gz')
     else:
-        raise ValueError("Preprocessing %s must be in ['t1-linear', 't1-extensive']." % preprocessing)
+        raise ValueError(
+            "Preprocessing %s must be in ['t1-linear', 't1-extensive']." %
+            preprocessing)
 
     return image_path
 
@@ -43,14 +45,16 @@ def im_loss_roi_gaussian_distribution(im_data, atlas_to_mask, min_value):
         coordinates.append([idx[0][i], idx[1][i], idx[2][i]])
     length_coordinates = len(coordinates)  # all the non zero values
 
-    n = np.random.normal(loc=0.0, scale=0.1, size=length_coordinates)  # gaussian distribution with std = 0.1 and media = 0
+    # gaussian distribution with std = 0.1 and media = 0
+    n = np.random.normal(loc=0.0, scale=0.1, size=length_coordinates)
     max_value = np.min(n)
     n_new = n + abs(max_value)
 
     n_diff = n_new * 10 + min_value
     for i, coord in enumerate(coordinates):
         x, y, z = coord
-        gm_loss[x, y, z] = gm_masked[x, y, z] - n_diff[i] * (gm_masked[x, y, z]) / 100
+        gm_loss[x, y, z] = gm_masked[x, y, z] - \
+            n_diff[i] * (gm_masked[x, y, z]) / 100
 
     normal_region = np.array(im_data, copy=True)
     normal_region[atlas_to_mask > 0] = 0
@@ -73,10 +77,20 @@ def find_borders_of_one_roi(output_path, i, tsv_atlas):
 
     total_mask = []
 
-    border_1 = nib.load(os.path.join(output_path, 'borders-' + str(i) + '.nii')).get_data()
+    border_1 = nib.load(
+        os.path.join(
+            output_path,
+            'borders-' +
+            str(i) +
+            '.nii')).get_data()
     for j in set(tsv_atlas.label):
         if j not in [0, i]:
-            border_2 = nib.load(os.path.join(output_path, 'borders-' + str(j) + '.nii')).get_data()
+            border_2 = nib.load(
+                os.path.join(
+                    output_path,
+                    'borders-' +
+                    str(j) +
+                    '.nii')).get_data()
             image_sum = border_1 + border_2
             mask = image_sum > 1
             mask = mask.astype('float32')
@@ -101,7 +115,8 @@ def find_border_of_prob_gm_and_atlas(output_path, i, tsv_atlas, im_data):
     """
     mask_of_ROI_AAL2 = find_borders_of_one_roi(output_path, i, tsv_atlas)
     gm = binary_t1_pgm(im_data)
-    final_mask_for_gaussian = mask_of_ROI_AAL2 + gm  # AAL2 borders between the ROIS + T1w GM
+    final_mask_for_gaussian = mask_of_ROI_AAL2 + \
+        gm  # AAL2 borders between the ROIS + T1w GM
     f_g = final_mask_for_gaussian > 1
     f_g = f_g.astype('float32')
 
