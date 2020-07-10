005677d87128a61d229a9205ec8584714fb62116
Mauricio DIAZ <mauricio.diaz@inria.fr>
2020-07-10 17:07:23 +0200

    Edit files to prepare pre-release
    Former-commit-id: 444247c9d4acec39f6b8cf442e33ee8b2aafaa88

--- clinicadl/setup.py
+++ clinicadl/setup.py
@@ -1,17 +1,46 @@
-from setuptools import setup
+from os.path import dirname, join, abspath, pardir
+from setuptools import setup, find_packages
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+with open(join(dirname(__file__), 'clinicadl/VERSION'), 'rb') as f:
+    version = f.read().decode('ascii').strip()
+
+this_directory = abspath(dirname(__file__))
+with open(join(this_directory, pardir,  'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+install_reqs = parse_requirements(join(this_directory, pardir, 'requirements.txt'), session='hack')
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name = 'clinicadl',
-    version = '0.0.1',
-    description = 'Deep learning classification with clinica',
-    url = '',
-    packages = ['clinicadl', ],
-    entry_points = {
-        'console_scripts': [
-            'clinicadl = clinicadl.main:main',
-        ],
-    },
-    license = 'MIT',
-    author = 'Mauricio Diaz',
-    author_email = 'mauricio.diaz@inria.fr',
-    )
+        name = 'clinicadl',
+        version = version,
+        description = 'Deep learning classification with clinica',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url = 'https://github.com/aramis-lab/AD-DL',
+        license = 'MIT license',
+        author = 'ARAMIS Lab',
+        maintainer = 'Mauricio DIAZ',
+        maintainer_email = 'mauricio.diaz@inria.fr',
+        packages = ['clinicadl', ],
+        include_package_data=True,
+        zip_safe=False,
+        entry_points = {
+            'console_scripts': [
+                'clinicadl = clinicadl.main:main',
+                ],
+            },
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Operating System :: OS Independent',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python',
+            ],
+        install_requires=reqs
+        )
