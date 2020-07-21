e9def2bbdd1d7fb7a0a3366e978d63eaa0ddce87
Mauricio DIAZ <mauricio.diaz@inria.fr>
2020-07-21 10:21:27 +0200

    Edit test for classify
    Former-commit-id: 62038f1e3b39290a03f9b2f94ad0eda1ac17ed71

--- clinicadl/tests/test_classify.py
+++ clinicadl/tests/test_classify.py
@@ -12,50 +12,52 @@ from os.path import join, exists
 ])
 def classify_commands(request):
 
+    out_filename = 'fold-0/cnn_classification/best_balanced_accuracy/DB-TEST_image_level_prediction.tsv'
     if request.param == 'classify_image':
+        data_folder = 'data/models/image_model_baseline_AD_CN_single_fold/'
         test_input = [
             'classify',
             'data/classify/OASIS_test',
             'data/classify/OASIS_test/data.tsv',
-            'data/models/image_model_baseline_AD_CN_single_fold/',
+            data_folder,
             '--prefix_output', 'DB-TEST',
             '-cpu'
         ]
+        output_files = join(data_folder, out_filename)
     elif request.param == 'classify_slice':
+        data_folder = 'data/models/slice_model_baseline_AD_CN_single_fold/'
         test_input = [
             'classify',
             'data/classify/OASIS_test',
             'data/classify/OASIS_test/data.tsv',
-            'data/models/slice_model_baseline_AD_CN_single_fold/',
+            data_folder,
             '--prefix_output', 'DB-TEST',
             '-cpu'
         ]
+        output_files = join(data_folder, out_filename)
     elif request.param == 'classify_patch':
+        data_folder = 'data/models/patch_model_baseline_AD_CN_multicnn_single_fold/'
         test_input = [
             'classify',
             'data/classify/OASIS_test',
             'data/classify/OASIS_test/data.tsv',
-            'data/models/patch_model_baseline_AD_CN_multicnn_single_fold/',
+            data_folder, 
             '--prefix_output', 'DB-TEST',
             '-cpu'
         ]
+        output_files = join(data_folder, out_filename)
     else:
         raise NotImplementedError(
             "Test %s is not implemented." %
             request.param)
 
-    return test_input
+    return test_input, output_files
 
 
 def test_classify(classify_commands):
     test_input = classify_commands
 
     flag_error = not os.system("clinicadl " + " ".join(test_input))
-    output_file_img = exists(join('data/models/image_model_baseline_AD_CN_single_fold/fold-0/cnn_classification/best_balanced_accuracy/', 'DB-TEST_image_level_prediction.tsv'))
-    #output_file_patch = exists(join('data/models/patch_model_baseline_AD_CN_multicnn_single_fold/fold-0/cnn_classification/best_balanced_accuracy/', 'DB-TEST_image_level_prediction.tsv'))
-    #output_file_slice = exists(join('data/models/slice_model_baseline_AD_CN_single_fold/fold-0/cnn_classification/best_balanced_accuracy/', 'DB-TEST_image_level_prediction.tsv'))
 
     assert flag_error
-    assert output_file_img
-    #assert output_file_patch
-    #assert output_file_slice
+    assert exists(output_files)
