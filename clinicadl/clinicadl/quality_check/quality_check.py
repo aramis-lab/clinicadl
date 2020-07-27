4701198b768485e6c9163fd5f1ff9d517ef6813f
Elina Thibeau--Sutre <elina.thibeausutre@icm-institute.org>
2020-07-27 15:15:58 +0200

    Remove tsv from mandatory args of generate

--- clinicadl/clinicadl/quality_check/quality_check.py
+++ clinicadl/clinicadl/quality_check/quality_check.py
@@ -7,7 +7,6 @@ from pathlib import Path
 import torch
 import pandas as pd
 from torch.utils.data import DataLoader
-from clinica.iotools.utils.data_handling import create_subs_sess_list
 
 from clinicadl.quality_check.utils import QCDataset, resnet_qc_18
 from clinicadl.tools.inputs.input import fetch_file
@@ -53,15 +52,7 @@ def quality_check(caps_dir, output_path, preprocessing,
         model.cuda()
 
     # Load DataFrame
-    if tsv_path is not None:
-        df = pd.read_csv(tsv_path, sep='\t')
-        if ('session_id' not in list(df.columns.values)) or (
-                'participant_id' not in list(df.columns.values)):
-            raise Exception("the data file is not in the correct format."
-                            "Columns should include ['participant_id', 'session_id']")
-    else:
-        create_subs_sess_list(caps_dir, output_path, is_bids_dir=False, use_session_tsv=False)
-        df = pd.read_csv(join(output_path, 'subjects_sessions_list.tsv'), sep="\t")
+    df = load_and_check_tsv(tsv_path, caps_dir, output_path)
 
     dataset = QCDataset(caps_dir, df)
     dataloader = DataLoader(
