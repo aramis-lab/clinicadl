a8f767975894d67ca19751507a04f2b5e8661e3e
Elina Thibeau-Sutre <elina.ts@free.fr>
2021-02-25 09:13:59 +0100

    Fix minor bug in qc

--- clinicadl/clinicadl/quality_check/t1_volume/quality_check.py
+++ clinicadl/clinicadl/quality_check/t1_volume/quality_check.py
@@ -10,7 +10,7 @@ from .utils import extract_metrics
 
 
 def quality_check(caps_dir, output_dir, group_label):
-    extract_metrics(caps_dir=caps_dir, output_dir=output_dir, group=group_label)
+    extract_metrics(caps_dir=caps_dir, output_dir=output_dir, group_label=group_label)
     qc_df = pd.read_csv(path.join(output_dir, 'QC_metrics.tsv'), sep='\t')
 
     rejection1_df = qc_df[qc_df.max_intensity > 0.95]
