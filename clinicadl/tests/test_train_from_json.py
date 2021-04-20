e7f569dc448c4955e37862477b1d5c81e54fd71b
Elina Thibeau-Sutre <elina.ts@free.fr>
2021-04-20 15:06:07 +0200

    Add corresponding test

--- clinicadl/tests/test_train_from_json.py
+++ clinicadl/tests/test_train_from_json.py
@@ -6,112 +6,43 @@ import shutil
 
 
 @pytest.fixture(params=[
-    'train_slice_cnn',
-    'train_image_cnn',
-    'train_patch_cnn',
-    'train_patch_multicnn',
-    'train_roi_cnn',
-    'train_roi_multicnn'
+    'train_roi_cnn'
 ])
 def cli_commands(request):
 
-    if request.param == 'train_slice_cnn':
-        test_input = [
-            'train',
-            'slice',
-            'cnn',
-            'data/dataset/random_example',
-            't1-linear',
-            'data/labels_list',
-            'results',
-            'resnet18',
-            '--epochs', '1',
-            '--n_splits', '2',
-            '--split', '0'
-        ]
-    elif request.param == 'train_image_cnn':
-        test_input = [
-            'train',
-            'image',
-            'cnn',
-            'data/dataset/random_example',
-            't1-linear',
-            'data/labels_list',
-            'results',
-            'Conv5_FC3',
-            '--epochs', '1',
-            '--n_splits', '2',
-            '--split', '0'
-        ]
-    elif request.param == 'train_patch_cnn':
-        test_input = [
-            'train',
-            'patch',
-            'cnn',
-            'data/dataset/random_example',
-            't1-linear',
-            'data/labels_list',
-            'results',
-            'Conv4_FC3',
-            '--epochs', '1',
-            '--n_splits', '2',
-            '--split', '0'
-        ]
-    elif request.param == 'train_patch_multicnn':
-        test_input = [
-            'train',
-            'patch',
-            'multicnn',
-            'data/dataset/random_example',
-            't1-linear',
-            'data/labels_list',
-            'results',
-            'Conv4_FC3',
-            '--epochs', '1',
-            '--n_splits', '2',
-            '--split', '0'
-        ]
-    elif request.param == 'train_roi_cnn':
-        test_input = [
-            'train',
-            'roi',
-            'cnn',
-            'data/dataset/random_example',
-            't1-linear',
-            'data/labels_list',
-            'results',
-            'Conv4_FC3',
-            '--epochs', '1',
-            '--n_splits', '2',
-            '--split', '0'
-        ]
-    elif request.param == 'train_roi_multicnn':
-        test_input = [
-            'train',
-            'roi',
-            'multicnn',
-            'data/dataset/random_example',
-            't1-linear',
-            'data/labels_list',
-            'results',
-            'Conv4_FC3',
-            '--epochs', '1',
-            '--n_splits', '2',
-            '--split', '0'
-        ]
+    if request.param == 'train_roi_cnn':
+        command_dict = {
+            "mode": "roi",
+            "network_type": "cnn",
+            "caps_dir": "data/dataset/random_example",
+            "preprocessing": "t1-linear",
+            "tsv_path": "data/labels_list",
+            "model": "Conv4_FC3",
+
+            "epochs": 1,
+            "n_splits": 2,
+            "split": 0,
+            "gpu": True
+        }
     else:
         raise NotImplementedError(
             "Test %s is not implemented." %
             request.param)
 
-    return test_input
+    return command_dict
 
 
 def test_train(cli_commands):
-    test_input = cli_commands
-    flag_error = not os.system("clinicadl " + " ".join(test_input))
+    import json
+
+    json = json.dumps(cli_commands, skipkeys=True, indent=4)
+    with open(os.path.join("commandline.json"), "w") as f:
+        f.write(json)
+
+    flag_error = not os.system("clinicadl train from_json commandline.json results")
     performances_flag = os.path.exists(
         os.path.join("results", "fold-0", "cnn_classification"))
     assert flag_error
     assert performances_flag
     shutil.rmtree("results")
+    os.remove("commandline.json")
