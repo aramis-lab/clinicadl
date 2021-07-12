cafd21056a78baf42f4e8c937e283fcf6d7358e9
14thibea <elina.ts@free.fr>
2021-07-12 17:38:26 +0200

    Fix import

--- clinicadl/clinicadl/utils/network/__init__.py
+++ clinicadl/clinicadl/utils/network/__init__.py
@@ -1,10 +1,3 @@
 from .autoencoder.models import AE_Conv4_FC3, AE_Conv5_FC3
 from .cnn.models import Conv4_FC3, Conv5_FC3, resnet18
 from .cnn.random import RandomArchitecture
-from .models import (
-    create_autoencoder,
-    create_model,
-    load_model,
-    load_optimizer,
-    save_checkpoint,
-)
