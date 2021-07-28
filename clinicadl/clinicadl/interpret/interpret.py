6e7eb4af35cd45778af180087f3f4115b657f083
ravih18 <rhassana96@gmail.com>
2021-07-28 18:19:10 +0200

    Interpret command line

--- clinicadl/clinicadl/interpret/interpret.py
+++ clinicadl/clinicadl/interpret/interpret.py
@@ -1,23 +1,37 @@
 from clinicadl import MapsManager
 
 
-def interpret_cli(options):
-    maps_path = options.model_path
+def interpret(model_path,
+    caps_directory,
+    tsv_path,
+    prefix_name,
+    selection_metrics,
+    multi_cohort,
+    diagnosis,
+    baseline,
+    target_node,
+    save_individual,
+    batch_size,
+    nproc,
+    use_cpu,
+    verbose=0,
+    ):
+    maps_path = model_path
     verbose_list = ["warning", "info", "debug"]
 
-    maps_manager = MapsManager(maps_path, verbose=verbose_list[options.verbose])
+    maps_manager = MapsManager(maps_path, verbose=verbose_list[verbose])
 
     maps_manager.interpret(
-        caps_directory=options.caps_directory,
-        tsv_path=options.tsv_path,
-        prefix=options.name,
-        selection_metrics=options.selection_metrics,
-        multi_cohort=options.multi_cohort,
-        diagnoses=[options.diagnosis],
-        baseline=options.baseline,
-        target_node=options.target_node,
-        save_individual=options.save_individual,
-        batch_size=options.batch_size,
-        num_workers=options.nproc,
-        use_cpu=options.use_cpu,
+        caps_directory=caps_directory,
+        tsv_path=tsv_path,
+        prefix=prefix_name,
+        selection_metrics=selection_metrics,
+        multi_cohort=multi_cohort,
+        diagnoses=[diagnosis],
+        baseline=baseline,
+        target_node=target_node,
+        save_individual=save_individual,
+        batch_size=batch_size,
+        num_workers=nproc,
+        use_cpu=use_cpu,
     )
--- clinicadl/clinicadl/interpret/interpret_cli.py
+++ clinicadl/clinicadl/interpret/interpret_cli.py
@@ -1,1 +1,104 @@
 import click
+
+from clinicadl.utils import cli_param
+
+@click.command("interpret")
+@click.argument(
+    "model_directory", type=click.Path(),
+)
+@click.argument(
+    "interpretation_method",
+    type=str,
+)
+# Model
+@click.option(
+    "--selection_metrics",
+    default=["loss"],
+    type=str,
+    multiple=True,
+    help="Loads the model selected on the metrics given.",
+)
+# Data
+@click.option(
+    "--participants_tsv",
+    type=click.File(),
+    default=None,
+    help="TSV path with subjects/sessions to process, if different from classification task.",
+)
+@click.option(
+    "--caps_directory",
+    type=click.Path(exists=True),
+    default=None,
+    help="Data using CAPS structure, if different from classification task",
+)
+@click.option(
+    "--multi_cohort",
+    type=bool,
+    default=False,
+    is_flag=True,
+    help="Performs multi-cohort interpretation. In this case, caps_directory and tsv_path must be paths to TSV files.",
+)
+@click.option(
+    "-d", "--diagnosis",
+    default="AD",
+    type=str,
+    help="The images corresponding to this diagnosis only will be loaded.",
+)
+@click.option(
+    "--target_node",
+    default=0,
+    type=str,
+    help="Which target node the gradients explain. Default takes the first output node.",
+)
+@click.option(
+    "--baseline",
+    type=bool,
+    default=False,
+    is_flag=True,
+    help="If provided, only the baseline sessions are used for interpretation.",
+)
+@click.option(
+    "--save_individual",
+    type=str,
+    default=None,
+    help="Saves individual saliency maps in addition to the mean saliency map.",
+)
+@cli_param.option.n_proc
+@cli_param.option.use_gpu
+@cli_param.option.batch_size
+def cli(model_directory,
+    interpretation_method,
+    caps_directory,
+    participants_tsv,
+    selection_metrics,
+    multi_cohort,
+    diagnosis,
+    baseline,
+    target_node,
+    save_individual,
+    batch_size,
+    n_proc,
+    use_gpu,
+    # verbose,
+    ):
+    """
+    Interpret the prediction of MODEL_DIRECTORY with the chosen INTERPRETATION_METHOD.
+    """
+    from .interpret import interpret
+
+    interpret(
+        model_path=model_directory,
+        caps_directory=caps_directory,
+        tsv_path=participants_tsv,
+        name=interpretation_method,
+        selection_metrics=selection_metrics,
+        multi_cohort=multi_cohort,
+        diagnosis=diagnosis,
+        baseline=baseline,
+        target_node=target_node,
+        save_individual=save_individual,
+        batch_size=batch_size,
+        nproc=n_proc,
+        use_cpu=not use_gpu,
+        # verbose=verbose,
+    )
