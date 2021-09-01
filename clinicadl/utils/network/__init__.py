386d5045f8f610f05c59e1cdcfabf005bcad4f7e
ravih18 <rhassana96@gmail.com>
2021-09-01 17:22:03 +0200

    added new models in init

--- clinicadl/utils/network/__init__.py
+++ clinicadl/utils/network/__init__.py
@@ -1,9 +1,13 @@
 from .autoencoder.models import AE_Conv4_FC3, AE_Conv5_FC3
 from .cnn.models import Conv4_FC3, Conv5_FC3, resnet18
 from .cnn.random import RandomArchitecture
+<<<<<<< HEAD
 from .vae.vanilla_vae import (
     Vanilla3DdenseVAE,
     Vanilla3DVAE,
     VanillaDenseVAE,
     VanillaSpatialVAE,
 )
+=======
+from .vae.vanilla_vae import Vanilla3DVAE, VanillaDenseVAE, VanillaSpatialVAE
+>>>>>>> added new models in init
