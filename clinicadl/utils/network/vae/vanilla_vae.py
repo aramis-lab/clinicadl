92ddcca35c3f8763b3f494bd76d85bb31a26b75d
ravih18 <rhassana96@gmail.com>
2021-09-13 14:48:41 +0200

    Added new vae with dense latent space

--- clinicadl/utils/network/vae/vanilla_vae.py
+++ clinicadl/utils/network/vae/vanilla_vae.py
@@ -309,3 +309,100 @@ class Vanilla3DdenseVAE(BaseVAE):
             recons_weight=recons_weight,
             KL_weight=kl_weight,
         )
+
+
+class Vanilla3DdenseVAE(BaseVAE):
+    def __init__(
+        self,
+        input_size,
+        use_cpu=False,
+    ):
+        n_conv = 4
+        first_layer_channels = 32
+        last_layer_channels = 32
+        feature_size = 2048
+        latent_size = 64
+        decoder_output_padding = [
+            [1, 0, 0],
+            [0, 0, 0],
+            [0, 0, 1],
+        ]
+
+        input_c = input_size[0]
+        input_h = input_size[1]
+        input_w = input_size[2]
+        input_d = input_size[3]
+
+        ## Encoder
+        encoder_layers = []
+        # Input Layer
+        encoder_layers.append(EncoderLayer3D(input_c, first_layer_channels))
+        # Conv Layers
+        for i in range(n_conv - 1):
+            encoder_layers.append(
+                EncoderLayer3D(
+                    first_layer_channels * 2 ** i, first_layer_channels * 2 ** (i + 1)
+                )
+            )
+        n_pix = (
+            first_layer_channels
+            * 2 ** (n_conv - 1)
+            * (input_h // (2 ** n_conv))
+            * (input_w // (2 ** n_conv))
+            * (input_d // (2 ** n_conv))
+        )
+        encoder_layers.append(
+            nn.Sequential(Flatten(), nn.Linear(n_pix, feature_size), nn.ReLU())
+        )
+        encoder = nn.Sequential(*encoder_layers)
+
+        ## Latent space
+        # hidden => mu
+        mu_layer = nn.Linear(feature_size, latent_size)
+        # hidden => logvar
+        var_layer = nn.Linear(feature_size, latent_size)
+
+        ## Decoder
+        decoder_layers = []
+        decoder_layers.append(
+            nn.Sequential(
+                nn.Linear(latent_size, feature_size),
+                nn.ReLU(),
+                nn.Linear(feature_size, n_pix),
+                nn.ReLU(),
+                Unflatten3D(
+                    last_layer_channels * 2 ** (n_conv - 1),
+                    input_h // (2 ** n_conv),
+                    input_w // (2 ** n_conv),
+                    input_d // (2 ** n_conv),
+                ),
+                nn.ReLU(),
+            )
+        )
+        for i in range(n_conv - 1, 0, -1):
+            decoder_layers.append(
+                DecoderLayer3D(
+                    last_layer_channels * 2 ** (i),
+                    last_layer_channels * 2 ** (i - 1),
+                    output_padding=decoder_output_padding[-i],
+                )
+            )
+        decoder_layers.append(
+            nn.Sequential(
+                nn.ConvTranspose3d(
+                    last_layer_channels,
+                    input_c,
+                    4,
+                    stride=2,
+                    padding=1,
+                    output_padding=[1, 0, 1],
+                    bias=False,
+                ),
+                nn.Sigmoid(),
+            )
+        )
+        decoder = nn.Sequential(*decoder_layers)
+
+        super(Vanilla3DdenseVAE, self).__init__(
+            encoder, decoder, mu_layer, var_layer, use_cpu=use_cpu
+        )
