21913f7e053291750575e3c434339c26c16767dd
ravih18 <rhassana96@gmail.com>
2021-09-29 17:51:43 +0200

    Added print for debug

--- clinicadl/utils/network/vae/vae_utils.py
+++ clinicadl/utils/network/vae/vae_utils.py
@@ -213,6 +213,7 @@ class VAE_Encoder(nn.Module):
             tensor_h, tensor_w = tensor_h // 2, tensor_w // 2
 
         self.decoder_padding = decoder_padding
+        print(self.decoder_padding)
 
         # Final Layer
         if latent_dim == 1:
