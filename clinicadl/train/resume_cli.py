bb38ed451716270b953e3997c7f96e436eafc625
ravih18 <rhassana96@gmail.com>
2021-10-01 13:49:40 +0200

    Fix resume cli argument name

--- clinicadl/train/resume_cli.py
+++ clinicadl/train/resume_cli.py
@@ -5,14 +5,14 @@ from clinicadl.utils import cli_param
 
 @click.command(name="resume")
 @cli_param.argument.input_maps
-def cli(maps_directory):
+def cli(input_maps_directory):
     """Resume training job in specified maps.
 
     INPUT_MAPS_DIRECTORY is the path to the MAPS folder where training job has started.
     """
     from .resume import automatic_resume
 
-    automatic_resume(maps_directory)
+    automatic_resume(input_maps_directory)
 
 
 if __name__ == "__main__":
