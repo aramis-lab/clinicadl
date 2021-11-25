8df1b74472edf5f6a3e06f2b2882bebd7670a304
ravih18 <rhassana96@gmail.com>
2021-11-25 17:26:42 +0100

    Fixed held network

--- clinicadl/utils/network/__init__.py
+++ clinicadl/utils/network/__init__.py
@@ -1,7 +1,7 @@
 from .autoencoder.models import AE_Conv4_FC3, AE_Conv5_FC3
 from .cnn.models import Conv4_FC3, Conv5_FC3, resnet18
 from .cnn.random import RandomArchitecture
-from .vae.convolutional_VAE import CVAE_3D
+from .vae.convolutional_VAE import CVAE_3D, CVAE_3D_half
 from .vae.vanilla_vae import (
     Vanilla3DdenseVAE,
     Vanilla3DVAE,
