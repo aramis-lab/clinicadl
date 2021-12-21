db3e33c5e671453dc11b4098826b9e5883ac0275
14thibea <elina.ts@free.fr>
2021-12-21 14:22:03 +0100

    Update input directory

--- tests/test_qc.py
+++ tests/test_qc.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.fixture(params=["t1-linear", "t1-volume"])
 def cli_commands(request):
     if request.param == "t1-linear":
-        input_dir = "image"
+        input_dir = "data/dataset/OasisCaps_example"
     elif request.param == "t1-volume":
         input_dir = "t1-volume"
     else:
