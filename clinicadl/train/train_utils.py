ef7a17a405543933b42fef1b48360be951a0e700
ravih18 <rhassana96@gmail.com>
2021-12-21 18:27:18 +0100

    Added command in train to list available models and their description

--- clinicadl/train/train_utils.py
+++ clinicadl/train/train_utils.py
@@ -67,3 +67,29 @@ def get_user_dict(config_file: str, task: str) -> Dict[str, Any]:
         )
 
     return train_dict
+
+
+def get_model_list(architecture=None, input_size=(128, 128)):
+    """"""
+    from inspect import getmembers, isclass
+
+    import clinicadl.utils.network as network_package
+
+    if not architecture:
+        model_list = getmembers(network_package, isclass)
+        for model in model_list:
+            print(model[0])
+    else:
+        model_class = getattr(network_package, architecture)
+        args = list(
+            model_class.__init__.__code__.co_varnames[
+                : model_class.__init__.__code__.co_argcount
+            ]
+        )
+        args.remove("self")
+        kwargs = dict()
+        kwargs["input_size"] = (1, input_size[0], input_size[1])
+        kwargs["use_cpu"] = True
+
+        model = model_class(**kwargs)
+        print(model.layers)
