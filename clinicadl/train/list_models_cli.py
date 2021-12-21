9f5dbb123ac5a233196a3a6ee65bd70f42d704a0
ravih18 <rhassana96@gmail.com>
2021-12-21 18:27:18 +0100

    Added command in train to list available models and their description

---
+++ clinicadl/train/list_models_cli.py
@@ -0,0 +1,33 @@
+import click
+
+
+@click.command(name="list_models", no_args_is_help=True)
+@click.option(
+    "-a",
+    "--architecture",
+    type=str,
+    help="Architecture of the chosen model to display.",
+)
+@click.option(
+    "-in",
+    "--input_size",
+    type=int,
+    multiple=2,
+    default=(128, 128),
+    show_default=True,
+    help="Size of the input image.",
+)
+def cli(
+    architecture,
+    input_size,
+):
+    """Show the list of available models in ClinicaDL.
+    If you choose a specific model with architecture, this pipeline will show the whole model layers.
+    """
+    from .train_utils import get_model_list
+
+    get_model_list(architecture, input_size)
+
+
+if __name__ == "__main__":
+    cli()
