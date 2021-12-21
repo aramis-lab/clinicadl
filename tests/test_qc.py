1daa76b922e4029ddc314470f56623fbff791173
14thibea <elina.ts@free.fr>
2021-12-21 12:33:09 +0100

    Add test QC

---
+++ tests/test_qc.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.fixture(params=["t1-linear", "t1-volume"])
+def cli_commands(request):
+    if request.param == "t1-linear":
+        input_dir = "image"
+    elif request.param == "t1-volume":
+        input_dir = "t1-volume"
+    else:
+        raise NotImplementedError("Test %s is not implemented." % request.param)
+
+    return input_dir, request.param
+
+
+def test_qc(cli_commands):
+    pass
