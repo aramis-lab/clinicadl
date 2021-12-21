77aff8580cc43bc9790192d399199ba124ab6e23
14thibea <elina.ts@free.fr>
2021-12-21 10:35:08 +0100

    Add losses

--- clinicadl/train/tasks/reconstruction_cli.py
+++ clinicadl/train/tasks/reconstruction_cli.py
@@ -43,6 +43,7 @@ from .task_utils import task_launcher
 @train_option.patience
 @train_option.tolerance
 @train_option.accumulation_steps
+@train_option.reconstruction_loss
 # transfer learning
 @train_option.transfer_path
 @train_option.transfer_selection_metric
