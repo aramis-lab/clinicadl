70f040d0f118af6afa692a04b35b6c6589002b2b
ravih18 <rhassana96@gmail.com>
2021-12-28 13:38:35 +0100

    Convert tensor to numpy to save it as nifti

--- clinicadl/utils/maps_manager/maps_manager.py
+++ clinicadl/utils/maps_manager/maps_manager.py
@@ -1109,7 +1109,7 @@ class MapsManager:
                     .cpu()
                 )
                 # Convert tensor to nifti image with appropriate affine
-                input_nii = nib.Nifti1Image(image, eye(4))
+                input_nii = nib.Nifti1Image(image.detach().cpu().numpy(), eye(4))
                 output_nii = nib.Nifti1Image(output, eye(4))
                 # Create file name according to participant and session id
                 participant_id = data["participant_id"]
