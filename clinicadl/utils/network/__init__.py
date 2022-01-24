18651e88d87cfc420b7dabb6c7dcf4f499e338a1
ravih18 <rhassana96@gmail.com>
2022-01-24 17:34:01 +0100

    Added new trivial BL models

--- clinicadl/utils/network/__init__.py
+++ clinicadl/utils/network/__init__.py
@@ -1,6 +1,7 @@
-from .autoencoder.models import AE_Conv4_FC3, AE_Conv5_FC3
+from .autoencoder.models import AE_Conv4_FC3, AE_Conv5_FC3, CAE_half
 from .cnn.models import Conv4_FC3, Conv5_FC3, Stride_Conv5_FC3, resnet18
 from .cnn.random import RandomArchitecture
+from .unet.unet import GeneratorUNet
 from .vae.convolutional_VAE import CVAE_3D, CVAE_3D_half
 from .vae.vanilla_vae import (
     Vanilla3DdenseVAE,
