126fd0c892165e980cc00a6745ccdce7dcf4ef36
ravih18 <rhassana96@gmail.com>
2022-04-01 19:14:43 +0200

    Changed reconstruction error variable

--- clinicadl/utils/network/vae/vae_utils.py
+++ clinicadl/utils/network/vae/vae_utils.py
@@ -6,8 +6,9 @@ def VAELoss(input, reconstruction, mu, logVar):
     kl_divergence = (
         0.5 * torch.sum(-1 - logVar + mu.pow(2) + logVar.exp()) / mu.shape[1]
     )
-    recon_error = MSELoss(reduction="mean")(reconstruction, input)
-    # recon_error = torch.sum((reconstructed - input_) ** 2) / input_.shape[0]
+    # recon_error = MSELoss(reduction="mean")(reconstruction, input)
+    # recon_error = MSELoss(reduction="mean")(reconstruction, input)
+    recon_error = torch.sum((reconstruction - input) ** 2) / input.shape[0]
     return recon_error, kl_divergence
 
 
