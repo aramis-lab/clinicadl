09d74176c2c88f465d7bb0b271747d225dab9128
ravih18 <rhassana96@gmail.com>
2022-04-22 20:25:38 +0200

    divide loss value

--- clinicadl/utils/network/vae/vae_utils.py
+++ clinicadl/utils/network/vae/vae_utils.py
@@ -5,9 +5,9 @@ from torch.nn import MSELoss
 def VAELoss(input, reconstruction, mu, logVar):
     kl_divergence = (
         0.5 * torch.sum(-1 - logVar + mu.pow(2) + logVar.exp()) / mu.shape[0]
-    )
+    ) / 100000
     # recon_error = MSELoss(reduction="mean")(reconstruction, input)
-    recon_error = MSELoss(reduction="sum")(reconstruction, input)
+    recon_error = MSELoss(reduction="sum")(reconstruction, input) / 100000
     # recon_error = torch.sum((reconstruction - input) ** 2) / input.shape[0]
     return recon_error, kl_divergence
 
