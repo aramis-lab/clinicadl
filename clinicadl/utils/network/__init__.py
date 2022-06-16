0713bc60db8ddd15192e02458b6ef28db1619284
ravih18 <rhassana96@gmail.com>
2022-06-16 09:56:04 +0200

    Added new VAE with few new features

--- clinicadl/utils/network/__init__.py
+++ clinicadl/utils/network/__init__.py
@@ -1,3 +1,5 @@
+from vae.advanced_CVAE import CVAE_3D_final_conv
+
 from .autoencoder.models import AE_Conv4_FC3, AE_Conv5_FC3, CAE_half
 from .cnn.models import Conv4_FC3, Conv5_FC3, Stride_Conv5_FC3, resnet18
 from .cnn.random import RandomArchitecture
