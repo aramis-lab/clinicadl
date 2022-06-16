2ec5f62727d6443f7075e31ec72cc4c07610785f
ravih18 <rhassana96@gmail.com>
2022-06-16 17:34:46 +0200

    hot fix model import

--- clinicadl/utils/network/__init__.py
+++ clinicadl/utils/network/__init__.py
@@ -1,5 +1,3 @@
-from vae.advanced_CVAE import CVAE_3D_final_conv
-
 from .autoencoder.models import AE_Conv4_FC3, AE_Conv5_FC3, CAE_half
 from .cnn.models import (
     Conv4_FC3,
@@ -11,6 +9,7 @@ from .cnn.models import (
 )
 from .cnn.random import RandomArchitecture
 from .unet.unet import GeneratorUNet
+from .vae.advanced_CVAE import CVAE_3D_final_conv
 from .vae.convolutional_VAE import CVAE_3D, CVAE_3D_half
 from .vae.vanilla_vae import (
     Vanilla3DdenseVAE,
