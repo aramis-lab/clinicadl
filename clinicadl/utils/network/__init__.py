55f2beeac9bbb99019bcf5ab2ce4eed8cc535a25
Camille Brianceau <camille.brianceau@gmail.com>
2022-07-19 14:50:52 +0200

    Run pre-commit (black)

--- clinicadl/utils/network/__init__.py
+++ clinicadl/utils/network/__init__.py
@@ -1,5 +1,5 @@
 from .autoencoder.models import AE_Conv4_FC3, AE_Conv5_FC3
-from .cnn.models import Conv4_FC3, Conv5_FC3, Stride_Conv5_FC3, resnet18, Inception
+from .cnn.models import Conv4_FC3, Conv5_FC3, Inception, Stride_Conv5_FC3, resnet18
 from .cnn.random import RandomArchitecture
 from .vae.vanilla_vae import (
     Vanilla3DdenseVAE,
