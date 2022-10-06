a33c23998a88c6f458187998f3a7023c857917c3
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2022-10-06 09:26:57 +0200

    Update clinicadl/tsvtools/get_progression/get_progression_cli.py
    Co-authored-by: mdiazmel <mauricio.diaz@inria.fr>

--- clinicadl/tsvtools/get_progression/get_progression_cli.py
+++ clinicadl/tsvtools/get_progression/get_progression_cli.py
@@ -16,7 +16,7 @@ from clinicadl.utils import cli_param
 def cli(data_tsv, time_horizon):
     """Get the progression of Alzheimer's disease.
 
-    DATA_TSV is the path to the tsv file with colmuns including ["participants_id", "session_id"]
+    DATA_TSV is the path to the tsv file with columns including ["participants_id", "session_id"]
 
     TIME_HORIZON is the time in months choosen to analyse the stability of the label (default is 36)
 
