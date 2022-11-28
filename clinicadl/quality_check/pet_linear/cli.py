2e8d6b1e7ac7baaa8ab6d5f8f87c7d2c89d1058e
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2022-11-28 14:38:27 +0100

    add pet QC

---
+++ clinicadl/quality_check/pet_linear/cli.py
@@ -0,0 +1,51 @@
+import click
+
+from clinicadl.utils import cli_param
+
+
+@click.command(name="pet-linear", no_args_is_help=True)
+@cli_param.argument.caps_directory
+@click.argument(
+    "output_directory",
+    type=str,
+)
+@click.argument(
+    "acq_label",
+    type=str,
+)
+@click.argument(
+    "ref_region",
+    type=str,
+)
+@click.option(
+    "--threshold",
+    type=float,
+    default=0.5,
+    show_default=True,
+    help="The threshold on the output probability to decide if the image "
+    "passed or failed.",
+)
+def cli(
+    caps_directory,
+    output_directory,
+    acq_label,
+    ref_region,
+    threshold,
+):
+    """Performs quality check on t1-volume pipeline.
+
+    CAPS_DIRECTORY is the CAPS folder where t1-volume outputs are stored.
+
+    OUTPUT_DIRECTORY is the path to the directory in which TSV files will be written.
+
+    GROUP_LABEL is the group associated to the gray matter DARTEL template in CAPS_DIRECTORY.
+    """
+    from .quality_check import quality_check as pet_linear_qc
+
+    pet_linear_qc(
+        caps_directory,
+        output_directory,
+        acq_label,
+        ref_region,
+        threshold,
+    )
---
+++ clinicadl/quality_check/pet_linear/quality_check.py
@@ -0,0 +1,51 @@
+"""
+Automatically reject images incorrectly preprocessed pet-linear (Unified Segmentation) with ? criterion
+
+
+"""
+from logging import getLogger
+from os import path
+
+import pandas as pd
+
+from .utils import extract_metrics
+
+
+def quality_check(caps_dir, output_directory, acq_label, ref_region, threshold):
+
+    logger = getLogger("clinicadl.quality_check")
+
+    extract_metrics(caps_dir=caps_dir, output_dir=output_directory, acq_label=acq_label, ref_region=ref_region)
+    logger.info(f"Quality check metrics extracted at {path.join(output_directory, 'QC_metrics.tsv')}.")
+    qc_df = pd.read_csv(path.join(output_directory, "QC_metrics.tsv"), sep="\t")
+
+    rejection1_df = qc_df[qc_df.max_intensity > threshold]
+    rejection1_df.to_csv(path.join(output_directory, "data_cleaned.tsv"), sep="\t", index=False)
+
+    rejection2_df = qc_df[qc_df.max_intensity < threshold]
+    rejection2_df.to_csv(path.join(output_directory, "data_failed.tsv"), sep="\t", index=False)
+
+    logger.info(f"Number of sessions removed based on max intensity: {len(qc_df) - len(rejection1_df)}.")
+    logger.debug(f"{rejection2_df}")
+
+    # rejection2_df = rejection1_df[
+    #     (rejection1_df.non_zero_percentage < 0.5)
+    #     & (rejection1_df.non_zero_percentage > 0.15)]
+        
+    # rejection2_df.to_csv(
+    #     path.join(output_directory, "pass_step-2.tsv"), sep="\t", index=False
+    # )
+    # logger.info(
+    #     f"Number of sessions removed based on non-zero voxels: {len(rejection1_df) - len(rejection2_df)}."
+    # )
+    # logger.debug(f"{rejection2_df}")
+
+    # rejection3_df = rejection2_df[rejection2_df.frontal_similarity > 0.10]
+    # rejection3_df.to_csv(
+    #     path.join(output_directory, "pass_step-3.tsv"), sep="\t", index=False
+    # )
+    # logger.info(
+    #     f"Number of sessions removed based on frontal similarity with DARTEL "
+    #     f"template: {len(rejection2_df) - len(rejection3_df)}."
+    # )
+    # logger.debug(f"{rejection3_df}")
