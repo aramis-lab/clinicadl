53e5895fcb25201bfe7937ce96c69c5ca08d8147
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2023-02-07 14:58:04 +0100

    tests

--- clinicadl/tsvtools/analysis/analysis_cli.py
+++ clinicadl/tsvtools/analysis/analysis_cli.py
@@ -6,7 +6,7 @@ from clinicadl.utils import cli_param
 @click.command(name="analysis", no_args_is_help=True)
 @cli_param.argument.merged_tsv
 @cli_param.argument.data_tsv
-@cli_param.argument.results_tsv
+@cli_param.argument.output_tsv
 @cli_param.option.diagnoses
 def cli(merged_tsv, data_tsv, results_tsv, diagnoses):
     """Demographic analysis of the extracted labels.
