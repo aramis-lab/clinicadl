02cc15bb714aea66e94ee6527e992bd9e03f7b82
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2023-03-09 12:03:01 +0100

    add option --output_dir in get-labels (#392)
    * add option --output_dir in get-labels
    
    * fix json pb
    
    * Fix rdt build, use python 3.10 (#393)
    
    * solve pb rebasage
    
    ---------
    
    Co-authored-by: mdiazmel <mauricio.diaz@inria.fr>

--- clinicadl/tsvtools/get_labels/get_labels.py
+++ clinicadl/tsvtools/get_labels/get_labels.py
@@ -25,7 +25,6 @@ from clinicadl.utils.tsvtools_utils import (
     find_label,
     first_session,
     last_session,
-    merged_tsv_reader,
     neighbour_session,
 )
 
@@ -250,6 +249,7 @@ def get_labels(
     merged_tsv: str = None,
     missing_mods: str = None,
     remove_unique_session: bool = False,
+    output_dir: str = None,
 ):
     """
     Writes one TSV file based on merged_tsv and missing_mods.
@@ -277,11 +277,16 @@ def get_labels(
         Path to the output directory of clinica iotools check-missing-modalities if already exists
     remove_unique_session: bool
         If True, subjects with only one session are removed.
+    output_dir: str (path)
+        Path to the directory where the output labels.tsv will be stored.
     """
 
     from pathlib import Path
 
-    results_directory = Path(bids_directory).parents[0]
+    if output_dir == None:
+        results_directory = Path(bids_directory).parents[0]
+    else:
+        results_directory = output_dir
     output_tsv = results_directory / "labels.tsv"
 
     commandline_to_json(
