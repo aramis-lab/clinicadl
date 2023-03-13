2ad9be314bb128cdaedc5f0f24e189c52a8516f4
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2023-03-13 11:10:09 +0100

    fix tsvtools tests with new argument

--- tests/test_train_ae.py
+++ tests/test_train_ae.py
@@ -35,7 +35,7 @@ def test_train_ae(cmdopt, tmp_path, test_name):
     labels_path = str(input_dir / "labels_list" / "2_fold")
     config_path = str(input_dir / "train_config.toml")
     if test_name == "image_ae":
-        split = [0, 0]
+        split = [0, 1]
         test_input = [
             "train",
             "reconstruction",
