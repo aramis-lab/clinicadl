52967ee367f9244768cb8c27149b2bfb41d4e641
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2023-04-06 23:08:39 +0200

    cb_add_tests (#411)
    * add docs + tests
    
    * add flair data for prepare data tests

--- tests/test_prepare_data.py
+++ tests/test_prepare_data.py
@@ -38,21 +38,28 @@ def test_prepare_data(cmdopt, tmp_path, test_name):
     clean_folder(tmp_out_dir, recreate=True)
 
     input_caps_directory = input_dir / "caps"
+    input_caps_flair_directory = input_dir / "caps_flair"
     if test_name == "image":
         if (tmp_out_dir / "caps_image").is_dir():
             shutil.rmtree(tmp_out_dir / "caps_image")
+            shutil.rmtree(tmp_out_dir / "caps_image_flair")
+        shutil.copytree(input_caps_flair_directory, tmp_out_dir / "caps_image_flair")
         shutil.copytree(input_caps_directory, tmp_out_dir / "caps_image")
         parameters = {"mode": "image"}
 
     elif test_name == "patch":
         if (tmp_out_dir / "caps_patch").is_dir():
             shutil.rmtree(tmp_out_dir / "caps_patch")
+            shutil.rmtree(tmp_out_dir / "caps_patch_flair")
+        shutil.copytree(input_caps_flair_directory, tmp_out_dir / "caps_patch_flair")
         shutil.copytree(input_caps_directory, tmp_out_dir / "caps_patch")
         parameters = {"mode": "patch", "patch_size": 50, "stride_size": 50}
 
     elif test_name == "slice":
         if (tmp_out_dir / "caps_slice").is_dir():
             shutil.rmtree(tmp_out_dir / "caps_slice")
+            shutil.rmtree(tmp_out_dir / "caps_slice_flair")
+        shutil.copytree(input_caps_flair_directory, tmp_out_dir / "caps_slice_flair")
         shutil.copytree(input_caps_directory, tmp_out_dir / "caps_slice")
         parameters = {
             "mode": "slice",
@@ -64,6 +71,8 @@ def test_prepare_data(cmdopt, tmp_path, test_name):
     elif test_name == "roi":
         if (tmp_out_dir / "caps_roi").is_dir():
             shutil.rmtree(tmp_out_dir / "caps_roi")
+            shutil.rmtree(tmp_out_dir / "caps_roi_flair")
+        shutil.copytree(input_caps_flair_directory, tmp_out_dir / "caps_roi_flair")
         shutil.copytree(input_caps_directory, tmp_out_dir / "caps_roi")
         parameters = {
             "mode": "roi",
@@ -81,15 +90,15 @@ def test_prepare_data(cmdopt, tmp_path, test_name):
 
 def run_test_prepare_data(input_dir, ref_dir, out_dir, parameters):
 
-    modalities = ["t1-linear", "pet-linear"]  # , "custom"]
+    modalities = ["t1-linear", "pet-linear", "flair-linear"]
     uncropped_image = [True, False]
     acquisition_label = ["18FAV45", "11CPIB"]
-    parameters["save_features"] = True
     parameters["prepare_dl"] = True
 
     for modality in modalities:
         parameters["preprocessing"] = modality
         if modality == "pet-linear":
+            parameters["save_features"] = True
             for acq in acquisition_label:
                 parameters["tracer"] = acq
                 parameters["suvr_reference_region"] = "pons2"
@@ -102,6 +111,7 @@ def run_test_prepare_data(input_dir, ref_dir, out_dir, parameters):
                 extract_generic(out_dir, mode, tsv_file, parameters)
 
         elif modality == "custom":
+            parameters["save_features"] = True
             parameters["use_uncropped_image"] = True
             parameters[
                 "custom_suffix"
@@ -113,15 +123,29 @@ def run_test_prepare_data(input_dir, ref_dir, out_dir, parameters):
             extract_generic(out_dir, mode, tsv_file, parameters)
 
         elif modality == "t1-linear":
+            parameters["save_features"] = True
             for flag in uncropped_image:
                 parameters["use_uncropped_image"] = flag
                 parameters[
                     "extract_json"
                 ] = f"{modality}_crop-{not flag}_mode-{parameters['mode']}.json"
 
-                tsv_file = input_dir / "subjects.tsv"
+                # tsv_file = input_dir / "subjects.tsv"
                 mode = parameters["mode"]
-                extract_generic(out_dir, mode, tsv_file, parameters)
+                extract_generic(out_dir, mode, None, parameters)
+
+        elif modality == "flair-linear":
+            parameters["save_features"] = False
+            parameters["prepare_dl"] = False
+            for flag in uncropped_image:
+                parameters["use_uncropped_image"] = flag
+                parameters[
+                    "extract_json"
+                ] = f"{modality}_crop-{not flag}_mode-{parameters['mode']}.json"
+
+                # tsv_file = input_dir / "subjects_flair.tsv"
+                mode = f"{parameters['mode']}_flair"
+                extract_generic(out_dir, mode, None, parameters)
         else:
             raise NotImplementedError(
                 f"Test for modality {modality} was not implemented."
