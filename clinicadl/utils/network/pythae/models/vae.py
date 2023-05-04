f91beb7a3d95ba553392233e92e8a101c38aab1c
ravih18 <rhassana96@gmail.com>
2023-05-04 15:04:06 +0200

    Corrected batch size for vae

--- clinicadl/utils/network/pythae/models/vae.py
+++ clinicadl/utils/network/pythae/models/vae.py
@@ -46,6 +46,7 @@ class pythae_VAE(BasePythae):
             output_dir=output_dir,
             num_epochs=num_epochs,
             learning_rate=learning_rate,
-            batch_size=batch_size,
+            per_device_train_batch_size=batch_size,
+            per_device_eval_batch_size=batch_size,
             amp=True,
         )
