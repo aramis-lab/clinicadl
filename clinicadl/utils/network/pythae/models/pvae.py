3a979b946849b24a2e46307146c6ecf50a34b97f
ravih18 <rhassana96@gmail.com>
2023-05-04 18:19:57 +0200

    updated batch size argument for trainer config

--- clinicadl/utils/network/pythae/models/pvae.py
+++ clinicadl/utils/network/pythae/models/pvae.py
@@ -47,5 +47,6 @@ class pythae_PoincareVAE(BasePythae):
             output_dir=output_dir,
             num_epochs=num_epochs,
             learning_rate=learning_rate,
-            batch_size=batch_size
+            per_device_train_batch_size=batch_size,
+            per_device_eval_batch_size=batch_size,
         )
