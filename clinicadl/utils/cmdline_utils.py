0d742742c9a15c9428c32ace718c2d56d03b460f
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2023-06-27 15:06:38 +0200

    poetry update

--- clinicadl/utils/cmdline_utils.py
+++ clinicadl/utils/cmdline_utils.py
@@ -4,7 +4,7 @@ from clinicadl.utils.exceptions import ClinicaDLArgumentError
 def check_gpu():
     import torch
 
-    # if not torch.cuda.is_available():
-    #     raise ClinicaDLArgumentError(
-    #         "No GPU is available. Please add the --no-gpu flag to run on CPU."
-    #     )
+    if not torch.cuda.is_available():
+        raise ClinicaDLArgumentError(
+            "No GPU is available. Please add the --no-gpu flag to run on CPU."
+        )
