510594a2e256ce86f97507fa37d680222a6d5a2f
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2023-08-10 15:11:32 +0200

    tests

--- clinicadl/utils/task_manager/task_manager.py
+++ clinicadl/utils/task_manager/task_manager.py
@@ -169,182 +169,181 @@ class TaskManager:
         """Returns the default network to use when no architecture is specified."""
         pass
 
+    def test(
+        self,
+        model: Network,
+        dataloader: DataLoader,
+        criterion: _Loss,
+        monte_carlo: int = 0,
+        seed=None,
+        use_labels: bool = True,
+        amp: bool = False,
+        save_reconstruction_tensor=True,
+        save_reconstruction_nifti=False,
+        save_latent_tensor=False,
+        tensor_path=None,
+        nifti_path=None,
+        latent_tensor_path=None,
+    ) -> Tuple[pd.DataFrame, Dict[str, float], pd.DataFrame]:
+        """
+        Computes the predictions and evaluation metrics.
 
-def test(
-    self,
-    model: Network,
-    dataloader: DataLoader,
-    criterion: _Loss,
-    monte_carlo: int = 0,
-    seed=None,
-    use_labels: bool = True,
-    amp: bool = False,
-    save_reconstruction_tensor=True,
-    save_reconstruction_nifti=False,
-    save_latent_tensor=False,
-    tensor_path=None,
-    nifti_path=None,
-    latent_tensor_path=None,
-) -> Tuple[pd.DataFrame, Dict[str, float], pd.DataFrame]:
-    """
-    Computes the predictions and evaluation metrics.
+        Args:
+            model: the model trained.
+            dataloader: wrapper of a CapsDataset.
+            criterion: function to calculate the loss.
+            use_labels: If True the true_label will be written in output DataFrame
+                and metrics dict will be created.
+            monte_carlo: number of monte carlo iterations to perform.
+            seed: seed to use for the monte carlo sampling.
+            save_reconstruction_tensor: if True, the reconstruction tensor is saved.
+            save_reconstruction_nifti: if True, the reconstruction nifti is saved.
+            save_latent_tensor: if True, the latent tensor is saved.
+            tensor_path: path to the folder where the tensor will be saved.
+            nifti_path: path to the folder where the nifti will be saved.
+            latent_tensor_path: path to the folder where the latent tensor will be saved.
+        Returns:
+            the results and metrics on the image level.
+        """
+        model.eval()
+        dataloader.dataset.eval()
 
-    Args:
-        model: the model trained.
-        dataloader: wrapper of a CapsDataset.
-        criterion: function to calculate the loss.
-        use_labels: If True the true_label will be written in output DataFrame
-            and metrics dict will be created.
-        monte_carlo: number of monte carlo iterations to perform.
-        seed: seed to use for the monte carlo sampling.
-        save_reconstruction_tensor: if True, the reconstruction tensor is saved.
-        save_reconstruction_nifti: if True, the reconstruction nifti is saved.
-        save_latent_tensor: if True, the latent tensor is saved.
-        tensor_path: path to the folder where the tensor will be saved.
-        nifti_path: path to the folder where the nifti will be saved.
-        latent_tensor_path: path to the folder where the latent tensor will be saved.
-    Returns:
-        the results and metrics on the image level.
-    """
-    model.eval()
-    dataloader.dataset.eval()
+        results_df = pd.DataFrame(columns=self.columns())
+        mc_results_df = pd.DataFrame(columns=self.columns(monte_carlo=monte_carlo))
 
-    results_df = pd.DataFrame(columns=self.columns())
-    mc_results_df = pd.DataFrame(columns=self.columns(monte_carlo=monte_carlo))
-
-    total_loss = {}
-    with torch.no_grad():
-        for data in dataloader:
-            # output = model.predict(data)
-            with autocast(enabled=amp):
-                outputs, loss_dict = model.compute_outputs_and_loss(
-                    data, criterion, use_labels=use_labels
-                )
-
-                if i == 0:
-                    for loss_component in loss_dict.keys():
-                        total_loss[loss_component] = 0
-                for loss_component in total_loss.keys():
-                    total_loss[loss_component] += (
-                        loss_dict[loss_component].float().item()
+        total_loss = {}
+        with torch.no_grad():
+            for data in dataloader:
+                # output = model.predict(data)
+                with autocast(enabled=amp):
+                    outputs, loss_dict = model.compute_outputs_and_loss(
+                        data, criterion, use_labels=use_labels
                     )
 
-                image = data["data"]
-                # data["data"] = data["data"].unsqueeze(0)
-                participant_id = data["participant_id"][0]
-                session_id = data["session_id"][0]
-                mode_id = data[f"{self.mode}_id"][0]
-
-            # Save tensor
-            if save_reconstruction_tensor:
-                reconstruction = outputs["recon_x"].squeeze(0).cpu()
-                input_filename = (
-                    f"{participant_id}_{session_id}_{self.mode}-{mode_id}_input.pt"
-                )
-                output_filename = (
-                    f"{participant_id}_{session_id}_{self.mode}-{mode_id}_output.pt"
-                )
-                torch.save(image, tensor_path / input_filename)
-                torch.save(reconstruction, tensor_path / output_filename)
-                # logger.debug(f"File saved at {[input_filename, output_filename]}")
-
-            # Save nifti image
-            if save_reconstruction_nifti:
-                reconstruction = output["recon_x"].squeeze(0).cpu()
-                # Convert tensor to nifti image with appropriate affine
-                input_nii = nib.Nifti1Image(image[0].detach().numpy(), np.eye(4))
-                output_nii = nib.Nifti1Image(
-                    reconstruction[0].detach().numpy(),
-                    np.eye(4),
-                )
-                # Create file name according to participant and session id
-                input_filename = f"{participant_id}_{session_id}_image_input.nii.gz"
-                output_filename = f"{participant_id}_{session_id}_image_output.nii.gz"
-                nib.save(input_nii, nifti_path / input_filename)
-                nib.save(output_nii, nifti_path / output_filename)
-
-            # Save latent tensor
-            if save_latent_tensor:
-                latent = outputs["embedding"].squeeze(0).cpu()
-                output_filename = (
-                    f"{participant_id}_{session_id}_{self.mode}-{mode_id}_latent.pt"
-                )
-                torch.save(latent, latent_tensor_path / output_filename)
-
-            # Generate detailed DataFrame
-            for idx in range(len(data["participant_id"])):
-                row = self.generate_test_row(idx, data, outputs.float())
-                row_df = pd.DataFrame(row, columns=self.columns())
-                results_df = pd.concat([results_df, row_df])
-
-            del outputs, loss_dict
-
-            if monte_carlo:
-                outputs = model.predict(data, monte_carlo=monte_carlo, seed=seed)
-
-                for i in range(monte_carlo):
-                    output = outputs[i]
-
-                    if save_reconstruction_tensor:
-                        reconstruction = output["recon_x"].squeeze(0).cpu()
-                        input_filename = f"{participant_id}_{session_id}_{self.mode}-{mode_id}_input.pt"
-                        output_filename = f"{participant_id}_{session_id}_{self.mode}-{mode_id}_output-mc{i}.pt"
-                        torch.save(image, tensor_path / input_filename)
-                        torch.save(reconstruction, tensor_path / output_filename)
-                        # logger.debug(
-                        #     f"File saved at {[input_filename, output_filename]}"
-                        # )
-
-                    if save_reconstruction_nifti:
-                        input_nii = nib.Nifti1Image(
-                            image[0].detach().numpy(), np.eye(4)
+                    if i == 0:
+                        for loss_component in loss_dict.keys():
+                            total_loss[loss_component] = 0
+                    for loss_component in total_loss.keys():
+                        total_loss[loss_component] += (
+                            loss_dict[loss_component].float().item()
                         )
-                        output_nii = nib.Nifti1Image(
-                            reconstruction[0].detach().numpy(),
-                            np.eye(4),
-                        )
-                        # Create file name according to participant and session id
-                        input_filename = (
-                            f"{participant_id}_{session_id}_image_input.nii.gz"
-                        )
-                        output_filename = (
-                            f"{participant_id}_{session_id}_image_output-mc{i}.nii.gz"
-                        )
-                        nib.save(input_nii, nifti_path / input_filename)
-                        nib.save(output_nii, nifti_path / output_filename)
 
-                    if save_latent_tensor:
-                        latent = output["embedding"].squeeze(0).cpu()
-                        output_filename = f"{participant_id}_{session_id}_{self.mode}-{mode_id}_latent-mc{i}.pt"
-                        torch.save(latent, latent_tensor_path / output_filename)
+                    image = data["data"]
+                    # data["data"] = data["data"].unsqueeze(0)
+                    participant_id = data["participant_id"][0]
+                    session_id = data["session_id"][0]
+                    mode_id = data[f"{self.mode}_id"][0]
 
-                    row = self.generate_test_row_monte_carlo(
-                        idx, i, data, outputs[i]["recon_x"]
+                # Save tensor
+                if save_reconstruction_tensor:
+                    reconstruction = outputs["recon_x"].squeeze(0).cpu()
+                    input_filename = (
+                        f"{participant_id}_{session_id}_{self.mode}-{mode_id}_input.pt"
                     )
-                    row_df = pd.DataFrame(
-                        row, columns=self.columns(monte_carlo=monte_carlo)
+                    output_filename = (
+                        f"{participant_id}_{session_id}_{self.mode}-{mode_id}_output.pt"
                     )
-                    mc_results_df = pd.concat([mc_results_df, row_df])
+                    torch.save(image, tensor_path / input_filename)
+                    torch.save(reconstruction, tensor_path / output_filename)
+                    # logger.debug(f"File saved at {[input_filename, output_filename]}")
 
-                del outputs
+                # Save nifti image
+                if save_reconstruction_nifti:
+                    reconstruction = output["recon_x"].squeeze(0).cpu()
+                    # Convert tensor to nifti image with appropriate affine
+                    input_nii = nib.Nifti1Image(image[0].detach().numpy(), np.eye(4))
+                    output_nii = nib.Nifti1Image(
+                        reconstruction[0].detach().numpy(),
+                        np.eye(4),
+                    )
+                    # Create file name according to participant and session id
+                    input_filename = f"{participant_id}_{session_id}_image_input.nii.gz"
+                    output_filename = (
+                        f"{participant_id}_{session_id}_image_output.nii.gz"
+                    )
+                    nib.save(input_nii, nifti_path / input_filename)
+                    nib.save(output_nii, nifti_path / output_filename)
 
-        results_df.reset_index(inplace=True, drop=True)
-        results_df[self.evaluation_metrics] = results_df[self.evaluation_metrics].apply(
-            pd.to_numeric, axis=1
-        )
+                # Save latent tensor
+                if save_latent_tensor:
+                    latent = outputs["embedding"].squeeze(0).cpu()
+                    output_filename = (
+                        f"{participant_id}_{session_id}_{self.mode}-{mode_id}_latent.pt"
+                    )
+                    torch.save(latent, latent_tensor_path / output_filename)
 
-        if monte_carlo:
-            mc_results_df.reset_index(inplace=True, drop=True)
-            mc_results_df[self.evaluation_metrics] = mc_results_df[
+                # Generate detailed DataFrame
+                for idx in range(len(data["participant_id"])):
+                    row = self.generate_test_row(idx, data, outputs.float())
+                    row_df = pd.DataFrame(row, columns=self.columns())
+                    results_df = pd.concat([results_df, row_df])
+
+                del outputs, loss_dict
+
+                if monte_carlo:
+                    outputs = model.predict(data, monte_carlo=monte_carlo, seed=seed)
+
+                    for i in range(monte_carlo):
+                        output = outputs[i]
+
+                        if save_reconstruction_tensor:
+                            reconstruction = output["recon_x"].squeeze(0).cpu()
+                            input_filename = f"{participant_id}_{session_id}_{self.mode}-{mode_id}_input.pt"
+                            output_filename = f"{participant_id}_{session_id}_{self.mode}-{mode_id}_output-mc{i}.pt"
+                            torch.save(image, tensor_path / input_filename)
+                            torch.save(reconstruction, tensor_path / output_filename)
+                            # logger.debug(
+                            #     f"File saved at {[input_filename, output_filename]}"
+                            # )
+
+                        if save_reconstruction_nifti:
+                            input_nii = nib.Nifti1Image(
+                                image[0].detach().numpy(), np.eye(4)
+                            )
+                            output_nii = nib.Nifti1Image(
+                                reconstruction[0].detach().numpy(),
+                                np.eye(4),
+                            )
+                            # Create file name according to participant and session id
+                            input_filename = (
+                                f"{participant_id}_{session_id}_image_input.nii.gz"
+                            )
+                            output_filename = f"{participant_id}_{session_id}_image_output-mc{i}.nii.gz"
+                            nib.save(input_nii, nifti_path / input_filename)
+                            nib.save(output_nii, nifti_path / output_filename)
+
+                        if save_latent_tensor:
+                            latent = output["embedding"].squeeze(0).cpu()
+                            output_filename = f"{participant_id}_{session_id}_{self.mode}-{mode_id}_latent-mc{i}.pt"
+                            torch.save(latent, latent_tensor_path / output_filename)
+
+                        row = self.generate_test_row_monte_carlo(
+                            idx, i, data, outputs[i]["recon_x"]
+                        )
+                        row_df = pd.DataFrame(
+                            row, columns=self.columns(monte_carlo=monte_carlo)
+                        )
+                        mc_results_df = pd.concat([mc_results_df, row_df])
+
+                    del outputs
+
+            results_df.reset_index(inplace=True, drop=True)
+            results_df[self.evaluation_metrics] = results_df[
                 self.evaluation_metrics
             ].apply(pd.to_numeric, axis=1)
 
-    if not use_labels:
-        metrics_df = None
-    else:
-        metrics_dict = self.compute_metrics(results_df)
-        for loss_component in total_loss.keys():
-            metrics_dict[loss_component] = total_loss[loss_component]
-    torch.cuda.empty_cache()
+            if monte_carlo:
+                mc_results_df.reset_index(inplace=True, drop=True)
+                mc_results_df[self.evaluation_metrics] = mc_results_df[
+                    self.evaluation_metrics
+                ].apply(pd.to_numeric, axis=1)
 
-    return results_df, metrics_df, mc_results_df
+        if not use_labels:
+            metrics_df = None
+        else:
+            metrics_dict = self.compute_metrics(results_df)
+            for loss_component in total_loss.keys():
+                metrics_dict[loss_component] = total_loss[loss_component]
+        torch.cuda.empty_cache()
+
+        return results_df, metrics_df, mc_results_df
