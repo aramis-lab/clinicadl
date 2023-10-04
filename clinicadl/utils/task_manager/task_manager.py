10811bf7c485b592d4ee8bf14a6a497ee1482a8c
sophieloiz <sophie.loizillon@gmail.com>
2023-10-04 15:35:03 +0200

    Debug test_da function

--- clinicadl/utils/task_manager/task_manager.py
+++ clinicadl/utils/task_manager/task_manager.py
@@ -246,6 +246,7 @@ class TaskManager:
         criterion: _Loss,
         use_labels: bool = True,
         target: bool = True,
+        amp: bool = False,
     ) -> Tuple[pd.DataFrame, Dict[str, float]]:
         """
         Computes the predictions and evaluation metrics.
@@ -256,36 +257,103 @@ class TaskManager:
             criterion: function to calculate the loss.
             use_labels: If True the true_label will be written in output DataFrame
                 and metrics dict will be created.
+            amp: If True, enables Pytorch's automatic mixed precision.
         Returns:
             the results and metrics on the image level.
         """
         model.eval()
         dataloader.dataset.eval()
-        print(f"Start task manager for {target}")
+
         results_df = pd.DataFrame(columns=self.columns)
-        total_loss = 0
+        total_loss = {}
         with torch.no_grad():
             for i, data in enumerate(dataloader):
-                print(target)
-                outputs, loss_dict = model.compute_outputs_and_loss_test(
-                    data, criterion, 0, target
-                )
-                total_loss += loss_dict["loss"].item()
+                # initialize the loss list to save the loss components
+                with autocast(enabled=amp):
+                    outputs, loss_dict = model.compute_outputs_and_loss_test(
+                        data, criterion, 0, target
+                    )
+
+                if i == 0:
+                    for loss_component in loss_dict.keys():
+                        total_loss[loss_component] = 0
+                for loss_component in total_loss.keys():
+                    total_loss[loss_component] += loss_dict[loss_component].float()
 
                 # Generate detailed DataFrame
                 for idx in range(len(data["participant_id"])):
-                    row = self.generate_test_row(idx, data, outputs)
+                    row = self.generate_test_row(idx, data, outputs.float())
                     row_df = pd.DataFrame(row, columns=self.columns)
                     results_df = pd.concat([results_df, row_df])
 
                 del outputs, loss_dict
-            results_df.reset_index(inplace=True, drop=True)
+        dataframes = [None] * dist.get_world_size()
+        dist.gather_object(
+            results_df, dataframes if dist.get_rank() == 0 else None, dst=0
+        )
+        if dist.get_rank() == 0:
+            results_df = pd.concat(dataframes)
+        del dataframes
+        results_df.reset_index(inplace=True, drop=True)
 
         if not use_labels:
             metrics_dict = None
         else:
             metrics_dict = self.compute_metrics(results_df)
-            metrics_dict["loss"] = total_loss
+            for loss_component in total_loss.keys():
+                dist.reduce(total_loss[loss_component], dst=0)
+                metrics_dict[loss_component] = total_loss[loss_component].item()
         torch.cuda.empty_cache()
 
         return results_df, metrics_dict
+
+    # def test_da(
+    #     self,
+    #     model: Network,
+    #     dataloader: DataLoader,
+    #     criterion: _Loss,
+    #     use_labels: bool = True,
+    #     target: bool = True,
+    # ) -> Tuple[pd.DataFrame, Dict[str, float]]:
+    #     """
+    #     Computes the predictions and evaluation metrics.
+
+    #     Args:
+    #         model: the model trained.
+    #         dataloader: wrapper of a CapsDataset.
+    #         criterion: function to calculate the loss.
+    #         use_labels: If True the true_label will be written in output DataFrame
+    #             and metrics dict will be created.
+    #     Returns:
+    #         the results and metrics on the image level.
+    #     """
+    #     model.eval()
+    #     dataloader.dataset.eval()
+    #     print(f"Start task manager for {target}")
+    #     results_df = pd.DataFrame(columns=self.columns)
+    #     total_loss = 0
+    #     with torch.no_grad():
+    #         for i, data in enumerate(dataloader):
+    #             print(target)
+    #             outputs, loss_dict = model.compute_outputs_and_loss_test(
+    #                 data, criterion, 0, target
+    #             )
+    #             total_loss += loss_dict["loss"].item()
+
+    #             # Generate detailed DataFrame
+    #             for idx in range(len(data["participant_id"])):
+    #                 row = self.generate_test_row(idx, data, outputs)
+    #                 row_df = pd.DataFrame(row, columns=self.columns)
+    #                 results_df = pd.concat([results_df, row_df])
+
+    #             del outputs, loss_dict
+    #         results_df.reset_index(inplace=True, drop=True)
+
+    #     if not use_labels:
+    #         metrics_dict = None
+    #     else:
+    #         metrics_dict = self.compute_metrics(results_df)
+    #         metrics_dict["loss"] = total_loss
+    #     torch.cuda.empty_cache()
+
+    #     return results_df, metrics_dict
