2d5c33a21983652e17c30ba85e9c5ade2bfd77d9
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2024-01-19 10:25:39 +0100

    Add option to save model for each epochs  (#507)
    * add option

--- clinicadl/utils/maps_manager/maps_manager.py
+++ clinicadl/utils/maps_manager/maps_manager.py
@@ -79,6 +79,7 @@ class MapsManager:
             test_parameters = change_str_to_path(test_parameters)
             self.parameters = add_default_values(test_parameters)
             self.ssda_network = False  # A MODIFIER
+            self.save_all_models = self.parameters["save_all_models"]
             self.task_manager = self._init_task_manager(n_classes=self.output_size)
             self.split_name = (
                 self._check_split_wording()
@@ -1301,6 +1302,7 @@ class MapsManager:
                     best_dict,
                     split,
                     network=network,
+                    save_all_models=self.parameters["save_all_models"],
                 )
                 self._write_weights(
                     {
@@ -1311,6 +1313,7 @@ class MapsManager:
                     None,
                     split,
                     filename="optimizer.pth.tar",
+                    save_all_models=self.parameters["save_all_models"],
                 )
 
             epoch += 1
@@ -1633,6 +1636,7 @@ class MapsManager:
                 best_dict,
                 split,
                 network=network,
+                save_all_models=False,
             )
             self._write_weights(
                 {
@@ -1643,6 +1647,7 @@ class MapsManager:
                 None,
                 split,
                 filename="optimizer.pth.tar",
+                save_all_models=False,
             )
 
             epoch += 1
@@ -2425,6 +2430,7 @@ class MapsManager:
         split: int,
         network: int = None,
         filename: str = "checkpoint.pth.tar",
+        save_all_models: bool = False,
     ):
         """
         Update checkpoint and save the best model according to a set of metrics.
@@ -2441,6 +2447,13 @@ class MapsManager:
         checkpoint_dir.mkdir(parents=True, exist_ok=True)
         checkpoint_path = checkpoint_dir / filename
         torch.save(state, checkpoint_path)
+
+        if save_all_models:
+            all_models_dir = (
+                self.maps_path / f"{self.split_name}-{split}" / "all_models"
+            )
+            all_models_dir.mkdir(parents=True, exist_ok=True)
+            torch.save(state, all_models_dir / f"model_epoch_{state['epoch']}.pth.tar")
 
         best_filename = "model.pth.tar"
         if network is not None:
