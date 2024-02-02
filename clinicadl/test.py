7732e3b45d219bb4c087125fe30cb3b693c87934
camillebrianceau <camille.brianceau@gmail.com>
2024-02-02 11:48:43 +0100

    delete test file

--- clinicadl/test.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-from clinicadl.train.train_utils import bids_to_caps
-
-bids_to_caps(
-    Path("/Users/camille.brianceau/aramis/DATA/bids_QC_OASIS1"),
-    Path("/Users/camille.brianceau/aramis/DATA/caps_QC_OASIS1_bis"),
-)
