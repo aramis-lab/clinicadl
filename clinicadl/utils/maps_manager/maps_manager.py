f180f04b9d686ba647818726603e2f8f9a066787
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2024-02-16 16:58:20 +0100

    resolve issue on predict (#529)

--- clinicadl/utils/maps_manager/maps_manager.py
+++ clinicadl/utils/maps_manager/maps_manager.py
@@ -256,7 +256,6 @@ class MapsManager:
                 multi_cohort=multi_cohort,
             )
         criterion = self.task_manager.get_criterion(self.loss)
-
         self._check_data_group(
             data_group,
             caps_directory,
@@ -2237,7 +2236,6 @@ class MapsManager:
                 if data_group in ["train", "validation"]:
                     raise MAPSError("Cannot overwrite train or validation data group.")
                 else:
-                    shutil.rmtree(group_dir)
                     if not split_list:
                         split_list = self._find_splits()
                     for split in split_list:
