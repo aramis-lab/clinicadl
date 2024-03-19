c6e2721fb56c86cd38cab5135ff5fe01520d25e4
thibaultdvx <154365476+thibaultdvx@users.noreply.github.com>
2024-03-19 14:08:20 +0100

    fix indent issue

--- tests/unittests/utils/test_clinica_utils.py
+++ tests/unittests/utils/test_clinica_utils.py
@@ -2,21 +2,21 @@ import pytest
 
 
 @pytest.mark.parametrize(
-        "acq_label,suvr_reference_region,uncropped_image,expected_pattern",
-        [
-            (
-                "18FFDG", 
-                "cerebellumPons2", 
-                True, 
-                "pet_linear/*_trc-18FFDG_space-MNI152NLin2009cSym_res-1x1x1_suvr-cerebellumPons2_pet.nii.gz"
-            ),
-            (
-                "18FAV45", 
-                "pons", 
-                False, 
-                "pet_linear/*_trc-18FAV45_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_suvr-pons_pet.nii.gz"
-            )
-        ] 
+    "acq_label,suvr_reference_region,uncropped_image,expected_pattern",
+    [
+        (
+            "18FFDG", 
+            "cerebellumPons2", 
+            True, 
+            "pet_linear/*_trc-18FFDG_space-MNI152NLin2009cSym_res-1x1x1_suvr-cerebellumPons2_pet.nii.gz"
+        ),
+        (
+            "18FAV45", 
+            "pons", 
+            False, 
+            "pet_linear/*_trc-18FAV45_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_suvr-pons_pet.nii.gz"
+        )
+    ] 
 
 )
 def test_pet_linear_nii(
