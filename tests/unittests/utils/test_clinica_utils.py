5e2fd3f3db42e32e4e6118b40eb699c468d0607a
thibaultdvx <154365476+thibaultdvx@users.noreply.github.com>
2024-03-20 10:53:09 +0100

    resolve formatting

--- tests/unittests/utils/test_clinica_utils.py
+++ tests/unittests/utils/test_clinica_utils.py
@@ -5,29 +5,26 @@ import pytest
     "acq_label,suvr_reference_region,uncropped_image,expected_pattern",
     [
         (
-            "18FFDG", 
-            "cerebellumPons2", 
-            True, 
-            "pet_linear/*_trc-18FFDG_space-MNI152NLin2009cSym_res-1x1x1_suvr-cerebellumPons2_pet.nii.gz"
+            "18FFDG",
+            "cerebellumPons2",
+            True,
+            "pet_linear/*_trc-18FFDG_space-MNI152NLin2009cSym_res-1x1x1_suvr-cerebellumPons2_pet.nii.gz",
         ),
         (
-            "18FAV45", 
-            "pons", 
-            False, 
-            "pet_linear/*_trc-18FAV45_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_suvr-pons_pet.nii.gz"
-        )
-    ] 
-
+            "18FAV45",
+            "pons",
+            False,
+            "pet_linear/*_trc-18FAV45_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_suvr-pons_pet.nii.gz",
+        ),
+    ],
 )
 def test_pet_linear_nii(
     acq_label, suvr_reference_region, uncropped_image, expected_pattern
 ):
     from clinicadl.utils.clinica_utils import pet_linear_nii
 
-    assert(
-        pet_linear_nii(acq_label, suvr_reference_region, uncropped_image) == {
-            "description": "",
-            "needed_pipeline": "pet-linear",
-            "pattern": expected_pattern,
-        }
-    )
+    assert pet_linear_nii(acq_label, suvr_reference_region, uncropped_image) == {
+        "description": "",
+        "needed_pipeline": "pet-linear",
+        "pattern": expected_pattern,
+    }
