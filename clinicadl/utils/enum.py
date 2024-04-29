2dd93a8035f651d87f26b6955cb5ed63a815fa00
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2024-04-29 14:24:20 +0200

    Cleaning (#564)
    * first commirt to clean last PR
    
    * add enum file + adapt prepare-data

---
+++ clinicadl/utils/enum.py
@@ -0,0 +1,74 @@
+from enum import Enum
+
+
+class InterpretationMethod(str, Enum):
+    """Possible interpretation method in clinicaDL."""
+
+    GRADIENTS = "gradients"
+    GRAD_CAM = "grad-cam"
+
+
+class SUVRReferenceRegions(str, Enum):
+    """Possible SUVR reference region for pet images in clinicaDL."""
+
+    PONS = "pons"
+    CEREBELLUMPONS = "cerebellumPons"
+    PONS2 = "pons2"
+    CEREBELLUMPONS2 = "cerebellumPons2"
+
+
+class Tracer(str, Enum):
+    """Possible tracer for pet images in clinicaDL."""
+
+    FFDG = "18FFDG"
+    FAV45 = "18FAV45"
+    CPIB = "11CPIB"
+
+
+class Pathology(str, Enum):
+    """Possible pathology for hypometabolic generation of pet images in clinicaDL."""
+
+    AD = "ad"
+    BVFTD = "bvftd"
+    LVPPA = "lvppa"
+    NFVPPA = "nfvppa"
+    PCA = "pca"
+    SVPPA = "svppa"
+
+
+class BIDSModality(str, Enum):
+    """Possible modality for images in clinicaDL."""
+
+    T1 = "t1"
+    DWI = "dwi"
+    PET = "pet"
+    FLAIR = "flair"
+    DTI = "dti"
+    CUSTOM = "custom"
+
+
+class LinearModality(str, Enum):
+    T1W = "T1w"
+    T2W = "T2w"
+    FLAIR = "flair"
+
+
+class Preprocessing(str, Enum):
+    """Possible preprocessing method in clinicaDL."""
+
+    T1_LINEAR = "t1-linear"
+    T1_EXTENSIVE = "t1-extensive"
+    PET_LINEAR = "pet-linear"
+    FLAIR_LINEAR = "flair-linear"
+    CUSTOM = "custom"
+    DWI_DTI = "dwi-dti"
+    T2_LINEAR = "t2-linear"
+
+
+class DTIBasedMeasure(str, Enum):
+    """Possible DTI measures."""
+
+    FRACTIONAL_ANISOTROPY = "FA"
+    MEAN_DIFFUSIVITY = "MD"
+    AXIAL_DIFFUSIVITY = "AD"
+    RADIAL_DIFFUSIVITY = "RD"
