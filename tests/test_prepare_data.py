7417fa7288b6b8f9fbe74f43f39e6545f34f8132
camillebrianceau <camille.brianceau@gmail.com>
2024-05-14 15:56:56 +0200

    solve pb with tests prepare-data

--- tests/test_prepare_data.py
+++ tests/test_prepare_data.py
@@ -147,7 +147,9 @@ def run_test_prepare_data(input_dir, ref_dir, out_dir, parameters):
             raise NotImplementedError(
                 f"Test for modality {modality} was not implemented."
             )
-    assert compare_folders(out_dir / f"caps_{mode}", ref_dir / f"caps_{mode}", out_dir)
+        assert compare_folders(
+            out_dir / f"caps_{mode}", ref_dir / f"caps_{mode}", out_dir
+        )
 
 
 def extract_generic(out_dir, mode, tsv_file, parameters):
