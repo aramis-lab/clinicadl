68e5acfc59fb42d16d000bf0d276e7d467f4b53d
thibaultdvx <154365476+thibaultdvx@users.noreply.github.com>
2024-05-16 11:16:39 +0200

    change cli options with new config classes

--- clinicadl/train/tasks/reconstruction/reconstruction_cli_options.py
+++ clinicadl/train/tasks/reconstruction/reconstruction_cli_options.py
@@ -4,32 +4,32 @@ from clinicadl.utils import cli_param
 from clinicadl.utils.config_utils import get_default_from_config_class as get_default
 from clinicadl.utils.config_utils import get_type_from_config_class as get_type
 
-from .reconstruction_config import ReconstructionConfig
+from .reconstruction_config import ModelConfig, ValidationConfig
 
-reconstruction_config = ReconstructionConfig
-
+# Model
 architecture = cli_param.option_group.model_group.option(
     "-a",
     "--architecture",
-    type=get_type("architecture", reconstruction_config),
-    default=get_default("architecture", reconstruction_config),
+    type=get_type("architecture", ModelConfig),
+    default=get_default("architecture", ModelConfig),
     help="Architecture of the chosen model to train. A set of model is available in ClinicaDL, default architecture depends on the NETWORK_TASK (see the documentation for more information).",
-)
-selection_metrics = cli_param.option_group.task_group.option(
-    "--selection_metrics",
-    "-sm",
-    multiple=True,
-    type=click.Choice(get_type("selection_metrics", reconstruction_config)),
-    default=get_default("selection_metrics", reconstruction_config),
-    help="""Allow to save a list of models based on their selection metric. Default will
-    only save the best model selected on loss.""",
-    show_default=True,
 )
 loss = cli_param.option_group.task_group.option(
     "--loss",
     "-l",
-    type=click.Choice(get_type("loss", reconstruction_config)),
-    default=get_default("loss", reconstruction_config),
+    type=click.Choice(get_type("loss", ModelConfig)),
+    default=get_default("loss", ModelConfig),
     help="Loss used by the network to optimize its training task.",
+    show_default=True,
+)
+# Validation
+selection_metrics = cli_param.option_group.task_group.option(
+    "--selection_metrics",
+    "-sm",
+    multiple=True,
+    type=click.Choice(get_type("selection_metrics", ValidationConfig)),
+    default=get_default("selection_metrics", ValidationConfig),
+    help="""Allow to save a list of models based on their selection metric. Default will
+    only save the best model selected on loss.""",
     show_default=True,
 )
