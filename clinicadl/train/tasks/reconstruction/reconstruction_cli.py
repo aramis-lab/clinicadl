c59836c936279eff8bab0ab49b6aa1fa856e2a69
thibaultdvx <154365476+thibaultdvx@users.noreply.github.com>
2024-05-16 11:43:37 +0200

    rename tasks cli options

--- clinicadl/train/tasks/reconstruction/reconstruction_cli.py
+++ clinicadl/train/tasks/reconstruction/reconstruction_cli.py
@@ -1,6 +1,6 @@
 import click
 
-from clinicadl.train.tasks import training_cli_options
+from clinicadl.train.tasks import train_task_cli_options
 from clinicadl.train.trainer import Task, Trainer
 from clinicadl.train.utils import merge_cli_and_config_file_options
 
@@ -10,64 +10,64 @@ from .reconstruction_config import ReconstructionConfig
 
 @click.command(name="reconstruction", no_args_is_help=True)
 # Mandatory arguments
-@training_cli_options.caps_directory
-@training_cli_options.preprocessing_json
-@training_cli_options.tsv_directory
-@training_cli_options.output_maps
+@train_task_cli_options.caps_directory
+@train_task_cli_options.preprocessing_json
+@train_task_cli_options.tsv_directory
+@train_task_cli_options.output_maps
 # Options
-@training_cli_options.config_file
+@train_task_cli_options.config_file
 # Computational
-@training_cli_options.gpu
-@training_cli_options.n_proc
-@training_cli_options.batch_size
-@training_cli_options.evaluation_steps
-@training_cli_options.fully_sharded_data_parallel
-@training_cli_options.amp
+@train_task_cli_options.gpu
+@train_task_cli_options.n_proc
+@train_task_cli_options.batch_size
+@train_task_cli_options.evaluation_steps
+@train_task_cli_options.fully_sharded_data_parallel
+@train_task_cli_options.amp
 # Reproducibility
-@training_cli_options.seed
-@training_cli_options.deterministic
-@training_cli_options.compensation
-@training_cli_options.save_all_models
+@train_task_cli_options.seed
+@train_task_cli_options.deterministic
+@train_task_cli_options.compensation
+@train_task_cli_options.save_all_models
 # Model
 @reconstruction_cli_options.architecture
-@training_cli_options.multi_network
-@training_cli_options.ssda_network
+@train_task_cli_options.multi_network
+@train_task_cli_options.ssda_network
 # Data
-@training_cli_options.multi_cohort
-@training_cli_options.diagnoses
-@training_cli_options.baseline
-@training_cli_options.valid_longitudinal
-@training_cli_options.normalize
-@training_cli_options.data_augmentation
-@training_cli_options.sampler
-@training_cli_options.caps_target
-@training_cli_options.tsv_target_lab
-@training_cli_options.tsv_target_unlab
-@training_cli_options.preprocessing_json_target
+@train_task_cli_options.multi_cohort
+@train_task_cli_options.diagnoses
+@train_task_cli_options.baseline
+@train_task_cli_options.valid_longitudinal
+@train_task_cli_options.normalize
+@train_task_cli_options.data_augmentation
+@train_task_cli_options.sampler
+@train_task_cli_options.caps_target
+@train_task_cli_options.tsv_target_lab
+@train_task_cli_options.tsv_target_unlab
+@train_task_cli_options.preprocessing_json_target
 # Cross validation
-@training_cli_options.n_splits
-@training_cli_options.split
+@train_task_cli_options.n_splits
+@train_task_cli_options.split
 # Optimization
-@training_cli_options.optimizer
-@training_cli_options.epochs
-@training_cli_options.learning_rate
-@training_cli_options.adaptive_learning_rate
-@training_cli_options.weight_decay
-@training_cli_options.dropout
-@training_cli_options.patience
-@training_cli_options.tolerance
-@training_cli_options.accumulation_steps
-@training_cli_options.profiler
-@training_cli_options.track_exp
+@train_task_cli_options.optimizer
+@train_task_cli_options.epochs
+@train_task_cli_options.learning_rate
+@train_task_cli_options.adaptive_learning_rate
+@train_task_cli_options.weight_decay
+@train_task_cli_options.dropout
+@train_task_cli_options.patience
+@train_task_cli_options.tolerance
+@train_task_cli_options.accumulation_steps
+@train_task_cli_options.profiler
+@train_task_cli_options.track_exp
 # transfer learning
-@training_cli_options.transfer_path
-@training_cli_options.transfer_selection_metric
-@training_cli_options.nb_unfrozen_layer
+@train_task_cli_options.transfer_path
+@train_task_cli_options.transfer_selection_metric
+@train_task_cli_options.nb_unfrozen_layer
 # Task-related
 @reconstruction_cli_options.selection_metrics
 @reconstruction_cli_options.loss
 # information
-@training_cli_options.emissions_calculator
+@train_task_cli_options.emissions_calculator
 def cli(**kwargs):
     """
     Train a deep learning model to learn a reconstruction task on neuroimaging data.
