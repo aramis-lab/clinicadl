1dadc84e1a0a717963a6c7a6e006285ea438d02b
thibaultdvx <154365476+thibaultdvx@users.noreply.github.com>
2024-05-16 11:14:52 +0200

    changes task config so that they inherit from training configs

--- clinicadl/train/tasks/regression/regression_config.py
+++ clinicadl/train/tasks/regression/regression_config.py
@@ -2,14 +2,17 @@ from enum import Enum
 from logging import getLogger
 from typing import Tuple
 
-from pydantic import PrivateAttr, field_validator
+from pydantic import computed_field, field_validator
 
-from clinicadl.train.tasks import BaseTaskConfig, Task
+from clinicadl.train.trainer import DataConfig as BaseDataConfig
+from clinicadl.train.trainer import ModelConfig as BaseModelConfig
+from clinicadl.train.trainer import Task, TrainingConfig
+from clinicadl.train.trainer import ValidationConfig as BaseValidationConfig
 
 logger = getLogger("clinicadl.regression_config")
 
 
-class RegressionLoss(str, Enum):
+class RegressionLoss(str, Enum):  # TODO : put in loss module
     """Available regression losses in ClinicaDL."""
 
     L1Loss = "L1Loss"
@@ -20,7 +23,7 @@ class RegressionLoss(str, Enum):
     SmoothL1Loss = "SmoothL1Loss"
 
 
-class RegressionMetric(str, Enum):
+class RegressionMetric(str, Enum):  # TODO : put in metric module
     """Available regression metrics in ClinicaDL."""
 
     R2_score = "R2_score"
@@ -29,15 +32,31 @@ class RegressionMetric(str, Enum):
     LOSS = "loss"
 
 
-class RegressionConfig(BaseTaskConfig):
-    """Config class to handle parameters of the regression task."""
+class DataConfig(BaseDataConfig):  # TODO : put in data module
+    """Config class to specify the data in regression mode."""
+
+    label: str = "diagnosis"
+
+    @field_validator("label")
+    def validator_label(cls, v):
+        return v  # TODO : check if label in columns
+
+
+class ModelConfig(BaseModelConfig):  # TODO : put in model module
+    """Config class for regression models."""
 
     architecture: str = "Conv5_FC3"
     loss: RegressionLoss = RegressionLoss.MSELoss
-    label: str = "age"
+
+    @field_validator("architecture")
+    def validator_architecture(cls, v):
+        return v  # TODO : connect to network module to have list of available architectures
+
+
+class ValidationConfig(BaseValidationConfig):
+    """Config class for the validation procedure in regression mode."""
+
     selection_metrics: Tuple[RegressionMetric, ...] = (RegressionMetric.LOSS,)
-    # private
-    _network_task: Task = PrivateAttr(default=Task.REGRESSION)
 
     @field_validator("selection_metrics", mode="before")
     def list_to_tuples(cls, v):
@@ -45,10 +64,23 @@ class RegressionConfig(BaseTaskConfig):
             return tuple(v)
         return v
 
-    @field_validator("architecture")
-    def validator_architecture(cls, v):
-        return v  # TODO : connect to network module to have list of available architectures
 
-    @field_validator("label")
-    def validator_label(cls, v):
-        return v  # TODO : check if column is in labels
+class RegressionConfig(TrainingConfig):
+    """
+    Config class for the training of a regression model.
+
+    The user must specified at least the following arguments:
+    - caps_directory
+    - preprocessing_json
+    - tsv_directory
+    - output_maps_directory
+    """
+
+    data: DataConfig
+    model: ModelConfig
+    validation: ValidationConfig
+
+    @computed_field
+    @property
+    def network_task(self) -> Task:
+        return Task.REGRESSION
