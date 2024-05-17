3dd3d6a934c4c4fe331e9f047c1094065dbde4fd
thibaultdvx <154365476+thibaultdvx@users.noreply.github.com>
2024-05-17 11:41:52 +0200

    correction in unittests

--- tests/unittests/train/test_utils.py
+++ tests/unittests/train/test_utils.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from clinicadl.train.tasks import Task
+from clinicadl.train.trainer import Task
 
 expected_classification = {
     "architecture": "default",
