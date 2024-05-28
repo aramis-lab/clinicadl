4281c73a96a9b6188059e1285421202bc5f979e1
thibaultdvx <154365476+thibaultdvx@users.noreply.github.com>
2024-05-28 10:42:45 +0200

    add simulate-gpu option

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,10 +14,19 @@ def pytest_addoption(parser):
         action="store",
         help="Directory for (only-read) inputs for tests",
     )
+    parser.addoption(
+        "--simulate-gpu",
+        action="store_true",
+        help="""To simulate the presence of a gpu on a cpu-only device. Default is False.
+            To use carefully, only to run tests locally. Should not be used in final CI tests.
+            Concretely, the tests won't fail if gpu option if false in the output MAPS whereas
+            it should be true.""",
+    )
 
 
 @pytest.fixture
 def cmdopt(request):
     config_param = {}
     config_param["input"] = request.config.getoption("--input_data_directory")
+    config_param["simulate gpu"] = request.config.getoption("--simulate-gpu")
     return config_param
