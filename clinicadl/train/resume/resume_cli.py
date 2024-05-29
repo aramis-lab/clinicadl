d1abaa49f5729bfe467aa7dc700d883cf5670560
camillebrianceau <camille.brianceau@gmail.com>
2024-05-29 10:01:10 +0200

    tests

--- clinicadl/train/resume/resume_cli.py
+++ clinicadl/train/resume/resume_cli.py
@@ -9,11 +9,11 @@ from clinicadl.config.options import (
 @click.command(name="resume", no_args_is_help=True)
 @arguments.input_maps
 @cross_validation.split
-def cli(**kwargs):
+def cli(input_maps_directory, split):
     """Resume training job in specified maps.
 
     INPUT_MAPS_DIRECTORY is the path to the MAPS folder where training job has started.
     """
     from .resume import automatic_resume
 
-    automatic_resume(kwargs["input_maps_directory"], user_split_list=kwargs["split"])
+    automatic_resume(input_maps_directory, user_split_list=split)
