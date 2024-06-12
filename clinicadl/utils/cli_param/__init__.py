a36e285fba7c92d33c04a5a9560fc413b39b676d
thibaultdvx <154365476+thibaultdvx@users.noreply.github.com>
2024-06-12 11:37:59 +0200

    trigger tests

--- clinicadl/utils/cli_param/__init__.py
+++ clinicadl/utils/cli_param/__init__.py
@@ -1,2 +1,2 @@
 from . import argument, option, option_group
-# TODO: remove this folder when all the options are in commandline folder
+# TODO : remove this folder when all the options are in commandline folder
