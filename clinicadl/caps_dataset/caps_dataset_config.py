8afec8a685c60b16eea7c3b961428d90c3ddbf03
camillebrianceau <camille.brianceau@gmail.com>
2024-06-12 11:35:25 +0200

    restore preprocessing dict

--- clinicadl/caps_dataset/caps_dataset_config.py
+++ clinicadl/caps_dataset/caps_dataset_config.py
@@ -1,6 +1,6 @@
-from typing import Optional, Union
+from typing import Any, Dict, Optional, Union
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, computed_field
 
 from clinicadl.caps_dataset.data_config import DataConfig
 from clinicadl.caps_dataset.dataloader_config import DataLoaderConfig
@@ -109,27 +109,28 @@ class CapsDatasetConfig(CapsDatasetBase):
             transforms=TransformsConfig(**kwargs),
         )
 
-    # @computed_field
-    # @property
-    # def preprocessing_dict(self) -> Dict[str, Any]:
-    #     """
-    #     Gets the preprocessing dictionary from a preprocessing json file.
+    @computed_field
+    @property
+    def preprocessing_dict(self) -> Dict[str, Any]:
+        """
+        Gets the preprocessing dictionary from a preprocessing json file.
 
-    #     Returns
-    #     -------
-    #     Dict[str, Any]
-    #         The preprocessing dictionary.
+        Returns
+        -------
+        Dict[str, Any]
+            The preprocessing dictionary.
 
-    #     Raises
-    #     ------
-    #     ValueError
-    #         In case of multi-cohort dataset, if no preprocessing file is found in any CAPS.
-    #     """
+        Raises
+        ------
+        ValueError
+            In case of multi-cohort dataset, if no preprocessing file is found in any CAPS.
+        """
+        preprocessing_dict = read_preprocessing(self.data.preprocessing_json)
 
-    #     if (
-    #         preprocessing_dict["mode"] == "roi"
-    #         and "roi_background_value" not in preprocessing_dict
-    #     ):
-    #         preprocessing_dict["roi_background_value"] = 0
+        if (
+            preprocessing_dict["mode"] == "roi"
+            and "roi_background_value" not in preprocessing_dict
+        ):
+            preprocessing_dict["roi_background_value"] = 0
 
-    #     return preprocessing_dict
+        return preprocessing_dict
