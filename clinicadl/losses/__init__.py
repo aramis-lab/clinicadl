068e7ed1bed0fd1672a4fa6f9b5daf0cf024ecdb
thibaultdvx <thibault.devarax@icm-institute.org>
2024-09-19 15:20:35 +0200

    fix factory

--- clinicadl/losses/__init__.py
+++ clinicadl/losses/__init__.py
@@ -1,2 +1,3 @@
-from .config import ClassificationLoss, ImplementedLoss, LossConfig
+from .config import create_loss_config
+from .enum import ClassificationLoss, ImplementedLoss
 from .factory import get_loss_function
