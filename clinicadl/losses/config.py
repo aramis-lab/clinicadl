fd87aa822cd247cef57c897e77ba918947189866
thibaultdvx <thibault.devarax@icm-institute.org>
2024-09-19 15:20:18 +0200

    create a config class foar all losses

--- clinicadl/losses/config.py
+++ clinicadl/losses/config.py
@@ -1,96 +1,64 @@
-from enum import Enum
-from typing import List, Optional, Union
+from abc import ABC, abstractmethod
+from typing import Any, List, Optional, Type, Union
 
 from pydantic import (
     BaseModel,
     ConfigDict,
     NonNegativeFloat,
     PositiveFloat,
+    computed_field,
     field_validator,
-    model_validator,
 )
 
-from clinicadl.utils.enum import BaseEnum
 from clinicadl.utils.factories import DefaultFromLibrary
 
+from .enum import ImplementedLoss, Order, Reduction
 
-class ClassificationLoss(str, BaseEnum):
-    """Losses that can be used only for classification."""
-
-    CROSS_ENTROPY = "CrossEntropyLoss"  # for multi-class classification, inputs are unormalized logits and targets are int (same dimension without the class channel)
-    MULTI_MARGIN = "MultiMarginLoss"  # no particular restriction on the input, targets are int (same dimension without th class channel)
-    BCE = "BCELoss"  # for binary classification, targets and inputs should be probabilities and have same shape
-    BCE_LOGITS = "BCEWithLogitsLoss"  # for binary classification, targets should be probabilities and inputs logits, and have the same shape. More stable numerically
-
-
-class ImplementedLoss(str, Enum):
-    """Implemented losses in ClinicaDL."""
-
-    CROSS_ENTROPY = "CrossEntropyLoss"
-    MULTI_MARGIN = "MultiMarginLoss"
-    BCE = "BCELoss"
-    BCE_LOGITS = "BCEWithLogitsLoss"
-    L1 = "L1Loss"
-    MSE = "MSELoss"
-    HUBER = "HuberLoss"
-    SMOOTH_L1 = "SmoothL1Loss"
-    KLDIV = "KLDivLoss"  # if log_target=False, target must be positive
-
-    @classmethod
-    def _missing_(cls, value):
-        raise ValueError(
-            f"{value} is not implemented. Implemented losses are: "
-            + ", ".join([repr(m.value) for m in cls])
-        )
+__all__ = [
+    "LossConfig",
+    "NLLConfig",
+    "CrossEntropyConfig",
+    "BCEConfig",
+    "BCEWithLogitsConfig",
+    "MultiMarginConfig",
+    "KLDivConfig",
+    "HuberConfig",
+    "SmoothL1Config",
+    "L1Config",
+    "MSEConfig",
+    "create_loss_config",
+]
 
 
-class Reduction(str, Enum):
-    """Supported reduction method in ClinicaDL."""
+class LossConfig(BaseModel, ABC):
+    """Base config class for the loss function."""
 
-    MEAN = "mean"
-    SUM = "sum"
-
-
-class Order(int, Enum):
-    """Supported order of L-norm for MultiMarginLoss."""
-
-    ONE = 1
-    TWO = 2
-
-
-class LossConfig(BaseModel):
-    """Config class to configure the loss function."""
-
-    loss: ImplementedLoss = ImplementedLoss.MSE
     reduction: Union[Reduction, DefaultFromLibrary] = DefaultFromLibrary.YES
-    delta: Union[PositiveFloat, DefaultFromLibrary] = DefaultFromLibrary.YES
-    beta: Union[PositiveFloat, DefaultFromLibrary] = DefaultFromLibrary.YES
-    p: Union[Order, DefaultFromLibrary] = DefaultFromLibrary.YES
-    margin: Union[float, DefaultFromLibrary] = DefaultFromLibrary.YES
     weight: Union[
         Optional[List[NonNegativeFloat]], DefaultFromLibrary
-    ] = DefaultFromLibrary.YES  # a weight for each class
-    ignore_index: Union[int, DefaultFromLibrary] = DefaultFromLibrary.YES
-    label_smoothing: Union[
-        NonNegativeFloat, DefaultFromLibrary
     ] = DefaultFromLibrary.YES
-    log_target: Union[bool, DefaultFromLibrary] = DefaultFromLibrary.YES
-    pos_weight: Union[
-        Optional[List[NonNegativeFloat]], DefaultFromLibrary
-    ] = DefaultFromLibrary.YES  # a positive weight for each class
     # pydantic config
     model_config = ConfigDict(
         validate_assignment=True, use_enum_values=True, validate_default=True
     )
 
-    @field_validator("label_smoothing")
-    @classmethod
-    def validator_label_smoothing(cls, v):
-        if isinstance(v, float):
-            assert (
-                0 <= v <= 1
-            ), f"label_smoothing must be between 0 and 1 but it has been set to {v}."
-        return v
+    @computed_field
+    @property
+    @abstractmethod
+    def loss(self) -> str:
+        """The name of the loss."""
+
+
+class NLLConfig(LossConfig):
+    """Config class for Negative Log Likelihood loss."""
+
+    ignore_index: Union[int, DefaultFromLibrary] = DefaultFromLibrary.YES
+
+    @computed_field
+    @property
+    def loss(self) -> str:
+        """The name of the loss."""
+        return "NLLLoss"
 
     @field_validator("ignore_index")
     @classmethod
@@ -101,17 +69,173 @@ class LossConfig(BaseModel):
             ), "ignore_index must be a positive int (or -100 when disabled)."
         return v
 
-    @model_validator(mode="after")
-    def model_validator(self):
-        if (
-            self.loss == ImplementedLoss.BCE_LOGITS
-            and self.weight is not None
-            and self.weight != DefaultFromLibrary.YES
-        ):
-            raise ValueError("Cannot use weight with BCEWithLogitsLoss.")
-        elif (
-            self.loss == ImplementedLoss.BCE
-            and self.weight is not None
-            and self.weight != DefaultFromLibrary.YES
-        ):
-            raise ValueError("Cannot use weight with BCELoss.")
+
+class CrossEntropyConfig(NLLConfig):
+    """Config class for Cross Entropy loss."""
+
+    label_smoothing: Union[
+        NonNegativeFloat, DefaultFromLibrary
+    ] = DefaultFromLibrary.YES
+
+    @computed_field
+    @property
+    def loss(self) -> str:
+        """The name of the loss."""
+        return "CrossEntropyLoss"
+
+    @field_validator("label_smoothing")
+    @classmethod
+    def validator_label_smoothing(cls, v):
+        if isinstance(v, float):
+            assert (
+                0 <= v <= 1
+            ), f"label_smoothing must be between 0 and 1 but it has been set to {v}."
+        return v
+
+
+class BCEConfig(LossConfig):
+    """Config class for Binary Cross Entropy loss."""
+
+    weight: Optional[List[NonNegativeFloat]] = None
+
+    @computed_field
+    @property
+    def loss(self) -> str:
+        """The name of the loss."""
+        return "BCELoss"
+
+    @field_validator("weight")
+    @classmethod
+    def validator_weight(cls, v):
+        if v is not None:
+            raise ValueError(
+                "Cannot use weight with BCEWithLogitsLoss. If you want more flexibility, please use API mode."
+            )
+        return v
+
+
+class BCEWithLogitsConfig(BCEConfig):
+    """Config class for Binary Cross Entropy With Logits loss."""
+
+    pos_weight: Union[Optional[List[Any]], DefaultFromLibrary] = DefaultFromLibrary.YES
+
+    @computed_field
+    @property
+    def loss(self) -> str:
+        """The name of the loss."""
+        return "BCEWithLogitsLoss"
+
+    @field_validator("pos_weight")
+    @classmethod
+    def validator_pos_weight(cls, v):
+        if isinstance(v, list):
+            check = cls._recursive_float_check(v)
+            if not check:
+                raise ValueError(
+                    f"elements in pos_weight must be non-negative float, got: {v}"
+                )
+        return v
+
+    @classmethod
+    def _recursive_float_check(cls, item):
+        if isinstance(item, list):
+            return all(cls._recursive_float_check(i) for i in item)
+        else:
+            return (isinstance(item, float) or isinstance(item, int)) and item >= 0
+
+
+class MultiMarginConfig(LossConfig):
+    """Config class for Multi Margin loss."""
+
+    p: Union[Order, DefaultFromLibrary] = DefaultFromLibrary.YES
+    margin: Union[float, DefaultFromLibrary] = DefaultFromLibrary.YES
+
+    @computed_field
+    @property
+    def loss(self) -> str:
+        """The name of the loss."""
+        return "MultiMarginLoss"
+
+
+class KLDivConfig(LossConfig):
+    """Config class for Kullback-Leibler Divergence loss."""
+
+    log_target: Union[bool, DefaultFromLibrary] = DefaultFromLibrary.YES
+
+    @computed_field
+    @property
+    def loss(self) -> str:
+        """The name of the loss."""
+        return "KLDivLoss"
+
+
+class HuberConfig(LossConfig):
+    """Config class for Huber loss."""
+
+    delta: Union[PositiveFloat, DefaultFromLibrary] = DefaultFromLibrary.YES
+
+    @computed_field
+    @property
+    def loss(self) -> str:
+        """The name of the loss."""
+        return "HuberLoss"
+
+
+class SmoothL1Config(LossConfig):
+    """Config class for Smooth L1 loss."""
+
+    beta: Union[NonNegativeFloat, DefaultFromLibrary] = DefaultFromLibrary.YES
+
+    @computed_field
+    @property
+    def loss(self) -> str:
+        """The name of the loss."""
+        return "SmoothL1Loss"
+
+
+class L1Config(LossConfig):
+    """Config class for L1 loss."""
+
+    @computed_field
+    @property
+    def loss(self) -> str:
+        """The name of the loss."""
+        return "L1Loss"
+
+
+class MSEConfig(LossConfig):
+    """Config class for Mean Squared Error loss."""
+
+    @computed_field
+    @property
+    def loss(self) -> str:
+        """The name of the loss."""
+        return "MSELoss"
+
+
+def create_loss_config(
+    loss: Union[str, ImplementedLoss],
+) -> Type[LossConfig]:
+    """
+    A factory function to create a config class suited for the loss.
+
+    Parameters
+    ----------
+    loss : Union[str, ImplementedLoss]
+        The name of the loss.
+
+    Returns
+    -------
+    Type[LossConfig]
+        The config class.
+
+    Raises
+    ------
+    ValueError
+        If `loss` is not supported.
+    """
+    loss = ImplementedLoss(loss)
+    config_name = "".join([loss.replace(" ", ""), "Config"])
+    config = globals()[config_name]
+
+    return config
