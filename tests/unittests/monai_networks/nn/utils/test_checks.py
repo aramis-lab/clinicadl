586cc9c7909c7b3605ee067bcba5621c2836b171
thibaultdvx <thibault.devarax@icm-institute.org>
2024-10-01 17:00:56 +0200

    test utils

---
+++ tests/unittests/monai_networks/nn/utils/test_checks.py
@@ -0,0 +1,112 @@
+import pytest
+
+from clinicadl.monai_networks.nn.utils.checks import (
+    _check_layer_parameter,
+    check_conv_args,
+    check_mlp_args,
+    check_norm_layer,
+    check_pool_indices,
+    ensure_list_of_tuples,
+)
+
+
+@pytest.mark.parametrize(
+    "parameter,expected_output",
+    [
+        (5, (5, 5, 5)),
+        ((5, 4, 4), (5, 4, 4)),
+        ([5, 4], [(5, 5, 5), (4, 4, 4)]),
+        ([5, (4, 3, 3)], [(5, 5, 5), (4, 3, 3)]),
+        ((5, 5), None),
+        ([5, 5, 5], None),
+        ([5, (4, 4)], None),
+        (5.0, None),
+    ],
+)
+def test_check_layer_parameter(parameter, expected_output):
+    if expected_output:
+        assert (
+            _check_layer_parameter(parameter, dim=3, n_layers=2, name="abc")
+            == expected_output
+        )
+    else:
+        with pytest.raises(ValueError):
+            _check_layer_parameter(parameter, dim=3, n_layers=2, name="abc")
+
+
+@pytest.mark.parametrize(
+    "parameter,expected_output",
+    [
+        (5, [(5, 5, 5), (5, 5, 5)]),
+        ((5, 4, 4), [(5, 4, 4), (5, 4, 4)]),
+        ([5, 4], [(5, 5, 5), (4, 4, 4)]),
+        ([5, (4, 3, 3)], [(5, 5, 5), (4, 3, 3)]),
+    ],
+)
+def test_ensure_list_of_tuples(parameter, expected_output):
+    assert (
+        ensure_list_of_tuples(parameter, dim=3, n_layers=2, name="abc")
+        == expected_output
+    )
+
+
+@pytest.mark.parametrize(
+    "indices,n_layers,error",
+    [
+        ([0, 1, 2], 4, False),
+        ([0, 1, 2], 3, True),
+        ([0, 1, 2], 2, True),
+    ],
+)
+def test_check_pool_indices(indices, n_layers, error):
+    if error:
+        with pytest.raises(ValueError):
+            _ = check_pool_indices(indices, n_layers)
+    else:
+        check_pool_indices(indices, n_layers)
+
+
+@pytest.mark.parametrize(
+    "inputs,error",
+    [
+        (None, False),
+        ("abc", True),
+        ("batch", False),
+        ("group", True),
+        (("batch",), True),
+        (("batch", 3), True),
+        (("batch", {"eps": 0.1}), False),
+        (("group", {"num_groups": 2}), False),
+        (("group", {"num_groups": 2, "eps": 0.1}), False),
+    ],
+)
+def test_check_norm_layer(inputs, error):
+    if error:
+        with pytest.raises(ValueError):
+            _ = check_norm_layer(inputs)
+    else:
+        assert check_norm_layer(inputs) == inputs
+
+
+@pytest.mark.parametrize(
+    "conv_args,error",
+    [(None, True), ({"kernel_size": 3}, True), ({"channels": [2]}, False)],
+)
+def test_check_conv_args(conv_args, error):
+    if error:
+        with pytest.raises(ValueError):
+            check_conv_args(conv_args)
+    else:
+        check_conv_args(conv_args)
+
+
+@pytest.mark.parametrize(
+    "mlp_args,error",
+    [({"act": "tanh"}, True), ({"hidden_channels": [2]}, False)],
+)
+def test_check_mlp_args(mlp_args, error):
+    if error:
+        with pytest.raises(ValueError):
+            check_mlp_args(mlp_args)
+    else:
+        check_mlp_args(mlp_args)
---
+++ tests/unittests/monai_networks/nn/utils/test_shapes.py
@@ -0,0 +1,240 @@
+import pytest
+import torch
+
+from clinicadl.monai_networks.nn.utils.shapes import (
+    _calculate_avgpool_out_shape,
+    _calculate_maxpool_out_shape,
+    _calculate_upsample_out_shape,
+    calculate_conv_out_shape,
+    calculate_convtranspose_out_shape,
+    calculate_pool_out_shape,
+    calculate_unpool_out_shape,
+)
+
+INPUT_1D = torch.randn(2, 1, 10)
+INPUT_2D = torch.randn(2, 1, 32, 32)
+INPUT_3D = torch.randn(2, 1, 20, 21, 22)
+
+
+@pytest.mark.parametrize(
+    "input_tensor,kernel_size,stride,padding,dilation",
+    [
+        (INPUT_3D, 7, 2, (1, 2, 3), 3),
+        (INPUT_2D, (5, 3), 1, 0, (2, 2)),
+        (INPUT_1D, 3, 1, 2, 1),
+    ],
+)
+def test_calculate_conv_out_shape(input_tensor, kernel_size, stride, padding, dilation):
+    in_shape = input_tensor.shape[2:]
+    dim = len(input_tensor.shape[2:])
+    args = {
+        "in_channels": 1,
+        "out_channels": 1,
+        "kernel_size": kernel_size,
+        "stride": stride,
+        "padding": padding,
+        "dilation": dilation,
+    }
+    if dim == 1:
+        conv = torch.nn.Conv1d
+    elif dim == 2:
+        conv = torch.nn.Conv2d
+    else:
+        conv = torch.nn.Conv3d
+
+    output_shape = conv(**args)(input_tensor).shape[2:]
+    assert (
+        calculate_conv_out_shape(in_shape, kernel_size, stride, padding, dilation)
+        == output_shape
+    )
+
+
+@pytest.mark.parametrize(
+    "input_tensor,kernel_size,stride,padding,dilation,output_padding",
+    [
+        (INPUT_3D, 7, 2, (1, 2, 3), 3, 0),
+        (INPUT_2D, (5, 3), 1, 0, (2, 2), (1, 0)),
+        (INPUT_1D, 3, 3, 2, 1, 2),
+    ],
+)
+def test_calculate_convtranspose_out_shape(
+    input_tensor, kernel_size, stride, padding, dilation, output_padding
+):
+    in_shape = input_tensor.shape[2:]
+    dim = len(input_tensor.shape[2:])
+    args = {
+        "in_channels": 1,
+        "out_channels": 1,
+        "kernel_size": kernel_size,
+        "stride": stride,
+        "padding": padding,
+        "dilation": dilation,
+        "output_padding": output_padding,
+    }
+    if dim == 1:
+        conv = torch.nn.ConvTranspose1d
+    elif dim == 2:
+        conv = torch.nn.ConvTranspose2d
+    else:
+        conv = torch.nn.ConvTranspose3d
+
+    output_shape = conv(**args)(input_tensor).shape[2:]
+    assert (
+        calculate_convtranspose_out_shape(
+            in_shape, kernel_size, stride, padding, output_padding, dilation
+        )
+        == output_shape
+    )
+
+
+@pytest.mark.parametrize(
+    "input_tensor,kernel_size,stride,padding,dilation,ceil_mode",
+    [
+        (INPUT_3D, 7, 2, (1, 2, 3), 3, False),
+        (INPUT_3D, 7, 2, (1, 2, 3), 3, True),
+        (INPUT_2D, (5, 3), 1, 0, (2, 2), False),
+        (INPUT_2D, (5, 3), 1, 0, (2, 2), True),
+        (INPUT_1D, 2, 1, 1, 1, False),
+        (INPUT_1D, 2, 1, 1, 1, True),
+    ],
+)
+def test_calculate_maxpool_out_shape(
+    input_tensor, kernel_size, stride, padding, dilation, ceil_mode
+):
+    in_shape = input_tensor.shape[2:]
+    dim = len(input_tensor.shape[2:])
+    args = {
+        "kernel_size": kernel_size,
+        "stride": stride,
+        "padding": padding,
+        "dilation": dilation,
+        "ceil_mode": ceil_mode,
+    }
+    if dim == 1:
+        max_pool = torch.nn.MaxPool1d
+    elif dim == 2:
+        max_pool = torch.nn.MaxPool2d
+    else:
+        max_pool = torch.nn.MaxPool3d
+
+    output_shape = max_pool(**args)(input_tensor).shape[2:]
+    assert (
+        _calculate_maxpool_out_shape(
+            in_shape, kernel_size, stride, padding, dilation, ceil_mode=ceil_mode
+        )
+        == output_shape
+    )
+
+
+@pytest.mark.parametrize(
+    "input_tensor,kernel_size,stride,padding,ceil_mode",
+    [
+        (INPUT_3D, 7, 2, (1, 2, 3), False),
+        (INPUT_3D, 7, 2, (1, 2, 3), True),
+        (INPUT_2D, (5, 3), 1, 0, False),
+        (INPUT_2D, (5, 3), 1, 0, True),
+        (INPUT_1D, 2, 1, 1, False),
+        (INPUT_1D, 2, 1, 1, True),
+        (
+            INPUT_1D,
+            2,
+            3,
+            1,
+            True,
+        ),  # special case with ceil_mode (see: https://pytorch.org/docs/stable/generated/torch.nn.AvgPool1d.html)
+    ],
+)
+def test_calculate_avgpool_out_shape(
+    input_tensor, kernel_size, stride, padding, ceil_mode
+):
+    in_shape = input_tensor.shape[2:]
+    dim = len(input_tensor.shape[2:])
+    args = {
+        "kernel_size": kernel_size,
+        "stride": stride,
+        "padding": padding,
+        "ceil_mode": ceil_mode,
+    }
+    if dim == 1:
+        avg_pool = torch.nn.AvgPool1d
+    elif dim == 2:
+        avg_pool = torch.nn.AvgPool2d
+    else:
+        avg_pool = torch.nn.AvgPool3d
+    output_shape = avg_pool(**args)(input_tensor).shape[2:]
+    assert (
+        _calculate_avgpool_out_shape(
+            in_shape, kernel_size, stride, padding, ceil_mode=ceil_mode
+        )
+        == output_shape
+    )
+
+
+def test_calculate_pool_out_shape():
+    in_shape = INPUT_3D.shape[2:]
+    assert calculate_pool_out_shape(
+        pool_mode="max",
+        in_shape=in_shape,
+        kernel_size=7,
+        stride=2,
+        padding=(1, 2, 3),
+        dilation=3,
+        ceil_mode=True,
+    ) == (3, 4, 6)
+    assert calculate_pool_out_shape(
+        pool_mode="avg",
+        in_shape=in_shape,
+        kernel_size=7,
+        stride=2,
+        padding=(1, 2, 3),
+        ceil_mode=True,
+    ) == (9, 10, 12)
+    with pytest.raises(ValueError):
+        calculate_pool_out_shape(
+            pool_mode="abc",
+            in_shape=in_shape,
+            kernel_size=7,
+            stride=2,
+            padding=(1, 2, 3),
+            dilation=3,
+            ceil_mode=True,
+        )
+
+
+@pytest.mark.parametrize(
+    "input_tensor,kwargs",
+    [
+        (INPUT_3D, {"scale_factor": 2}),
+        (INPUT_2D, {"size": (40, 40)}),
+        (INPUT_1D, {"scale_factor": 3}),
+    ],
+)
+def test_calculate_upsample_out_shape(input_tensor, kwargs):
+    in_shape = input_tensor.shape[2:]
+    unpool = torch.nn.Upsample(**kwargs)
+
+    output_shape = unpool(input_tensor).shape[2:]
+    assert _calculate_upsample_out_shape(in_shape, **kwargs) == output_shape
+
+
+def test_calculate_unpool_out_shape():
+    in_shape = INPUT_3D.shape[2:]
+    assert calculate_unpool_out_shape(
+        unpool_mode="convtranspose",
+        in_shape=in_shape,
+        kernel_size=5,
+        stride=1,
+        padding=0,
+        output_padding=0,
+        dilation=1,
+    ) == (24, 25, 26)
+    assert calculate_unpool_out_shape(
+        unpool_mode="upsample",
+        in_shape=in_shape,
+        scale_factor=2,
+    ) == (40, 42, 44)
+    with pytest.raises(ValueError):
+        calculate_unpool_out_shape(
+            unpool_mode="abc",
+            in_shape=in_shape,
+        )
