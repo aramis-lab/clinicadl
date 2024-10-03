7382e97db1cd1bde0b1b8c25a13bf55abe6836e6
camillebrianceau <camille.brianceau@gmail.com>
2024-10-03 16:24:50 +0200

    change options

--- clinicadl/commandline/modules_options/cross_validation.py
+++ clinicadl/commandline/modules_options/cross_validation.py
@@ -2,13 +2,13 @@ import click
 
 from clinicadl.config.config_utils import get_default_from_config_class as get_default
 from clinicadl.config.config_utils import get_type_from_config_class as get_type
-from clinicadl.splitter.validation import ValidationConfig
+from clinicadl.splitter.config import SplitConfig
 
 # Cross Validation
 n_splits = click.option(
     "--n_splits",
-    type=get_type("n_splits", ValidationConfig),
-    default=get_default("n_splits", ValidationConfig),
+    type=get_type("n_splits", SplitConfig),
+    default=get_default("n_splits", SplitConfig),
     help="If a value is given for k will load data of a k-fold CV. "
     "Default value (0) will load a single split.",
     show_default=True,
@@ -17,7 +17,7 @@ split = click.option(
     "--split",
     "-s",
     type=int,  # get_type("split", config.ValidationConfig),
-    default=get_default("split", ValidationConfig),
+    default=get_default("split", SplitConfig),
     multiple=True,
     help="Train the list of given splits. By default, all the splits are trained.",
     show_default=True,
