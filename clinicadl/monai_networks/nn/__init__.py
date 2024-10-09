24157c6015504d21ea388f58f886f89bacd83f6e
thibaultdvx <thibault.devarax@icm-institute.org>
2024-10-09 13:43:43 +0200

    densenet

--- clinicadl/monai_networks/nn/__init__.py
+++ clinicadl/monai_networks/nn/__init__.py
@@ -2,6 +2,7 @@ from .autoencoder import AutoEncoder
 from .cnn import CNN
 from .conv_decoder import ConvDecoder
 from .conv_encoder import ConvEncoder
+from .densenet import DenseNet, get_densenet
 from .generator import Generator
 from .mlp import MLP
 from .vae import VAE
