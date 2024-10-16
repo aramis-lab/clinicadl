418ccffb99281a989e36a0a1c1bcdba5f470b367
thibaultdvx <thibault.devarax@icm-institute.org>
2024-10-16 15:07:45 +0200

    fix issue with enum

--- clinicadl/trainer/tasks_utils.py
+++ clinicadl/trainer/tasks_utils.py
@@ -1,31 +1,21 @@
-from abc import abstractmethod
 from typing import Any, Dict, List, Optional, Sequence, Tuple, Type, Union
 
 import numpy as np
 import pandas as pd
 import torch
-import torch.distributed as dist
-from pydantic import (
-    BaseModel,
-    ConfigDict,
-    computed_field,
-    model_validator,
-)
 from torch import Tensor, nn
-from torch.amp import autocast
 from torch.nn.functional import softmax
-from torch.nn.modules.loss import _Loss
-from torch.utils.data import DataLoader, Sampler, sampler
+from torch.utils.data import Sampler, sampler
 from torch.utils.data.distributed import DistributedSampler
 
 from clinicadl.dataset.caps_dataset import CapsDataset
 from clinicadl.metrics.old_metrics.metric_module import MetricModule
 from clinicadl.networks.old_network.network import Network
 from clinicadl.trainer.config.train import TrainConfig
-from clinicadl.utils import cluster
 from clinicadl.utils.enum import (
     ClassificationLoss,
     ClassificationMetric,
+    Mode,
     ReconstructionLoss,
     ReconstructionMetric,
     RegressionLoss,
@@ -249,7 +239,7 @@ def save_outputs(network_task: Union[str, Task]):
 
 def generate_test_row(
     network_task: Union[str, Task],
-    mode: str,
+    mode: Mode,
     metrics_module,
     n_classes: int,
     idx: int,
@@ -274,7 +264,7 @@ def generate_test_row(
             [
                 data["participant_id"][idx],
                 data["session_id"][idx],
-                data[f"{mode}_id"][idx].item(),
+                data[f"{mode.value}_id"][idx].item(),
                 data["label"][idx].item(),
                 prediction,
             ]
@@ -286,7 +276,7 @@ def generate_test_row(
             [
                 data["participant_id"][idx],
                 data["session_id"][idx],
-                data[f"{mode}_id"][idx].item(),
+                data[f"{mode.value}_id"][idx].item(),
                 data["label"][idx].item(),
                 outputs[idx].item(),
             ]
@@ -298,7 +288,7 @@ def generate_test_row(
         row = [
             data["participant_id"][idx],
             data["session_id"][idx],
-            data[f"{mode}_id"][idx].item(),
+            data[f"{mode.value}_id"][idx].item(),
         ]
 
         for metric in evaluation_metrics(Task.RECONSTRUCTION):
