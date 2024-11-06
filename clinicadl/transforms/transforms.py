a97769ccf0b143562d8c097fb1c5c560f610272f
camillebrianceau <camille.brianceau@gmail.com>
2024-11-06 13:52:14 +0100

    pb self or cls

--- clinicadl/transforms/transforms.py
+++ clinicadl/transforms/transforms.py
@@ -6,9 +6,9 @@ import torchio
 class Transforms:
     def __init__(
         self,
-        data_augmentation=List[torchio],
-        image_transforms=List[torchio],
-        object_transforms=List[torchio],
+        data_augmentation=List[torchio.Transform],
+        image_transforms=List[torchio.Transform],
+        object_transforms=List[torchio.Transform],
     ) -> None:
         """TO COMPLETE"""
         self.data_augmentation = data_augmentation
