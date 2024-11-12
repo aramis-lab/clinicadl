b8152281018275b4300006b0812142154e1bf2e2
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2024-11-12 11:51:21 +0100

    Update clinicadl/transforms/transforms.py
    Co-authored-by: Thibault de Varax <154365476+thibaultdvx@users.noreply.github.com>

--- clinicadl/transforms/transforms.py
+++ clinicadl/transforms/transforms.py
@@ -6,9 +6,9 @@ import torchio
 class Transforms:
     def __init__(
         self,
-        data_augmentation=List[torchio.Transform],
-        image_transforms=List[torchio.Transform],
-        object_transforms=List[torchio.Transform],
+        data_augmentation=List[Callable],
+        image_transforms=List[Callable],
+        object_transforms=List[Callable],
     ) -> None:
         """TO COMPLETE"""
         self.data_augmentation = data_augmentation
