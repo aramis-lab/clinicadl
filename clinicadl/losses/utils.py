d1cc8679e479bf86477b6c6b36b0c5d033ec1d59
thibaultdvx <thibault.devarax@icm-institute.org>
2024-11-13 09:37:55 +0100

    change loss type

--- clinicadl/losses/utils.py
+++ clinicadl/losses/utils.py
@@ -1,5 +1,5 @@
-from typing import Callable, Tuple
+from typing import Callable
 
 from torch import Tensor
 
-Loss = Callable[[*Tuple[Tensor, ...]], Tensor]
+Loss = Callable[[Tensor, Tensor], Tensor]
