999803e88df147229247a34d8e136ceffc0524da
camillebrianceau <camille.brianceau@gmail.com>
2024-12-04 14:12:32 +0100

    tesst

--- tests/unittests/train/trainer/test_training_config.py
+++ tests/unittests/train/trainer/test_training_config.py
@@ -159,7 +159,6 @@ def good_inputs(dummy_arguments):
 #     assert c.split.split == (0,)
 #     assert c.early_stopping.min_delta == 0.0
 
-
 # Test config manipulation #
 def test_assignment(dummy_arguments, training_config):
     c = training_config(**dummy_arguments)
