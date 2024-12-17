a7e4d9310ee7eba6c264b82de87f565ca810dbf8
thibaultdvx <thibault.devarax@icm-institute.org>
2024-12-17 15:46:13 +0100

    some changes on dataloder part

--- clinicadl/splitter/dataloader.py
+++ clinicadl/splitter/dataloader.py
@@ -1,56 +1,79 @@
 from typing import Optional
 
-from pydantic import BaseModel, ConfigDict, NonNegativeInt, PositiveInt
+from pydantic import NonNegativeInt, PositiveInt
 from torch.utils.data import DataLoader, DistributedSampler, Sampler
 from torch.utils.data import WeightedRandomSampler as BaseWeightedRandomSampler
 
 from clinicadl.dataset.datasets.caps_dataset import CapsDataset
+from clinicadl.utils.config import ClinicaDLConfig
 from clinicadl.utils.seed import pl_worker_init_function
+
+from .defaults import (
+    BATCH_SIZE,
+    DP_DEGREE,
+    DROP_LAST,
+    NUM_WORKERS,
+    PIN_MEMORY,
+    PREFETCH_FACTOR,
+    RANK,
+    SAMPLING_WEIGHTS,
+    SHUFFLE,
+)
 
 
 class WeightedRandomSampler(BaseWeightedRandomSampler):
+    """
+    Modifies PyTorch's WeightedRandomSampler to have a similar behavior to
+    PyTorch's DistributedSampler.
+    """
+
     def set_epoch(self, epoch: int) -> None:
         """
-        Fake method to simulate 'set_epoch' of pytorch's DistributedSampler.
+        Fake method to simulate 'set_epoch' of PyTorch's DistributedSampler.
         To be able to always call sampler.set_epoch(), no matter the sampler.
         """
-        pass
 
 
-class DataLoaderConfig(BaseModel):
-    batch_size: PositiveInt = 10
-    # sampling_weights: Optional[str] = None
-    shuffle: bool = False
-    num_workers: int = 0
-    drop_last: bool = False
-    prefetch_factor: Optional[NonNegativeInt] = None
+class DataLoaderConfig(ClinicaDLConfig):
+    """Config class to parametrize a PyTorch DataLoader."""
 
-    model_config = ConfigDict(validate_assignment=True, arbitrary_types_allowed=True)
+    batch_size: PositiveInt = BATCH_SIZE
+    sampling_weights: Optional[str] = SAMPLING_WEIGHTS
+    shuffle: bool = SHUFFLE
+    drop_last: bool = DROP_LAST
+    num_workers: NonNegativeInt = NUM_WORKERS
+    prefetch_factor: Optional[NonNegativeInt] = PREFETCH_FACTOR
+    pin_memory: bool = PIN_MEMORY
 
     def _generate_sampler(
         self,
         dataset: CapsDataset,
-        sampling_weights: Optional[str] = None,
-        dp_degree: PositiveInt = 1,
-        rank: int = 0,
+        dp_degree: Optional[int] = DP_DEGREE,
+        rank: Optional[int] = RANK,
     ) -> Sampler:
         """
-        Returns sampler according to the wanted options.
-
-        Args:
-            dataset: the dataset.
-            sampling_weights: the column of sample weights in the tsv. If None, no weights
-            will be used.
-            shuffle: if no sampling_weights are passed, whether to shuffle or not.
-            dp_degree: the degree of data parallelism.
-            rank: process id within the data parallelism communicator.
-        Returns:
-            callable given to the training data loader.
+        Returns a WeightedRandomSampler if self.sampling_weights is not None, otherwise a
+        a DistributedSampler, even when data parallelism is not performed (in this case
+        the degree of data parallelism is set to 1, so it is equivalent to a simple PyTorch
+        RandomSampler if self.shuffle is True or no sampler if self.shuffle is False).
         """
-        distributed = rank == 0 or dp_degree == 1
+        if (rank is not None and dp_degree is None) or (
+            dp_degree is not None and rank is None
+        ):
+            raise ValueError(
+                "For data parallelism, none of 'dp_degree' and 'rank' can be None. "
+                f"Got rank={rank} and dp_degree={dp_degree}"
+            )
+        distributed = dp_degree is not None
 
-        if sampling_weights:
-            weights = dataset.df[sampling_weights].values.astype(float)
+        if self.sampling_weights:
+            try:
+                weights = dataset.df[self.sampling_weights].values.astype(float)
+            except KeyError as exc:
+                raise KeyError(
+                    f"Got {self.sampling_weights} for 'sampling_weights' but there is no "
+                    "column named like that in the dataframe of the dataset."
+                ) from exc
             length = (
                 len(weights) // dp_degree + int(rank < len(weights) % dp_degree)
                 if distributed
@@ -58,15 +81,15 @@ class DataLoaderConfig(BaseModel):
             )
             sampler = WeightedRandomSampler(weights, num_samples=length)  # type: ignore
         else:
-            if distributed:
-                rank = 0
+            if not distributed:
                 dp_degree = 1
+                rank = 0
             sampler = DistributedSampler(
                 dataset,
                 num_replicas=dp_degree,
                 rank=rank,
                 shuffle=self.shuffle,
-                drop_last=False,
+                drop_last=False,  # not the same as self.drop_last
             )
 
         return sampler
@@ -74,13 +97,31 @@ class DataLoaderConfig(BaseModel):
     def get_dataloader(
         self,
         dataset: CapsDataset,
-        sampling_weights: Optional[str] = None,
-        dp_degree: PositiveInt = 1,
-        rank: int = 0,
-    ):
+        dp_degree: Optional[int] = DP_DEGREE,
+        rank: Optional[int] = RANK,
+    ) -> DataLoader:
+        """
+        To get a dataloader from a dataset. The dataloader is parametrized
+        with the options stored in this configuration class.
+
+        Parameters
+        ----------
+        dataset : CapsDataset
+            The dataset to put in a Dataloader.
+        dp_degree : Optional[int] (optional, default=None)
+            The degree of data parallelism. None if no data parallelism.
+        rank : Optional[int] (optional, default=None)
+            Process id within the data parallelism communicator.
+            None if no data parallelism.
+
+        Returns
+        -------
+        DataLoader
+            The dataloader that wraps the dataset.
+        """
         loader = DataLoader(
             dataset=dataset,
-            sampler=self._generate_sampler(dataset, sampling_weights, dp_degree, rank),
+            sampler=self._generate_sampler(dataset, dp_degree, rank),
             worker_init_fn=pl_worker_init_function,
             **self.model_dump(),
         )
--- clinicadl/splitter/split.py
+++ clinicadl/splitter/split.py
@@ -1,149 +1,166 @@
-from dataclasses import dataclass
+from pathlib import Path
 from typing import Optional
 
-from pydantic import ConfigDict, NonNegativeInt
+from pydantic import NonNegativeInt
 from torch.utils.data import DataLoader
 
 from clinicadl.dataset.datasets.caps_dataset import CapsDataset
 from clinicadl.splitter.dataloader import DataLoaderConfig
+from clinicadl.utils.config import ClinicaDLConfig
+
+from .defaults import (
+    BATCH_SIZE,
+    DP_DEGREE,
+    DROP_LAST,
+    NUM_WORKERS,
+    PIN_MEMORY,
+    PREFETCH_FACTOR,
+    RANK,
+    SAMPLING_WEIGHTS,
+    SHUFFLE,
+)
 
 
-@dataclass
-class Split:
+class Split(ClinicaDLConfig):
     """Dataclass that contains all the useful info on the split."""
 
-    index: int
+    index: NonNegativeInt
+    split_dir: Path
     train_dataset: CapsDataset
     val_dataset: CapsDataset
     train_loader: Optional[DataLoader] = None
     val_loader: Optional[DataLoader] = None
-
-    model_config = ConfigDict(validate_assignment=True, arbitrary_types_allowed=True)
+    train_loader_config: Optional[DataLoaderConfig] = None
+    val_loader_config: Optional[DataLoaderConfig] = None
 
     def build_train_loader(
         self,
         dataloader_config: Optional[DataLoaderConfig] = None,
         *,
-        batch_size: int = 1,
-        sampling_weights: Optional[str] = None,
-        shuffle: bool = False,
-        num_workers: int = 0,
-        drop_last: bool = False,
-        prefetch_factor: Optional[NonNegativeInt] = None,
-        dp_degree: int = 1,
-        rank: int = 1,
+        batch_size: int = BATCH_SIZE,
+        sampling_weights: Optional[str] = SAMPLING_WEIGHTS,
+        shuffle: bool = SHUFFLE,
+        drop_last: bool = DROP_LAST,
+        num_workers: int = NUM_WORKERS,
+        prefetch_factor: Optional[int] = PREFETCH_FACTOR,
+        pin_memory: bool = PIN_MEMORY,
+        dp_degree: Optional[int] = DP_DEGREE,
+        rank: Optional[int] = RANK,
     ) -> None:
         """
         Build a train loader for the split.
 
         Parameters
         ----------
-        dataloader_config : Optional[DataLoaderConfig], default=None
+        dataloader_config : Optional[DataLoaderConfig] (optional, default=None)
             Pre-configured DataLoader configuration.
-        batch_size : Optional[int], default=None
+        batch_size : int (optional, default=1)
             Batch size for the DataLoader (used if `dataloader_config` is not provided).
-        sampling_weights : Optional[str], default=None
-            Sampling weights for the DataLoader.
-        shuffle : bool, default=False
-            Whether to shuffle the data.
-        num_workers : int, default=0
-            Number of workers for data loading.
-        drop_last : bool, default=False
-            Whether to drop the last incomplete batch.
-        prefetch_factor : Optional[NonNegativeInt], default=None
-            Prefetch factor for the DataLoader.
-        dp_degree : int, default=1
-            Data parallelism degree.
-        rank : int, default=1
-            Rank for distributed data loading.
+        sampling_weights : Optional[str] (optional, default=None)
+            Name of the column in the dataframe of the CapsDatasets where to find the sampling
+            weights (used if `dataloader_config` is not provided).
+        shuffle : bool (optional, default=False)
+            Whether to shuffle the data (used if `dataloader_config` is not provided).
+        drop_last : bool (optional, default=False)
+            Whether to drop the last incomplete batch (used if `dataloader_config` is not provided).
+        num_workers : int (optional, default=0)
+            Number of workers for data loading (used if `dataloader_config` is not provided).
+        prefetch_factor : Optional[int] (optional, default=None)
+            Prefetch factor if num_workers is not 0 (used if `dataloader_config` is not provided).
+        dp_degree : Optional[int] (optional, default=None)
+           The degree of data parallelism. None if no data parallelism.
+        rank : Optional[int] (optional, default=None)
+            Process id within the data parallelism communicator.
+            None if no data parallelism.
 
         Raises
         ------
         ValueError
-            If neither a configuration object nor batch_size is provided.
+            If one of 'dp_degree' and 'rank' is None but the other is not None.
+        KeyError
+            If 'sampling_weights' is passed but there is no such column in the dataframe
+            of the train dataset.
         """
         if dataloader_config:
-            self.train_loader = dataloader_config.get_dataloader(
-                dataset=self.train_dataset
-            )
-        elif batch_size is not None:
-            dataloader = DataLoaderConfig(
+            self.train_loader_config = dataloader_config
+        else:
+            self.train_loader_config = DataLoaderConfig(
                 batch_size=batch_size,
+                sampling_weights=sampling_weights,
                 shuffle=shuffle,
                 num_workers=num_workers,
                 drop_last=drop_last,
                 prefetch_factor=prefetch_factor,
+                pin_memory=pin_memory,
             )
-            self.train_loader = dataloader.get_dataloader(
-                dataset=self.train_dataset,
-                sampling_weights=sampling_weights,
-                dp_degree=dp_degree,
-                rank=rank,
-            )
-        else:
-            raise ValueError(
-                "Either a DataLoaderConfig or batch_size must be provided."
-            )
+        self.train_loader = self.train_loader_config.get_dataloader(
+            dataset=self.train_dataset,
+            dp_degree=dp_degree,
+            rank=rank,
+        )
 
     def build_val_loader(
         self,
         dataloader_config: Optional[DataLoaderConfig] = None,
         *,
-        batch_size: int = 1,
-        sampling_weights: Optional[str] = None,
-        shuffle: bool = False,
-        num_workers: int = 0,
-        drop_last: bool = False,
-        prefetch_factor: Optional[NonNegativeInt] = None,
-        dp_degree: int = 1,
-        rank: int = 1,
+        batch_size: int = BATCH_SIZE,
+        sampling_weights: Optional[str] = SAMPLING_WEIGHTS,
+        shuffle: bool = SHUFFLE,
+        drop_last: bool = DROP_LAST,
+        num_workers: int = NUM_WORKERS,
+        prefetch_factor: Optional[int] = PREFETCH_FACTOR,
+        pin_memory: bool = PIN_MEMORY,
+        dp_degree: Optional[int] = DP_DEGREE,
+        rank: Optional[int] = RANK,
     ) -> None:
         """
-        Build a train loader for the split.
+        Build a validation loader for the split.
 
         Parameters
         ----------
-        dataloader_config : Optional[DataLoaderConfig], default=None
+        dataloader_config : Optional[DataLoaderConfig] (optional, default=None)
             Pre-configured DataLoader configuration.
-        batch_size : Optional[int], default=None
+        batch_size : int (optional, default=1)
             Batch size for the DataLoader (used if `dataloader_config` is not provided).
-        sampling_weights : Optional[str], default=None
-            Sampling weights for the DataLoader.
-        shuffle : bool, default=False
-            Whether to shuffle the data.
-        num_workers : int, default=0
-            Number of workers for data loading.
-        drop_last : bool, default=False
-            Whether to drop the last incomplete batch.
-        prefetch_factor : Optional[NonNegativeInt], default=None
-            Prefetch factor for the DataLoader.
-        dp_degree : int, default=1
-            Data parallelism degree.
-        rank : int, default=1
-            Rank for distributed data loading.
+        sampling_weights : Optional[str] (optional, default=None)
+            Name of the column in the dataframe of the CapsDatasets where to find the sampling
+            weights (used if `dataloader_config` is not provided).
+        shuffle : bool (optional, default=False)
+            Whether to shuffle the data (used if `dataloader_config` is not provided).
+        drop_last : bool (optional, default=False)
+            Whether to drop the last incomplete batch (used if `dataloader_config` is not provided).
+        num_workers : int (optional, default=0)
+            Number of workers for data loading (used if `dataloader_config` is not provided).
+        prefetch_factor : Optional[int] (optional, default=None)
+            Prefetch factor if num_workers is not 0 (used if `dataloader_config` is not provided).
+        dp_degree : Optional[int] (optional, default=None)
+           The degree of data parallelism. None if no data parallelism.
+        rank : Optional[int] (optional, default=None)
+            Process id within the data parallelism communicator.
+            None if no data parallelism.
 
         Raises
         ------
         ValueError
-            If neither a configuration object nor batch_size is provided.
+            If one of 'dp_degree' and 'rank' is None but the other is not None.
+        KeyError
+            If 'sampling_weights' is passed but there is no such column in the dataframe
+            of the validation dataset.
         """
         if dataloader_config:
-            self.val_loader = dataloader_config.get_dataloader(dataset=self.val_dataset)
-        elif batch_size is not None:
-            dataloader = DataLoaderConfig(
+            self.val_loader_config = dataloader_config
+        else:
+            self.val_loader_config = DataLoaderConfig(
                 batch_size=batch_size,
+                sampling_weights=sampling_weights,
                 shuffle=shuffle,
                 num_workers=num_workers,
                 drop_last=drop_last,
                 prefetch_factor=prefetch_factor,
+                pin_memory=pin_memory,
             )
-            self.val_loader = dataloader.get_dataloader(
-                dataset=self.val_dataset,
-                sampling_weights=sampling_weights,
-                dp_degree=dp_degree,
-                rank=rank,
-            )
-        else:
-            raise ValueError(
-                "Either a DataLoaderConfig or batch_size must be provided."
-            )
+        self.val_loader = self.val_loader_config.get_dataloader(
+            dataset=self.val_dataset,
+            dp_degree=dp_degree,
+            rank=rank,
+        )
