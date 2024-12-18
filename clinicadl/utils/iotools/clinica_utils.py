3a3e0dd9f855314e734dc628f03d14abc48bcd99
thibaultdvx <thibault.devarax@icm-institute.org>
2024-12-18 18:18:23 +0100

    path issue

--- clinicadl/utils/iotools/clinica_utils.py
+++ clinicadl/utils/iotools/clinica_utils.py
@@ -227,7 +227,7 @@ def create_subs_sess_list(
 
     if not file_name:
         file_name = "subjects_sessions_list.tsv"
-    subjs_sess_tsv = open(subjs_sess_tsv_path, "w")
+    subjs_sess_tsv = open(output_dir / file_name, "w")
     subjs_sess_tsv.write("participant_id" + "\t" + "session_id" + "\n")
 
     if is_bids_dir:
