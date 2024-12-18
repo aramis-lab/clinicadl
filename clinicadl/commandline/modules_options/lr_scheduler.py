d9b7e8a9a812b3f5a738572c8ae8bf8e9b9c1d36
camillebrianceau <camille.brianceau@gmail.com>
2024-12-18 15:41:06 +0100

    remove unused module

--- clinicadl/commandline/modules_options/lr_scheduler.py
+++
@@ -1,9 +0,0 @@
-import click
-
-# LR scheduler
-adaptive_learning_rate = click.option(
-    "--adaptive_learning_rate",
-    "-alr",
-    is_flag=True,
-    help="Whether to diminish the learning rate",
-)
