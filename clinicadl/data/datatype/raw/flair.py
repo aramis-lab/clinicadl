385c32d3fe1cd62b258fc3f7f5394f9568aec348
camillebrianceau <57992134+camillebrianceau@users.noreply.github.com>
2025-01-07 09:43:13 +0100

    Update clinicadl/data/datatype/raw/flair.py
    Co-authored-by: Thibault de Varax <154365476+thibaultdvx@users.noreply.github.com>

--- clinicadl/data/datatype/raw/flair.py
+++ clinicadl/data/datatype/raw/flair.py
@@ -14,7 +14,7 @@ class RawFlair(RawData, Flair):
     methods to define file patterns and descriptions.
     """
 
-    def _get_bids_filetype(self, reconstruction: Optional[str] = None) -> FileType:
+    def _get_bids_filetype(self) -> FileType:
         """
         Generate the BIDS-compatible file type pattern and description.
 
