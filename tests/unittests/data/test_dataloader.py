7d85e9e55c83d073332f6d03c61e52c6801ace6b
thibaultdvx <thibault.devarax@icm-institute.org>
2025-01-09 09:40:54 +0100

    test dataloader

--- tests/unittests/data/test_dataloader.py
+++ tests/unittests/data/test_dataloader.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 import pytest
 from pydantic import ValidationError
 
 from clinicadl.data.dataloader import DataLoaderConfig
+from clinicadl.data.datasets import CapsDataset
 
 BAD_INPUTS = [
     {"batch_size": 0},
@@ -39,3 +42,19 @@ def test_bad_inputs(args: dict):
 def test_get_dataloader(args: dict):
     with pytest.raises(ValidationError):
         DataLoaderConfig(**args)
+
+
+caps_dir = Path(__file__).parents[1] / "ressources" / "caps_example"
+
+# def test_get_dataloader():
+#     caps_dir = Path(__file__).parents[1] / "ressources" / "caps_example"
+#     dataset = CapsDataset(caps_directory=caps_dir, data=)
+#     dataloader_config = DataLoaderConfig(
+#         batch_size=2,
+#         sampling_weights="age",
+#         drop_last=True,
+#         num_workers=1,
+#         prefetch_factor=2,
+#         pin_memory=True,
+#     )
+#     dataloder = dataloader_config.get_dataloader()
