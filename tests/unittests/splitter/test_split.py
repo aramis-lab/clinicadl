36b6153baa9ca279b413b2a4febc75d8d5738bb5
thibaultdvx <thibault.devarax@icm-institute.org>
2025-06-11 13:41:05 +0200

    try ending multiprocessing tests

--- tests/unittests/splitter/test_split.py
+++ tests/unittests/splitter/test_split.py
@@ -40,10 +40,6 @@ def test_build_loaders():
     split.build_train_loader(config, batch_size=1)
     assert split.train_loader.batch_size == 2
     assert split.train_loader.sampler.num_replicas == 1
-
-    config = DataLoaderConfig(num_workers=1)
-    split.build_val_loader(config, num_workers=0)
-    assert split.val_loader.num_workers == 1
     assert split.val_loader.sampler.num_replicas == 1
 
     split.parallelism(dp_degree=2, rank=0)
@@ -94,6 +90,8 @@ def test_build_loaders():
     with pytest.raises(ValueError):
         split.parallelism(dp_degree=2, rank=2)
 
+    del split
+
 
 def test_to_dict():
     split = Split(
@@ -117,4 +115,6 @@ def test_to_dict():
     assert dict_["train_dataset"]["total_samples"] == 6
     assert dict_["val_dataset"]["total_samples"] == 2
     assert dict_["train_loader_config"]["batch_size"] == 2
-    assert dict_["val_loader_config"]["num_workers"] == 1
+    assert dict_["val_loader_config"]["num_workers"] == 0
+
+    del split
