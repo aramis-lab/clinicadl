fc896ae5075e96686a431c60d21efbd089f9e0bb
camillebrianceau <camille.brianceau@gmail.com>
2025-06-17 10:05:30 +0200

    remove union str

--- clinicadl/data/datatypes/modalities/pet.py
+++ clinicadl/data/datatypes/modalities/pet.py
@@ -1,7 +1,7 @@
 from enum import Enum
-from typing import Optional, Self, Union
+from typing import Optional
 
-from pydantic import computed_field, model_validator
+from pydantic import computed_field
 
 from ..enum import ImageModality
 from .base import Modality
