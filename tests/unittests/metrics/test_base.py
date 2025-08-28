bfae5748b484abf49ed4cb92436cd9bf8c050e53
thibaultdvx <thibault.devarax@icm-institute.org>
2025-08-28 10:41:49 +0200

    test metrics with ddp

--- tests/unittests/metrics/test_base.py
+++ tests/unittests/metrics/test_base.py
@@ -1,7 +1,14 @@
+import os
+import random
 from copy import deepcopy
+from functools import wraps
 
+import pytest
 import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
 import torchio as tio
+from monai.metrics import ConfusionMatrixMetric
 
 from clinicadl.data.structures import DataPoint
 from clinicadl.metrics import Metric
@@ -58,3 +65,59 @@ def test_metric():
     assert metric.aggregate() == 0.25
 
     assert metric.optimum == "max"
+
+
+def setup_ddp(rank, world_size):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = str(random.randint(10000, 20000))
+    backend = "nccl" if torch.cuda.is_available() else "gloo"
+    dist.init_process_group(backend, rank=rank, world_size=world_size)
+    if torch.cuda.is_available():
+        torch.cuda.set_device(rank)
+
+
+def cleanup():
+    dist.destroy_process_group()
+
+
+def ddp_test(world_size: int):
+    def ddp_test_builder(func):
+        @wraps(func)
+        def wrapped(rank, *args, **kwargs):
+            setup_ddp(rank, world_size)
+
+            try:
+                func(rank, *args, **kwargs)
+            finally:
+                cleanup()
+
+        return wrapped
+
+    return ddp_test_builder
+
+
+WORLD_SIZE = 2
+
+
+@ddp_test(world_size=WORLD_SIZE)
+def ddp_worker(rank):
+    metric = ConfusionMatrixMetric(metric_name="accuracy")
+    if rank == 0:
+        pred, target = Y_1_PRED.to(rank), Y_1.to(rank)
+        metric(pred, target)
+        pred, target = Y_3_PRED.to(rank), Y_3.to(rank)
+        metric(pred, target)
+    elif rank == 1:
+        pred, target = Y_2_PRED.to(rank), Y_2.to(rank)
+        metric(pred, target)
+
+    dist.barrier()
+
+    if rank == 0:
+        out = metric.aggregate()
+        assert out[0].item() == 0.25
+
+
+@pytest.mark.multi_gpu
+def test_metric_dpp():
+    mp.spawn(ddp_worker, nprocs=WORLD_SIZE, join=True)
