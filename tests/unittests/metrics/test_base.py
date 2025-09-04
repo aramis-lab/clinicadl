466b5d0fd2206379a99e16765054c430776b165d
thibaultdvx <thibault.devarax@icm-institute.org>
2025-09-04 09:49:27 +0200

    fix test issue

--- tests/unittests/metrics/test_base.py
+++ tests/unittests/metrics/test_base.py
@@ -1,9 +1,5 @@
-import os
-from functools import wraps
-
 import pytest
 import torch
-import torch.distributed as dist
 import torchio as tio
 
 from clinicadl.data.dataloader import Batch
@@ -48,7 +44,7 @@ class CustomTestMetric(Metric):
 def test_metric():
     metric = CustomTestMetric()
 
-    batches = [BATCH[:2], BATCH[2:4], BATCH[4:]]
+    batches = [BATCH[:2], BATCH[2:4], Batch(BATCH[4:])]
     results = (
         torch.tensor([1.0, 1.0]),
         torch.tensor([0.0, 1.0]),
@@ -64,7 +60,7 @@ def test_metric():
 
 @ddp_wrapper
 def metric_parallelism(rank):
-    batches = [BATCH[:2], BATCH[2:4], Batch(BATCH[4:])]
+    batches = [Batch(BATCH[:2]), Batch(BATCH[2:4]), Batch(BATCH[4:])]
     metric = CustomTestMetric()
     if rank == 0:
         assert rank == 0
