ea66cea9a9b916b0cec055a174b5da356026e53d
thibaultdvx <thibault.devarax@icm-institute.org>
2025-11-06 19:30:53 +0100

    utils

---
+++ clinicadl/utils/config.py
@@ -0,0 +1,573 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+from collections.abc import Sequence
+from copy import deepcopy
+from pathlib import Path
+from typing import Any, Callable, Generic, Optional, TypeVar, Union
+
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    ValidationError,
+    computed_field,
+    field_serializer,
+)
+from pydantic.fields import ModelPrivateAttr
+from typing_extensions import Self
+
+from clinicadl.dictionary.words import NAME, READER
+from clinicadl.utils.exceptions import (
+    CannotReadFieldError,
+    CannotReadJsonFieldError,
+    MissingFieldsError,
+    MissingFieldsJsonError,
+    NotInterpretableJsonError,
+    WrongFieldsError,
+    WrongFieldsJsonError,
+)
+from clinicadl.utils.json import read_json, write_json
+from clinicadl.utils.typing import PathType
+
+CONFIG = "Config"
+
+FieldReaderType = Callable[[dict[str, Any]], Any]
+FieldReadersType = dict[str, FieldReaderType]
+
+
+class ClinicaDLConfig(BaseModel):
+    """Base pydantic dataclass."""
+
+    model_config = ConfigDict(
+        validate_assignment=True,
+        use_enum_values=True,
+        validate_default=True,
+        arbitrary_types_allowed=True,
+    )
+
+    def __init__(self, **kwargs):
+        """Useless method but needed for the doc (typing)."""
+        super().__init__(**kwargs)
+
+    @classmethod
+    def get_fields(cls, computed: bool = False) -> list[str]:
+        """
+        Gets the list of the fields in the config class.
+
+        Parameters
+        ----------
+        computed : bool, default=False
+            Whether to also return computed fields.
+
+        Returns
+        -------
+        list[str]
+            The list of the field names.
+        """
+        fields = list(cls.model_fields.keys())
+        if computed:
+            fields += list(cls.model_computed_fields.keys())
+
+        return fields
+
+    def to_raw_dict(self, exclude: Optional[Sequence[str]] = None) -> dict[str, Any]:
+        """
+        Returns the config class as a dictionary, but **not** serialized.
+
+        Parameters
+        ----------
+        exclude : Optional[Sequence[str]], default=None
+            Potential fields to exclude.
+
+        Returns
+        -------
+        dict[str, Any]
+            The raw config class as a dict.
+        """
+        d = self.__dict__.copy()
+
+        if exclude:
+            for name in exclude:
+                del d[name]
+
+        for name, value in d.items():
+            if hasattr(value, "to_raw"):
+                to_raw_dict = getattr(value, "to_raw")
+                if callable(to_raw_dict):
+                    d[name] = to_raw_dict()
+
+        return d
+
+    def to_dict(self, **kwargs) -> dict[str, Any]:
+        """
+        Customized version of :py:method:`pydantic.BaseModel.model_dump`.
+
+        Returns
+        -------
+        dict[str, Any]
+            The serialized config class.
+        kwargs
+            Any argument accepted by :py:method:`pydantic.BaseModel.model_dump`.
+        """
+        return _order_dict(self.model_dump(**kwargs, serialize_as_any=True))
+
+    def to_json(self, json_path: PathType, overwrite: bool = False, **kwargs) -> None:
+        """
+        Writes the serialized config class to a ``JSON`` file.
+
+        Parameters
+        ----------
+        json_path : PathType
+            Path to the ``JSON`` file.
+        overwrite : bool
+            Whether to overwrite the file if it exists.
+        """
+        write_json(
+            json_path=json_path, data=self.to_dict(**kwargs), overwrite=overwrite
+        )
+
+    @classmethod
+    def from_dict(cls, dict_: dict[str, Any], **kwargs) -> Self:
+        """
+        To create a ``ClinicaDLConfig`` from a dictionary.
+
+        Parameters
+        ----------
+        dict_ : dict[str, Any]
+            The dictionary.
+        kwargs
+            Any field to overwrite.
+
+        Returns
+        -------
+        ClinicaDLConfig
+            The config class.
+        """
+        fields_in_dict = set(dict_)
+        expected_fields = set(cls.get_fields())
+
+        diff = expected_fields.difference(fields_in_dict)
+        if len(diff) > 0:
+            raise MissingFieldsError(fields=list(diff))
+
+        diff = fields_in_dict.difference(expected_fields)
+        if len(diff) > 0:
+            raise WrongFieldsError(fields=list(diff), object_name=cls._get_name())
+
+        for field, value in dict_.items():
+            if field not in kwargs:
+                reader = cls._get_reader(field)
+                kwargs[field] = cls._read_anything(value, field, reader=reader)
+
+        try:
+            return cls(**kwargs)
+        except ValidationError as e:
+            raise CannotReadFieldError(error=e, object_name=cls._get_name()) from e
+
+    @classmethod
+    def from_json(cls, json_path: PathType, **kwargs) -> Self:
+        """
+        To create a ``ClinicaDLConfig`` from a ``JSON`` file.
+
+        Parameters
+        ----------
+        json_path : PathType
+            Path to the ``JSON`` file.
+        kwargs
+            Any field to overwrite.
+
+        Returns
+        -------
+        ClinicaDLConfig
+            The config class.
+        """
+        json_path = Path(json_path)
+        dict_ = cls._read_json(json_path=json_path)
+
+        try:
+            return cls.from_dict(dict_, **kwargs)
+        except MissingFieldsError as exc:
+            raise MissingFieldsJsonError(exc, json_path) from exc
+        except WrongFieldsError as exc:
+            raise WrongFieldsJsonError(exc, json_path) from exc
+        except CannotReadFieldError as exc:
+            raise CannotReadJsonFieldError(exc, json_path) from exc
+
+    @staticmethod
+    def serialize_anything(value: Any) -> Any:
+        """
+        To serialize any object in ``ClinicaDL``.
+        Serialization of an object can be customized with a method
+        ``to_dict``. Otherwise, the raw object
+        will be returned and serialization will be done by pydantic.
+
+        Parameters
+        ----------
+        value : Any
+            The value of the field.
+
+        Returns
+        -------
+            The serialized field.
+        """
+        if hasattr(value, "to_dict"):
+            to_dict = getattr(value, "to_dict")
+            if callable(to_dict):
+                return _order_dict(to_dict())
+
+        return value
+
+    @classmethod
+    def _read_anything(
+        cls, value: Any, field: str, reader: Optional[FieldReaderType] = None
+    ) -> Any:
+        """
+        To read any serialized object.
+        It will try to use the reader.
+        """
+        if reader:
+            try:
+                return reader(value)
+            except Exception as e:
+                raise CannotReadFieldError(
+                    field_name=field, object_name=cls._get_name()
+                ) from e
+        else:
+            return value
+
+    @classmethod
+    def _read_json(cls, json_path: PathType) -> dict[str, Any]:
+        """
+        Reads the serialized config class from a JSON file.
+        """
+        config_dict = read_json(json_path=json_path)
+
+        if not isinstance(config_dict, dict):
+            raise NotInterpretableJsonError(json_path, cls._get_name())
+
+        return config_dict
+
+    @field_serializer("*", when_used="always")
+    def _field_serializer(self, value: Any) -> Any:
+        """
+        To serialize a fields.
+        """
+        return self.serialize_anything(value)
+
+    @classmethod
+    def _get_name(cls) -> str:
+        """Returns the name of the class."""
+        return cls.__name__
+
+    @classmethod
+    def _get_reader(cls, field: str) -> Optional[FieldReaderType]:
+        """Gets the reader for a field."""
+        cls._FIELD_READERS: ModelPrivateAttr
+        if cls.model_fields[field].json_schema_extra:  # pylint: disable=unsubscriptable-object
+            return cls.model_fields[field].json_schema_extra.get(READER, None)  # pylint: disable=no-member, disable=unsubscriptable-object
+
+        return None
+
+
+T = TypeVar("T")
+
+
+class ObjectConfig(ClinicaDLConfig, ABC, Generic[T]):
+    """
+    Base config class associated to a Python object.
+
+    The user can then get the parametrized object with
+    the method :py:meth:`get_object`.
+    """
+
+    @computed_field
+    @property
+    def name(self) -> str:
+        """The name of the class associated to this config class."""
+        return self._get_name()
+
+    def get_object(self) -> T:
+        """
+        Returns the object associated to this configuration,
+        parametrized with the parameters passed by the user.
+
+        Returns
+        -------
+        T
+            The parametrized object.
+        """
+        associated_class = self._get_class()
+        parameters = self._get_parameters()
+        return associated_class(**parameters)
+
+    @classmethod
+    def from_dict(cls, dict_: dict[str, Any], **kwargs) -> Self:
+        dict_ = deepcopy(dict_)
+        if NAME in dict_:
+            del dict_[NAME]
+        return super().from_dict(dict_, **kwargs)
+
+    @classmethod
+    @abstractmethod
+    def _get_class(cls) -> type[T]:
+        """Returns the class associated to this config class."""
+
+    def _get_parameters(self) -> dict[str, Any]:
+        """
+        Gets the parameters of the class associated class.
+        If some parameters has been passed via ``ObjectConfig``
+        (or any class with a ``get_object`` method) they are converted
+        to the underlying objects.
+        """
+        params = {}
+        for field, value in self:
+            if hasattr(value, "get_object") and callable(
+                get_object := getattr(value, "get_object")
+            ):
+                params[field] = get_object()
+            else:
+                params[field] = value
+
+        return params
+
+    @classmethod
+    def _get_name(cls) -> str:
+        """Returns the name of the class associated to this config class."""
+        return cls.__name__.replace(CONFIG, "")
+
+
+TConfig = TypeVar("TConfig", bound=ObjectConfig)
+
+
+class ObjectOrConfig(BaseModel, Generic[T, TConfig]):
+    """
+    To handle fields that accept an object or the associated config.
+    """
+
+    value: Union[T, TConfig]
+
+    model_config = ConfigDict(
+        validate_assignment=True,
+        arbitrary_types_allowed=True,
+    )
+
+    def __init__(self, value: Union[T, TConfig]):
+        super().__init__(value=value)
+
+    def get_object(self) -> T:
+        """
+        Returns the object. If a config class was passed,
+        it is converted to the underlying object.
+
+        Returns
+        -------
+        T
+            The parametrized object.
+        """
+        if isinstance(self.value, ObjectConfig):
+            return self.value.get_object()
+        else:
+            return self.value
+
+    def to_raw(self) -> Union[T, TConfig]:
+        """
+        Unwrap the value of the field.
+
+        Returns
+        -------
+        Union[T, TConfig]
+            The raw value of the field.
+        """
+        return self.value
+
+    def to_dict(self) -> Any:
+        """
+        To serialize the current field.
+
+        Returns
+        -------
+        Any
+            The serialized field.
+        """
+        return ClinicaDLConfig.serialize_anything(self.value)
+
+    @classmethod
+    def from_value(cls, value: Union[Self, Union[T, TConfig]]) -> Self:
+        """
+        To safely create an ``ObjectOrConfig`` from the value of the underlying
+        field.
+
+        Useful for pydantic field validators.
+
+        Parameters
+        ----------
+        value : Union[Self, Union[T, TConfig]]
+            The field value or an ``ObjectOrConfig``.
+
+        Returns
+        -------
+        Self
+            The ``ObjectOrConfig``.
+        """
+        if isinstance(value, cls):
+            return value
+        return cls(value)
+
+    @classmethod
+    def build_reader(
+        cls,
+        config_reader: Callable[[dict[str, Any]], TConfig],
+    ) -> Callable[[Any], Self]:
+        """
+        To build a reader to deserialize the serialized object/config.
+
+        If a dict is passed, the reader will use ``config_reader``,
+        otherwise it will not modify the input.
+
+        Parameters
+        ----------
+        config_reader : Callable[[dict[str, Any]], TConfig]
+            The function to read the serialized config class.
+
+        Returns
+        -------
+        Callable[[Any], Self]
+            The reader to read the serialized object/config.
+        """
+
+        def reader(serialized_obj: Any) -> Self:
+            if isinstance(serialized_obj, dict):
+                obj = config_reader(serialized_obj)
+            else:
+                obj = serialized_obj
+            return cls(obj)
+
+        return reader
+
+
+class SequenceOfObjects(BaseModel, Generic[T, TConfig]):
+    """
+    To handle fields that are sequences of objects or the associated configs.
+    """
+
+    values: list[ObjectOrConfig[T, TConfig]]
+
+    model_config = ConfigDict(
+        validate_assignment=True,
+        arbitrary_types_allowed=True,
+    )
+
+    def __init__(
+        self, values: Sequence[Union[Union[T, TConfig], ObjectOrConfig[T, TConfig]]]
+    ):
+        super().__init__(
+            values=[
+                ObjectOrConfig(obj) if not isinstance(obj, ObjectOrConfig) else obj
+                for obj in values
+            ]
+        )
+
+    def get_object(self) -> list[T]:
+        """
+        Returns the list of objects. If config classes were passed,
+        they are converted to the underlying object.
+
+        Returns
+        -------
+        list[T]
+            The parametrized objects.
+        """
+        return [obj.get_object() for obj in self.values]
+
+    def to_raw(self) -> list[Union[T, TConfig]]:
+        """
+        Unwrap the value of the field.
+
+        Returns
+        -------
+        list[Union[T, TConfig]]
+            The raw value of the field.
+        """
+        return [value.to_raw() for value in self.values]
+
+    def to_dict(self) -> list[dict[str, Any]]:
+        """
+        To serialize the current field.
+
+        Returns
+        -------
+        list[dict[str, Any]]
+            The serialized sequence of objects/configs.
+        """
+        return [obj.to_dict() for obj in self.values]
+
+    @classmethod
+    def from_sequence(cls, value: Any, field_name: str) -> Self:
+        """
+        Checks if the input is a sequence and build a ``SequenceOfConfigs``.
+
+        Useful for pydantic field validators.
+
+        Parameters
+        ----------
+        value : Any
+            The field value.
+        field_name : str
+            The field name
+
+        Returns
+        -------
+            The instantiated object.
+        """
+        if isinstance(value, cls):
+            return value
+        if not isinstance(value, Sequence):
+            raise ValueError(f"'{field_name}' must be a sequence. Got: {value}")
+        return cls(value)
+
+    @classmethod
+    def build_reader(
+        cls,
+        config_reader: Callable[[dict[str, Any]], TConfig],
+    ) -> Callable[[list[Any]], Self]:
+        """
+        To build a reader to deserialize the sequence of
+        serialized objects/configs.
+
+        Parameters
+        ----------
+        reader : Callable[[dict[str, Any]], TConfig]
+            The function to read the serialized config class.
+
+        Returns
+        -------
+        Callable[[list[Any]], Self]
+            The reader to read the serialized list of objects/configs.
+        """
+
+        def list_reader(serialized_objects: list[Any]) -> Self:
+            obj_reader = ObjectOrConfig.build_reader(config_reader)
+            return cls([obj_reader(obj) for obj in serialized_objects])
+
+        return list_reader
+
+
+def _order_dict(model_or_field: Any) -> Any:
+    """
+    To always have the field 'name' at the beginning.
+
+    Recursive function to handle fields that
+    contain themselves 'ClinicaDLConfig' instances.
+    """
+    if isinstance(model_or_field, dict):
+        ordered_dict = OrderedDict(**model_or_field)
+        if NAME in ordered_dict:  # always 'name' at the beginning
+            ordered_dict.move_to_end(NAME, last=False)
+
+        for key, value in ordered_dict.items():
+            ordered_dict[key] = _order_dict(value)
+
+        return ordered_dict
+
+    return model_or_field
--- tests/unittests/utils/test_config.py
+++ tests/unittests/utils/test_config.py
@@ -1,28 +1,269 @@
+import json
 from collections import OrderedDict
-from typing import Union
+from copy import deepcopy
+from typing import Any, Sequence, Union
 
-from clinicadl.utils.config import ClinicaDLConfig
+import pytest
+from pydantic import Field, field_validator
+from typing_extensions import Self
+
+from clinicadl.utils.config import (
+    ClinicaDLConfig,
+    ObjectConfig,
+    ObjectOrConfig,
+    SequenceOfObjects,
+)
+from clinicadl.utils.exceptions import (
+    CannotReadJsonFieldError,
+    MissingFieldsJsonError,
+    NotInterpretableJsonError,
+    WrongFieldsJsonError,
+)
+from clinicadl.utils.json import read_json
 
 
-def test_to_dict():
-    class SubConfigTest(ClinicaDLConfig):
-        a: str = "a"
-        name: str = "SubTest"
+class ConfigTest(ClinicaDLConfig):
+    a: int = 0
 
-    class ConfigTest(ClinicaDLConfig):
-        list_test: list[Union[str, SubConfigTest]] = ["list", SubConfigTest()]
-        tuple_test: tuple[SubConfigTest, SubConfigTest] = (
-            SubConfigTest(),
-            SubConfigTest(),
-        )
-        name: str = "Config"
 
-    config = ConfigTest()
-    ordered_subtest = OrderedDict(name="SubTest", a="a")
-    assert config.to_dict() == OrderedDict(
-        **{
-            "name": "Config",
-            "list_test": ["list", ordered_subtest],
-            "tuple_test": (ordered_subtest, ordered_subtest),
-        }
+class ConfigTestBis(ConfigTest):
+    pass
+
+
+class ObjectTest:
+    def __init__(self, a: int):
+        self.a: int = a
+
+
+class ObjectTestConfig(ObjectConfig[ObjectTest], ConfigTest):
+    @classmethod
+    def _get_class(cls) -> Any:
+        return ObjectTest
+
+
+class ClinicaDLObjectTest(ObjectTest):
+    def to_dict(self):
+        return self.__dict__ | {"bonus": "abc"}
+
+    @classmethod
+    def from_dict(cls, dict_) -> Self:
+        d = deepcopy(dict_)
+        del d["bonus"]
+        return cls(**d)
+
+
+class CollectionTest:
+    def __init__(self, object: ObjectTest, list_objects: Sequence[ObjectTest]):
+        self.object = object
+        self.list_objects = list_objects
+
+
+class MainConfigTest(ClinicaDLConfig):
+    simple: str
+    sub_test: ConfigTest
+    object_or_config: Union[ObjectTest, ObjectTestConfig]
+    object_or_config_bis: ObjectOrConfig[ObjectTest, ObjectTestConfig] = Field(
+        reader=ObjectOrConfig.build_reader(ObjectTestConfig.from_dict)
     )
+    object: ClinicaDLObjectTest = Field(reader=ClinicaDLObjectTest.from_dict)
+    list_configs: SequenceOfObjects[ObjectTest, ObjectTestConfig] = Field(
+        reader=SequenceOfObjects.build_reader(ObjectTestConfig.from_dict)
+    )
+
+    @field_validator("object_or_config_bis", mode="before")
+    @classmethod
+    def _handle_any_value(cls, v: Any) -> ObjectOrConfig:
+        return ObjectOrConfig.from_value(v)
+
+    @field_validator("list_configs", mode="before")
+    @classmethod
+    def _handle_sequence(cls, v: Any) -> SequenceOfObjects:
+        return SequenceOfObjects.from_sequence(v, field_name="list_configs")
+
+
+class CollectionTestConfig(ObjectConfig[CollectionTest]):
+    object: ObjectOrConfig[ObjectTest, ObjectTestConfig] = Field(
+        reader=ObjectOrConfig.build_reader(ObjectTestConfig.from_dict)
+    )
+    list_objects: SequenceOfObjects[ObjectTest, ObjectTestConfig] = Field(
+        reader=SequenceOfObjects.build_reader(ObjectTestConfig.from_dict)
+    )
+
+    @field_validator("object", mode="before")
+    @classmethod
+    def _handle_any_value(cls, v: Any) -> ObjectOrConfig:
+        return ObjectOrConfig.from_value(v)
+
+    @field_validator("list_objects", mode="before")
+    @classmethod
+    def _handle_sequence(cls, v: Any) -> SequenceOfObjects:
+        return SequenceOfObjects.from_sequence(v, field_name="list_configs")
+
+    @classmethod
+    def _get_class(cls) -> type[CollectionTest]:
+        return CollectionTest
+
+
+def test_clinicadl_config(tmp_path):
+    params = {
+        "simple": "str",
+        "sub_test": ConfigTest(a=1),
+        "object_or_config": ObjectTestConfig(a=-1),
+        "object_or_config_bis": ObjectTestConfig(a=-1),
+        "object": ClinicaDLObjectTest(a=-1),
+        "list_configs": [ObjectTestConfig(a=0), ObjectTestConfig(a=1)],
+    }
+    t = MainConfigTest(**params)
+
+    # to json
+    t.to_json(tmp_path / "config.json", overwrite=True)
+    dict_ = read_json(tmp_path / "config.json")
+    assert dict_["simple"] == "str"
+    assert dict_["sub_test"] == {"a": 1}
+    assert OrderedDict(dict_["object_or_config"]) == OrderedDict(
+        {"name": "ObjectTest", "a": -1}
+    )
+    assert OrderedDict(dict_["object_or_config_bis"]) == OrderedDict(
+        {"name": "ObjectTest", "a": -1}
+    )
+    assert dict_["object"] == {"a": -1, "bonus": "abc"}
+    assert [OrderedDict(d) for d in dict_["list_configs"]] == [
+        OrderedDict({"name": "ObjectTest", "a": 0}),
+        OrderedDict({"name": "ObjectTest", "a": 1}),
+    ]
+
+    # from json
+    new_t = MainConfigTest.from_json(
+        tmp_path / "config.json",
+    )
+    assert new_t.simple == "str"
+    assert new_t.sub_test.a == 1
+    assert new_t.object_or_config.a == -1
+    assert new_t.object_or_config_bis.value.a == -1
+    assert new_t.object.a == -1
+    assert new_t.list_configs.values[0].value.a == 0
+    assert new_t.list_configs.values[1].value.a == 1
+
+    # cannot read errors
+    t = MainConfigTest(
+        simple="str",
+        sub_test=ConfigTest(a=1),
+        object_or_config=ObjectTest(a=-1),
+        object_or_config_bis=ObjectTest(a=-1),
+        object=ClinicaDLObjectTest(a=-1),
+        list_configs=[ObjectTestConfig(a=0), ObjectTest(a=1)],
+    )
+    t.to_json(tmp_path / "config.json", overwrite=True)
+
+    new_t = MainConfigTest.from_json(
+        tmp_path / "config.json",
+        sub_test=ConfigTest(a=2),
+        object_or_config=ObjectTest(a=-1),
+        object_or_config_bis=ObjectTest(a=-1),
+        list_configs=[ObjectTestConfig(a=0), ObjectTest(a=1)],
+    )
+    assert new_t.simple == "str"
+    assert new_t.sub_test.a == 2
+    assert new_t.object_or_config.a == -1
+    assert new_t.object_or_config_bis.value.a == -1
+    assert new_t.object.a == -1
+    assert new_t.list_configs.values[0].value.a == 0
+    assert new_t.list_configs.values[1].value.a == 1
+
+    with pytest.raises(
+        CannotReadJsonFieldError,
+        match="MainConfigTest cannot read 'object_or_config' in .*\nPlease pass this field via kwargs.",
+    ):
+        t = MainConfigTest.from_json(
+            tmp_path / "config.json",
+            object_or_config_bis=ObjectTest(a=-1),
+            list_configs=[ObjectTestConfig(a=0), ObjectTest(a=1)],
+        )
+
+    with pytest.raises(
+        CannotReadJsonFieldError,
+        match="MainConfigTest cannot read 'object_or_config_bis' in .*\nPlease pass this field via kwargs.",
+    ):
+        t = MainConfigTest.from_json(
+            tmp_path / "config.json",
+            object_or_config=ObjectTest(a=-1),
+            list_configs=[ObjectTestConfig(a=0), ObjectTest(a=1)],
+        )
+
+    with pytest.raises(
+        CannotReadJsonFieldError,
+        match="MainConfigTest cannot read 'list_configs' in .*\nPlease pass this field via kwargs.",
+    ):
+        MainConfigTest.from_json(
+            tmp_path / "config.json",
+            object_or_config=ObjectTest(a=-1),
+            object_or_config_bis=ObjectTest(a=-1),
+        )
+
+    with open(tmp_path / "config.json", "r") as f:
+        dict_ = json.load(f)
+    del dict_["object"]["a"]
+    with open(tmp_path / "config.json", "w") as f:
+        json.dump(dict_, f)
+
+    with pytest.raises(
+        CannotReadJsonFieldError,
+        match="MainConfigTest cannot read 'object' in .*\nPlease pass this field via kwargs.",
+    ):
+        MainConfigTest.from_json(
+            tmp_path / "config.json",
+            object_or_config=ObjectTest(a=-1),
+            object_or_config_bis=ObjectTest(a=-1),
+            list_configs=[ObjectTestConfig(a=0), ObjectTest(a=1)],
+        )
+
+
+def test_get_object():
+    t = CollectionTestConfig(
+        object=ObjectTestConfig(a=-1),
+        list_objects=[ObjectTestConfig(a=0), ObjectTest(a=1)],
+    ).get_object()
+    assert t.object.a == -1
+    assert isinstance(t.object, ObjectTest)
+    assert t.list_objects[0].a == 0
+    assert t.list_objects[1].a == 1
+    assert isinstance(t.list_objects[0], ObjectTest)
+    assert isinstance(t.list_objects[1], ObjectTest)
+
+
+def test_json_reading_error(tmp_path):
+    with open(tmp_path / "config.json", "w") as f:
+        json.dump(None, f)
+
+    with pytest.raises(NotInterpretableJsonError, match="ConfigTest cannot read .*"):
+        ConfigTest.from_json(tmp_path / "config.json")
+
+    with open(tmp_path / "config.json", "w") as f:
+        json.dump({"b": 0}, f)
+
+    with pytest.raises(
+        MissingFieldsJsonError, match=r"Fields \['a'\] are missing in .*"
+    ):
+        ConfigTest.from_json(tmp_path / "config.json")
+
+    with open(tmp_path / "config.json", "w") as f:
+        json.dump({"a": 10, "b": 0}, f)
+
+    with pytest.raises(
+        WrongFieldsJsonError,
+        match=r"Fields \['b'\] in .* are not expected by ConfigTest.",
+    ):
+        ConfigTest.from_json(tmp_path / "config.json")
+
+
+def test_to_raw_dict():
+    t = CollectionTestConfig(
+        object=ObjectTestConfig(a=-1),
+        list_objects=[ObjectTestConfig(a=0), ObjectTest(a=1)],
+    )
+    d = t.to_raw_dict()
+    assert isinstance(d["object"], ObjectTestConfig)
+    assert isinstance(d["list_objects"][0], ObjectTestConfig)
+    assert isinstance(d["list_objects"][1], ObjectTest)
+    d = t.to_raw_dict(exclude=["object"])
+    assert "object" not in d
