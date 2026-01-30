192ed3222e3dc4d0864b3f1b6ed30650bfe9d37b
thibaultdvx <thibault.devarax@icm-institute.org>
2026-01-30 10:58:43 +0100

    seed context manager

--- clinicadl/utils/seed.py
+++ clinicadl/utils/seed.py
@@ -1,7 +1,8 @@
 import logging
 import os
 import random
-from typing import Optional
+from contextlib import contextmanager
+from typing import ContextManager, Optional
 
 import numpy as np
 import torch
@@ -14,6 +15,8 @@ MAX_SEED_VALUE = np.iinfo(np.uint32).max
 MIN_SEED_VALUE = np.iinfo(np.uint32).min
 GLOBAL_SEED = "CLINICADL_GLOBAL_SEED"
 DETERMINISTIC = "CLINICADL_DETERMINISTIC"
+PYTHON_HASH_SEED = "PYTHONHASHSEED"
+CUBLAS_CONFIG = "CUBLAS_WORKSPACE_CONFIG"
 
 
 def pl_worker_init_function(worker_id: int) -> None:
@@ -72,20 +75,73 @@ def seed_everything(seed: Optional[int] = None, deterministic: bool = False) -> 
         )
 
     random.seed(seed)
-    os.environ["PYTHONHASHSEED"] = str(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(
         seed
     )  # manual_seed should be ok because one process per GPU
+    os.environ[PYTHON_HASH_SEED] = str(seed)
 
     os.environ[GLOBAL_SEED] = str(seed)
     logger.info("Global seed set to %d", seed)
 
     if deterministic:
-        os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+        os.environ[CUBLAS_CONFIG] = ":4096:8"
         torch.use_deterministic_algorithms(True)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
 
         os.environ[DETERMINISTIC] = "true"
+
+
+@contextmanager
+def seed_everything_context(
+    seed: Optional[int] = None, deterministic: bool = False
+) -> ContextManager[None]:
+    """
+    Context manager to control reproducibility.
+
+    Does the same as :py:func:`seed_everything`, but restore all
+    previous random states when exiting the context.
+
+    Parameters
+    ----------
+    seed : Optional[int], default=None
+        The seed to use. If ``None``, a random seed will be generated.
+    deterministic : bool, default=False
+        Whether to configure PyTorch's operations in deterministic mode.
+    """
+    prev_env = {
+        PYTHON_HASH_SEED: os.environ.get(PYTHON_HASH_SEED),
+        GLOBAL_SEED: os.environ.get(GLOBAL_SEED),
+        DETERMINISTIC: os.environ.get(DETERMINISTIC),
+        CUBLAS_CONFIG: os.environ.get(CUBLAS_CONFIG),
+    }
+
+    py_random_state = random.getstate()
+    np_state = np.random.get_state()
+    torch_cpu_state = torch.get_rng_state()
+    torch_cuda_states = torch.cuda.get_rng_state_all()
+
+    prev_det_algos = torch.are_deterministic_algorithms_enabled()
+    prev_cudnn_deterministic = torch.backends.cudnn.deterministic
+    prev_cudnn_benchmark = torch.backends.cudnn.benchmark
+
+    try:
+        seed_everything(seed, deterministic)
+        yield
+    finally:
+        random.setstate(py_random_state)
+        np.random.set_state(np_state)
+        torch.set_rng_state(torch_cpu_state)
+        torch.cuda.set_rng_state_all(torch_cuda_states)
+
+        torch.use_deterministic_algorithms(prev_det_algos)
+        torch.backends.cudnn.deterministic = prev_cudnn_deterministic
+        torch.backends.cudnn.benchmark = prev_cudnn_benchmark
+
+        for k, v in prev_env.items():
+            if v is None:
+                os.environ.pop(k, None)
+            else:
+                os.environ[k] = v
--- tests/unittests/utils/test_seed.py
+++ tests/unittests/utils/test_seed.py
@@ -6,13 +6,14 @@ import numpy as np
 import pytest
 import torch
 
-from clinicadl.utils.seed import pl_worker_init_function, seed_everything
-from tests.utils import ddp_test, ddp_wrapper
+from clinicadl.utils.seed import (
+    pl_worker_init_function,
+    seed_everything,
+    seed_everything_context,
+)
 
 
-
-@patch("torch.use_deterministic_algorithms")
-def test_seed_everything(mock, caplog):
+def test_seed_everything(caplog):
     assert "CLINICADL_GLOBAL_SEED" not in os.environ
     assert "CLINICADL_DETERMINISTIC" not in os.environ
     assert os.environ.get("PYTHONHASHSEED") != "10"
@@ -30,17 +31,72 @@ def test_seed_everything(mock, caplog):
     assert os.environ.get("CUBLAS_WORKSPACE_CONFIG") == ":4096:8"
     assert torch.backends.cudnn.deterministic
     assert not torch.backends.cudnn.benchmark
-    mock.assert_called_once_with(True)
+    assert torch.are_deterministic_algorithms_enabled()
 
     seed_everything(0)
     r1 = random.randint(0, 100)
     n1 = np.random.rand()
     t1 = torch.rand(1)
 
+    seed_everything(0)
+    r1_ = random.randint(0, 100)
+    n1_ = np.random.rand()
+    t1_ = torch.rand(1)
+
     seed_everything(1)
     r2 = random.randint(0, 100)
     n2 = np.random.rand()
     t2 = torch.rand(1)
+
+    assert np.isclose(r1, r1_)
+    assert np.isclose(n1, n1_)
+    assert torch.isclose(t1, t1_)
+
+    assert not np.isclose(r1, r2)
+    assert not np.isclose(n1, n2)
+    assert not torch.isclose(t1, t2)
+
+
+def test_seed_everything_context():
+    os.environ["CLINICADL_GLOBAL_SEED"] = "0"
+    assert "CLINICADL_DETERMINISTIC" not in os.environ
+    os.environ["PYTHONHASHSEED"] = "0"
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = "x"
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = True
+
+    with seed_everything_context(seed=10, deterministic=True):
+        r1 = random.randint(0, 100)
+        n1 = np.random.rand()
+        t1 = torch.rand(1)
+        assert os.environ.get("CLINICADL_GLOBAL_SEED") == "10"
+        assert os.environ.get("CLINICADL_DETERMINISTIC") == "true"
+        assert os.environ.get("PYTHONHASHSEED") == "10"
+        assert os.environ.get("CUBLAS_WORKSPACE_CONFIG") == ":4096:8"
+        assert torch.backends.cudnn.deterministic
+        assert not torch.backends.cudnn.benchmark
+        assert torch.are_deterministic_algorithms_enabled()
+
+    assert os.environ.get("CLINICADL_GLOBAL_SEED") == "0"
+    assert "CLINICADL_DETERMINISTIC" not in os.environ
+    assert os.environ.get("PYTHONHASHSEED") == "0"
+    assert os.environ.get("CUBLAS_WORKSPACE_CONFIG") == "x"
+    assert not torch.backends.cudnn.deterministic
+    assert torch.backends.cudnn.benchmark
+    assert not torch.are_deterministic_algorithms_enabled()
+
+    r2 = random.randint(0, 100)
+    n2 = np.random.rand()
+    t2 = torch.rand(1)
+
+    with seed_everything_context(seed=10, deterministic=True):
+        r1_ = random.randint(0, 100)
+        n1_ = np.random.rand()
+        t1_ = torch.rand(1)
+
+    assert np.isclose(r1, r1_)
+    assert np.isclose(n1, n1_)
+    assert torch.isclose(t1, t1_)
 
     assert not np.isclose(r1, r2)
     assert not np.isclose(n1, n2)
@@ -49,8 +105,12 @@ def test_seed_everything(mock, caplog):
 
 @pytest.mark.gpu
 def test_seed_everything_gpu():
+    with seed_everything_context(seed=10):
+        assert torch.cuda.initial_seed() == 10
+    assert torch.cuda.initial_seed() != 10
     seed_everything(7)
     assert torch.cuda.initial_seed() == 7
+
 
 @patch("numpy.random.SeedSequence", side_effect=np.random.SeedSequence)
 @patch(
