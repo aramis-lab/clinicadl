d8d7ee358c751da194145558adbc861c0a806ff3
thibaultdvx <thibault.devarax@icm-institute.org>
2026-02-25 13:17:38 +0100

    verbose in torchinfo.summary

--- clinicadl/models/supervised.py
+++ clinicadl/models/supervised.py
@@ -249,7 +249,7 @@ class SupervisedModel(HasConfig[SupervisedModelConfig], Model):
         from torchinfo import summary
 
         summary_ = summary(
-            self.network, input_data=input_data.get_field(IMAGE), batch_dim=0
+            self.network, input_data=input_data.get_field(IMAGE), batch_dim=0, verbose=0
         )
 
         return str(summary_)
