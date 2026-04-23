1f71e35069ca83213c76c1511ee028b055ef005b
thibaultdvx <thibault.devarax@icm-institute.org>
2026-04-23 10:14:13 +0200

    test BidsFile object

--- tests/unittests/utils/test_bids.py
+++ tests/unittests/utils/test_bids.py
@@ -1,8 +1,50 @@
 import re
+from pathlib import Path
 
 import pytest
 
-from clinicadl.utils.bids import BidsEntity, Session, Subject
+from clinicadl.utils.bids import BidsEntity, BidsFile, Session, Subject
+
+
+class TestBidsFile:
+    @pytest.mark.parametrize(
+        "path,entities,suffix,extension",
+        [
+            (
+                "sub-000_ses-M000_trc-18FDG_pet.nii.gz",
+                {"sub": "000", "ses": "M000", "trc": "18FDG"},
+                "pet",
+                ".nii.gz",
+            ),
+            (Path("participants.tsv"), {}, "participants", ".tsv"),
+        ],
+    )
+    def test_valid_inputs(self, path, entities, suffix, extension):
+        bids_file = BidsFile(path)
+        assert bids_file.entities == entities
+        assert bids_file.suffix == suffix
+        assert bids_file.extension == extension
+
+    @pytest.mark.parametrize(
+        "path,error",
+        [
+            (
+                "*participants.tsv",
+                "'*participants.tsv' is not a valid BIDS file: the suffix is not alphanumerical (got '*participants')!",
+            ),
+            (
+                "trc-18FDG_pet",
+                "'trc-18FDG_pet' is not a valid file: there is no extension!",
+            ),
+            (
+                "trc-18*FDG_pet.nii.gz",
+                "They value of a BIDS entity must be an alphanumeric string. Got: '18*FDG'",
+            ),
+        ],
+    )
+    def test_bad_inputs(self, path, error):
+        with pytest.raises(AssertionError, match=re.escape(error)):
+            BidsFile(path)
 
 
 def test_bids_entity():
@@ -16,6 +58,11 @@ def test_bids_entity():
         match="They key of a BIDS entity must be an alphanumeric string. Got: 'radio_trc'",
     ):
         BidsEntity("radio_trc-18FFDG")
+    with pytest.raises(
+        AssertionError,
+        match="They value of a BIDS entity must be an alphanumeric string. Got: '18_FFDG'",
+    ):
+        BidsEntity("trc-18_FFDG")
     tracer = BidsEntity("trc-18FFDG")
     assert tracer.key == "trc"
     assert tracer.value == "18FFDG"
