0c463ac6fdf8fdf59242ebaf83945cc7929d4f8e
thibaultdvx <thibault.devarax@icm-institute.org>
2026-06-01 16:46:28 +0200

    tranforms

--- clinicadl/transforms/homemade.py
+++ clinicadl/transforms/homemade.py
@@ -8,7 +8,6 @@ import torchio as tio
 from monai.utils.type_conversion import (
     convert_data_type,
     convert_to_dst_type,
-    convert_to_tensor,
 )
 
 from clinicadl.utils.dtype import DtypeLike
@@ -22,9 +21,9 @@ class Format(tio.Transform):
     """
     Transform to modify the shape or the type of some values in a :py:class:`~clinicadl.data.structures.DataPoint`.
 
-    To be transformed, the values are expected to be :py:class:`torch.Tensor` or a :py:class:`np.ndarray`.
+    To be transformed, the values are expected to be :py:class:`torch.Tensor` or a :py:class:`numpy.ndarray`.
 
-    This transform inherits from :py:class:`torchio.Transforms`, and therefore any argument accepted
+    This transform inherits from :py:class:`torchio.Transform`, and therefore any argument accepted
     by this parent class can be passed via a keyword argument here. Particularly, you may be interested in
     ``include`` or ``exclude`` to specify the keys whose values should be modified, and ``copy`` to specify
     if the output ``DataPoint`` should be the same object as the input or a deepcopy.
@@ -32,7 +31,7 @@ class Format(tio.Transform):
     Parameters
     ----------
     dtype : Optional[torch.dtype], default=None
-        The wanted dtype, passed as a :py:class:`torch.dtype`, a :py:class:`numpy.dtype`, or a ``str`` (e.g., "float32", "int64").
+        The wanted data type, passed as a :py:class:`torch.dtype`, a :py:class:`numpy.dtype`, or a ``str`` (e.g., "float32", "int64").
         If ``None``, input's dtype will be kept.
     squeeze : Union[bool, int, Sequence[int]], default=False
         Whether to squeeze the tensor/array, i.e. removing dimension(s) of size 1.
@@ -59,9 +58,9 @@ class Format(tio.Transform):
 
     .. code-block::
 
-        >>> Format(dtype="int64", include=["age"])(data).age
+        >>> Format(dtype="int64", include=["age"])(data)["age"]
         55
-        >>> Format(unsqueeze=1, include=["array"])(data).array
+        >>> Format(unsqueeze=1, include=["array"])(data)["array"]
         [[1]
          [2]]
     """
@@ -112,16 +111,16 @@ class MergeFields(tio.Transform):
 
     The result of the merger depends on the type of values:
 
-    - :py:class:`torch.Tensor` and :py:class:`np.ndarray` are stacked along a new dimension; they are
-      expected to all have the same shape;
-    - :py:class:`torchio.Image` are concatenated along the channel dimension; they are
-      expected to all have the same spatial shape;
+    - :py:class:`torch.Tensor` and :py:class:`numpy.ndarray` are stacked along a new dimension (so they are
+      expected to all have the same shape);
+    - :py:class:`torchio.Image` are concatenated along the channel dimension (so they are
+      expected to all have the same spatial shape);
     - ``lists`` and ``tuples`` are concatenated;
     - otherwise, the values are just put in a list.
 
-    This transform inherits from :py:class:`torchio.Transforms`, and therefore any argument accepted
+    This transform inherits from :py:class:`torchio.Transform`, and therefore any argument accepted
     by this parent class can be passed via a keyword argument here. Particularly, you may be interested in
-    in ``copy`` to specify if the output ``DataPoint`` should be the same object as the input or a deepcopy.
+    ``copy`` to specify if the output ``DataPoint`` should be the same object as the input or a deepcopy.
 
     Parameters
     ----------
@@ -145,9 +144,9 @@ class MergeFields(tio.Transform):
 
     .. code-block::
 
-        >>> MergeFields("age", "sex", output_key="label")(data).label
+        >>> MergeFields("age", "sex", output_key="label")(data)["label"]
         [55, 'M']
-        >>> MergeFields("array_1", "array_2", output_key="label")(data)
+        >>> MergeFields("array_1", "array_2", output_key="label")(data)["label"]
         [[0. 0.]
          [1. 1.]]
     """
--- docs/api/transforms/index.rst
+++ docs/api/transforms/index.rst
@@ -10,7 +10,7 @@
 .. autosummary::
    :toctree: ../generated/
    :nosignatures:
-   :template: autosummary/class.rst
+   :template: autosummary/transform.rst
 
    TransformsHandler
    MergeFields
